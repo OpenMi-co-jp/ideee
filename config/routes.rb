adbfefe7eb01b0ae4ceda1b05bdf4bc29d4321b9
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-07-24 14:55:51 +0900

    check: header UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'ideas#index'
   resources :ideas
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end

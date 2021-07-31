b9d892144388e44ec5effde02a41bb1ea933bd2f
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-07-31 17:15:41 +0900

    set: ImageUploader and show on UI

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
   has_rich_text :note
+  mount_uploader :icon, ImageUploader
 
   def user
     return User.find_by(id: self.user_id)
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+
+  process resize_to_fit: [500, 600]
   #
   # def scale(width, height)
   #   # do something
@@ -35,9 +37,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.

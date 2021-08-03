dc40397ee257cb6dcb5a40333ec00ecc090da344
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-08-03 11:38:41 +0900

    ref: set clock.js

---
+++ clock.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const URL = 'https://www.ideee.tech/';
+const INTERVAL_MSEC = 20 * 60 * 1000; // 10分毎(30分未満ならOK)
+
+setInterval(() => {
+  http
+    .get(URL, res => {
+      console.log(res, URL);
+    })
+    .on('error', err => {
+      console.log(err, URL);
+    });
+}, INTERVAL_MSEC);

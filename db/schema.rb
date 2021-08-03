77e64b9de8fae34a3409187c312ada13c6fc230d
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-08-03 21:49:46 +0900

    add: User model columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_134432) do
+ActiveRecord::Schema.define(version: 2021_08_03_122550) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -69,6 +69,12 @@ ActiveRecord::Schema.define(version: 2021_07_29_134432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", limit: 30, null: false
+    t.string "description", limit: 200
+    t.integer "point"
+    t.string "icon"
+    t.integer "type"
+    t.string "twitter_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

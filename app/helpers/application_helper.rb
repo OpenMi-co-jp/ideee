db56321cda020c09786124470185d4fe63ed5675
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-08-18 23:11:22 +0900

    set: ogp for twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'ideee'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def full_url(path)
+    domain = if Rails.env.development?
+               'http://localhost:3000'
+             else
+              'https://www.ideee.tech'
+             end
+    "#{domain}#{path}"
+  end
 end

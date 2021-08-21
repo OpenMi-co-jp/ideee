3539be7635a56c9fc7dca4b3468586fa61a13975
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-08-21 17:28:20 +0900

    set: user register routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'ideas#index'
   resources :ideas
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :users, only: %i[show]
   get 'login', to: 'devise/sessions#new'
   post 'login', to: 'devise/sessions#create'

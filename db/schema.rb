dbe7ff5bbef69da0665f19adf916c4ca945a2ec9
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-08-21 23:07:29 +0900

    migrate: user confirmed settings

--- db/schema.rb
+++ db/schema.rb
@@ -72,9 +72,13 @@ ActiveRecord::Schema.define(version: 2021_08_21_134159) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", limit: 30, null: false
+    t.string "name", limit: 30, default: ""
     t.string "description", limit: 200
     t.integer "point"
     t.string "icon"

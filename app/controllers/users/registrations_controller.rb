903f9fedb92aeb8ad4e44bd7d4788b8b27a31fd1
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-04 13:26:45 +0900

    update: production create bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    redirect_to new_user_registration_path
   end
 
   # GET /resource/edit

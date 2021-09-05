392129e286b6b880c87bde4bd15b037510196bed
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-05 20:38:15 +0900

    ref: materialize fix and add modal

--- app/javascript/channels/materialize.js
+++ app/javascript/channels/materialize.js
@@ -1,6 +1,13 @@
 // materialize initialization
 
-// form select
-$(window).on('load', function() {
+// $(document).on ('turbolinks:load', function(){
+//   // $('.modal').modal();
+//   $('.modal').on('click', function() {
+//     $('#modal1').open();
+//     alert("クリックされました");
+//   });
+// });
+$(document).ready(function(){
   $('select').formSelect();
-});
+  $('.modal').modal({startingTop: '30%', endingTop: '200px'});
+})

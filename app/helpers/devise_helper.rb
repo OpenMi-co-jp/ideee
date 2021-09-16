b17177e108b0b9c4b25bcd281d62bd33cf73d096
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-16 22:11:16 +0900

    change: agree note is set for before sign in and up

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,5 +1,10 @@
 module DeviseHelper
-  def omni_sign_up(provider)
+  def omni_sign_image(provider)
     image_tag "#{provider}_icon.png", class: "#{provider}-sign-up"
+  end
+
+  def omni_sign_word(provider)
+    submit = controller_name == 'sessions' ? 'ログイン' : 'ユーザー登録'
+    tag.p "＼ #{OmniAuth::Utils.camelize(provider)}で#{submit} ／", class: "#{provider}-sign-up"
   end
 end

4edb34fdea1c8ac2f687bffe2e4b6c6c461a9c8e
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-20 18:28:37 +0900

    add: upload image check and idea edit page change

---
+++ app/javascript/channels/form.js
@@ -0,0 +1,18 @@
+$(document).on ('turbolinks:load', function(){
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#avatar_img_prev').attr('src', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#post-img").on ('change', function(){
+    $('#avatar_img_prev').removeClass('disabled');
+    $('.present-img').remove();
+    readURL(this);
+  });
+});

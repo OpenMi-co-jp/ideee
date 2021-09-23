c4bf701f71d105d061dc7ac4c574ae227be83443
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-24 00:12:01 +0900

    fix: file delete and basic update

--- app/javascript/channels/comment.js
+++ app/javascript/channels/comment.js
@@ -2,7 +2,6 @@ $(document).on ('turbolinks:load', function(){
 
   $('#comment').on('click', function(e) {
     e.preventDefault()
-    console.log('hello');
     $.ajax({
       url: '/comments',
       type: 'POST',

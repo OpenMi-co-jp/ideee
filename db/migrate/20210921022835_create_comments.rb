c8c938a2e1da4c94b9124e2d6121e5ee3160698b
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-24 23:53:57 +0900

    add: comment migration file

--- db/migrate/20210921022835_create_comments.rb
+++ db/migrate/20210921022835_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.text :description
+      t.text :description, null: false
       t.references :user, null: false, foreign_key: true
       t.references :idea, null: false, foreign_key: true
 

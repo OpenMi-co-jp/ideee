b820361d60475f15a04575067ddb35bbaef9a4cb
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-09-26 12:35:11 +0900

    revert: unexpected commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # if Rails.env.production?
-  raise StandardError.new("Message")
-  raise StandardError
-rescue StandardError, RangeError
-  rescue_from StandardError, with: :render500
-  rescue_from ActiveRecord::RecordNotFound, with: :render404
-  # end
+  if Rails.env.production?
+    rescue_from StandardError, with: :render500
+    rescue_from ActiveRecord::RecordNotFound, with: :render404
+  end
 
   def render404
     render file: Rails.root.join('public', '404.html'), status: :not_found, layout: false, content_type: 'text/html'

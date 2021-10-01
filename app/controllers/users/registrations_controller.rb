b1e045b3eef0bf561c934ac300ee928eff56aeaf
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-10-01 14:07:15 +0900

    fix: submit button update more clickable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,7 +12,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     super
-    SlackNotifier.new.send(resource, user_url(resource.id))
+    SlackNotifier.new.send(resource, user_url(resource&.id))
   end
 
   # GET /resource/edit

b7c3a59784e3ed3959fb42b1dd9bfd5b43989849
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-10-06 22:02:13 +0900

    add: #205 コメントされたら送られた人にメールが飛ぶ

---
+++ app/models/send_email.rb
@@ -0,0 +1,33 @@
+class SendEmail
+  require 'sendgrid-ruby'
+  include SendGrid
+
+  def initialize
+    @from = Email.new(email: 'ideee.info@gmail.com') # SendGridの管理画面でSenderに登録したアドレス
+  end
+
+  def comment(users, idea, comment)
+    subject = "【ideee】【#{idea.name}】にコメントがきました💡"
+    body = "※このメールは自動送信メールです。\n" +
+            "ideee事務局です。\n\n" +
+            "気になるコメントが来ました！\nさっそく反応してみましょう！\n\n" +
+            "---------------\n\n" +
+            "#{comment}\n\n" +
+            "---------------\n" +
+            "https://www.ideee.tech/ideas/#{idea.id}"
+    content = Content.new(type: 'text/plain', value: body)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    if users.instance_of?(Array)
+      users.map do |user|
+        to = Email.new(email: user.email ) # 送信したいアドレスが複数
+        mail = Mail.new(@from, subject, to, content)
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+      end
+    else
+      to = Email.new(email: users.email ) # 送信したいアドレスが一つ
+      mail = Mail.new(@from, subject, to, content)
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+    end
+  end
+end

17c87745a5bfb035c9c94d6858cb360da2a9d2bd
sakita daiki <kurukuruskt28@gmail.com>
2021-10-08 09:49:41 +0900

    ref: #188 likeとcommentのseed_fuファイル

--- db/fixtures/development/03_comment.rb
+++ db/fixtures/development/03_comment.rb
@@ -2,6 +2,6 @@
   num = n + 1
   Comment.seed(
     :id,
-    { id: num, description: Faker::JapaneseMedia::OnePiece.quote, user: User.find((1..User.count).to_a.sample), idea: Idea.find((1..Idea.count).to_a.sample) },
+    { id: num, description: Faker::JapaneseMedia::OnePiece.quote, user: User.all.sample, idea: Idea.all.sample },
   )
 end
--- db/fixtures/development/04_like.rb
+++ db/fixtures/development/04_like.rb
@@ -2,7 +2,7 @@
   num = n + 1
   Like.seed(
     :id,
-    { id: num, user: User.find((1..User.count).to_a.sample), idea: Idea.find((1..Idea.count).to_a.sample) },
+    { id: num, user: User.all.sample, idea: Idea.all.sample },
   )
   # 同一の組み合わせがあった場合はrescueでエラーハンドリング
   rescue ActiveRecord::RecordNotUnique

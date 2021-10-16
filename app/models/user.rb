d1599241f4ecee93f2e1903c4a31f65a6114bc96
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-10-17 00:35:27 +0900

    #228 change: like_usersをlikes_numに刷新

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,6 +82,7 @@ class User < ApplicationRecord
 
   def like(idea)
     likes.find_or_create_by(idea: idea)
+    idea.count_likes
   end
 
   def like?(idea)
@@ -90,6 +91,7 @@ class User < ApplicationRecord
 
   def unlike(idea)
     like_ideas.delete(idea)
+    idea.count_likes
   end
 
   def create_comment(param)

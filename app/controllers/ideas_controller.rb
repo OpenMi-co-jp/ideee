2ba47c46a110132d28e3a3b17d4075ad7b552549
sakita daiki <kurukuruskt28@gmail.com>
2021-10-25 08:58:24 +0900

    ref: #212 トップページのN + 1問題を解消

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,8 +7,8 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all # 一度定義することで何度もDBに値を取りに行くことを阻止
     @latest_ideas = @ideas.order(created_at: "DESC").first(10)
-    @liked_ideas = Idea.all.order(likes_num: "DESC").first(5)
-    @most_viewed_ideas = @ideas.order(view: "DESC").first(5)
+    @liked_ideas = Idea.includes([:comments]).order(likes_num: "DESC").first(5)
+    @most_viewed_ideas = @ideas.includes([:comments]).order(view: "DESC").first(5)
     @featured_users = User.where(defined: true).order(point: "DESC").first(5) # 定義がされているユーザーだけをポイントが高い準に5名
   end
 

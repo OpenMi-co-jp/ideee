4818c835eb16e812523fd016c9ad2537305a34a5
sakita daiki <kurukuruskt28@gmail.com>
2021-10-26 08:31:25 +0900

    add: 各モデルのfactory botを作成

---
+++ spec/factories/ideas.rb
@@ -0,0 +1,13 @@
+carriwave_path = "./app/assets/images/idea_main.png"
+# これを書かないとなぜか関連モデルが作成されない
+FactoryBot.use_parent_strategy = false
+
+FactoryBot.define do
+  factory :idea do
+    name { Faker::JapaneseMedia::Doraemon.gadget }
+    note { Faker::Lorem.paragraph(sentence_count: 20) }
+    icon { File.open(carriwave_path) }
+    view { (0..200).to_a.sample }
+    user
+  end
+end

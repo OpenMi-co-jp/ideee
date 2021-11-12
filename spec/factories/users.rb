d9649b7ecf5d3513efa71d86a8df636f48229435
sakita daiki <kurukuruskt28@gmail.com>
2021-11-12 10:20:48 +0900

    users_controllerのリクエストスペックを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,19 @@ FactoryBot.define do
   factory :user do
     name { Faker::JapaneseMedia::OnePiece.character }
     sequence(:email)   { |n| "sample#{n}@example.com" }
-    definition { 2 }
+    definition { User.definitions.values.sample }
     defined { true }
     confirmed_at { Time.now }
     password { 'password' }
+
+    trait :idea_man do
+      definition { 0 }
+    end
+    trait :engineer do
+      definition { 1 }
+    end
+    trait :idea_engineer do
+      definition { 2 }
+    end
   end
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  let!(:user) { create(:user) }
+
+  describe "GET #index" do
+    it "リクエストが成功すること" do
+      get users_path
+      expect(response.status).to eq 200
+    end
+
+    it 'ユーザーの名前が表示されていること' do
+      get users_path
+      expect(response.body).to include user.name
+    end
+  end
+
+  describe 'GET #show' do
+    context 'ユーザーが存在する場合' do
+      it 'リクエストが成功すること' do
+        get user_path(user)
+        expect(response.status).to eq 200
+      end
+
+      it 'ユーザーの名前が表示されていること' do
+        get user_path(user)
+        expect(response.body).to include user.name
+      end
+    end
+
+    context 'ユーザーが存在しない場合' do
+      subject { -> { get user_path 100 } }
+
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
+  describe 'GET #search' do
+  let!(:idea_man) { create(:user, :idea_man) }
+  let!(:engineer) { create(:user, :engineer) }
+  let!(:idea_engineer) { create(:user, :idea_engineer) }
+    it 'リクエストが成功すること' do
+      get search_users_path
+      expect(response.status).to eq 200
+      expect(response.body).to include idea_man.name
+      expect(response.body).to include engineer.name
+      expect(response.body).to include idea_engineer.name
+    end
+
+    it '検索した定義のユーザーが表示されていること(アイデア/エンジニアは常に表示される)' do
+      get search_users_path, params: { key: 'idea_man' }
+      expect(response.body).to include idea_man.name
+      expect(response.body).not_to include engineer.name
+      expect(response.body).to include idea_engineer.name
+
+      get search_users_path, params: { key: 'engineer' }
+      expect(response.body).to include engineer.name
+      expect(response.body).not_to include idea_man.name
+      expect(response.body).to include idea_engineer.name
+
+      get search_users_path, params: { key: 'idea_engineer' }
+      expect(response.body).to include idea_engineer.name
+      expect(response.body).not_to include idea_man.name
+      expect(response.body).not_to include engineer.name
+    end
+  end
+end

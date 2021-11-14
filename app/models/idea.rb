a35836733a7379a52cc8721da646e69845e5de14
narugit <t.naruhiro.1026@gmail.com>
2021-11-14 22:29:32 +0900

    #296 change: 今回のデータに合わせて40日前までに作られたデータに限定

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -36,6 +36,7 @@ class Idea < ApplicationRecord
 
   scope :published, -> { where draft: false }
   scope :drafts, -> { where draft: true }
+  scope :recent_select, -> { where(created_at: 40.days.ago..Time.now) }
 
   def user
     return User.find_by(id: self.user_id)

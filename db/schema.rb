edc16d1e4f8236a1c9306e0085db056bc4b94184
なるpro <t.naruhiro.1026@gmail.com>
2021-11-22 15:36:48 +0900

    merge: conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_081411) do
+ActiveRecord::Schema.define(version: 2021_11_11_233022) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_081411) do
     t.integer "likes_num", default: 0
     t.integer "difficulty", default: 0
     t.boolean "draft", default: false
+    t.integer "comments_num", default: 0
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
@@ -137,6 +138,8 @@ ActiveRecord::Schema.define(version: 2021_11_11_081411) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "ideas"
   add_foreign_key "comments", "users"
+  add_foreign_key "difficulties", "ideas"
+  add_foreign_key "difficulties", "users"
   add_foreign_key "taggings", "ideas"
   add_foreign_key "taggings", "tags"
 end

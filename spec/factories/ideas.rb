a51447ae8de948eddbf56da9f9c4fb89b643afd8
なるpro <t.naruhiro.1026@gmail.com>
2021-11-22 15:23:40 +0900

    add: annotation

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: ideas
+#
+#  id         :bigint           not null, primary key
+#  difficulty :integer          default("not_yet")
+#  draft      :boolean          default(FALSE)
+#  icon       :string(255)
+#  likes_num  :integer          default(0)
+#  name       :string(255)
+#  note       :text(65535)
+#  view       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_ideas_on_user_id  (user_id)
+#
 carriwave_path = "./app/assets/images/idea_main.png"
 # これを書かないとなぜか関連モデルが作成されない
 FactoryBot.use_parent_strategy = false

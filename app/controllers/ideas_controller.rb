ac33941638249194ebc59cfc1f44935e7353bae4
なるpro <t.naruhiro.1026@gmail.com>
2021-11-25 01:22:26 +0900

    #137 add: アイデア投稿でtweetするbotを設定

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -39,6 +39,7 @@ class IdeasController < ApplicationController
       if draft_bool
         redirect_to @idea, notice: t('.draft_save')
       else
+        TwitterTweet.new.tweet(@idea, idea_url(@idea.id)) if Rails.env.production?
         SlackNotifier.new.send(@idea, idea_url(@idea.id)) if Rails.env.production?
         SlackNotifier.new.apply_send(@idea, idea_url(@idea.id))
         redirect_to @idea, notice: t('.success')
@@ -83,6 +84,7 @@ class IdeasController < ApplicationController
 
   def publish
     @idea.update(draft: false)
+    TwitterTweet.new.tweet(@idea, idea_url(@idea.id)) if Rails.env.production?
     SlackNotifier.new.send(@idea, idea_url(@idea.id)) if Rails.env.production?
     SlackNotifier.new.apply_send(@idea, idea_url(@idea.id))
     redirect_to @idea, notice: t('.success')

f2fd78f7a3703439c40fbe810297a3553cb306d8
sakita daiki <kurukuruskt28@gmail.com>
2021-11-26 09:12:13 +0900

    change: #323 テストデータのアイデアの作成日を10日前から5間の間に変更

--- db/fixtures/development/02_idea.rb
+++ db/fixtures/development/02_idea.rb
@@ -4,10 +4,10 @@ carriwave_path = "./app/assets/images/ideee-tech-logo.svg"
   num = n + 1
   Idea.seed(
     :id,
-    { id: num, name: Faker::JapaneseMedia::Doraemon.gadget , note: Faker::Lorem.paragraph(sentence_count: 20), icon: File.open(carriwave_path) , user: User.find(num), view: (0..200).to_a.sample, created_at: Faker::Date.between(from: 1.year.ago, to: 10.days.ago) },
+    { id: num, name: Faker::JapaneseMedia::Doraemon.gadget , note: Faker::Lorem.paragraph(sentence_count: 20), icon: File.open(carriwave_path) , user: User.find(num), view: (0..200).to_a.sample, created_at: Faker::Date.between(from: 10.days.ago, to: 5.days.ago) },
   )
   Idea.seed(
     :id,
-    { id: num + 15, name: Faker::JapaneseMedia::Doraemon.gadget , note: Faker::Lorem.paragraph(sentence_count: 20), user: User.find(num + 15), view: (0..200).to_a.sample, created_at: Faker::Date.between(from: 1.year.ago, to: 10.days.ago)},
+    { id: num + 15, name: Faker::JapaneseMedia::Doraemon.gadget , note: Faker::Lorem.paragraph(sentence_count: 20), user: User.find(num + 15), view: (0..200).to_a.sample, created_at: Faker::Date.between(from: 10.days.ago, to: 5.days.ago)},
   )
 end

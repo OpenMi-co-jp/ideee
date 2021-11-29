7f992d34eec39e6d164a06070282d79c7e12d301
sakita daiki <kurukuruskt28@gmail.com>
2021-11-29 12:46:12 +0900

    change: ルート名を変更

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,5 @@
 class IdeasController < ApplicationController
-  prepend_before_action :set_idea, only: %i[ show edit update destroy publish cooperation_start cooperation_complete cooperation_confirm cooperation_restart ]
+  prepend_before_action :set_idea, only: %i[ show edit update destroy publish cooperation_start cooperation_complete cooperation_start_confirm cooperation_restart ]
   before_action :authenticate_user!, except: %i[ index show search tags ]
   before_action :own_user_check, only: %i[ edit update destroy ]
   before_action :defined_check, except: %i[ index show search tags]
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
     end
     member do
       post 'publish'
-      get 'cooperation_confirm'
+      get 'cooperation_start_confirm'
+      get 'cooperation_join_confirm'
       post 'cooperation_start'
       post 'cooperation_complete'
       post 'cooperation_restart'

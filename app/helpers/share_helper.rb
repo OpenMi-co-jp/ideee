3dbcd970dce61298b703f1d16e4c796c2c45707b
yoshiki_murakami <eiim.yoshiki@gmail.com>
2021-12-08 21:24:14 +0900

    ref: rootの条件分岐を追加、メソッドの命名変更

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -1,10 +1,10 @@
 module ShareHelper
-  def twitter_share(content, root=false)
+  def twitter_share(content, root = false)
     # footerでのアプリ自体のシェアか、アイデアのシェアかどうかで分岐
     if root
       url = full_url
       text = content
-      twitter_id='ideee_tech'
+      twitter_id = 'ideee_tech'
     else
       url = request.url
       text = content.name
@@ -13,13 +13,18 @@ module ShareHelper
     "https://twitter.com/intent/tweet?text=#{text}&hashtags=ideee&via=#{twitter_id}&related=ideee_tech&url=#{url}"
   end
 
-  def share_sns(media)
-    link_to sns_share("#{media}"), target: '_blank' do
+  def sns_share(media, root = false)
+    link_to route_share_type(media, root), target: '_blank' do
       image_tag "#{media}_icon.png", class: "share__icon", alt: "#{media} share"
     end
   end
 
-  def sns_share(sns)
-    t(".share.#{sns}", url: request.url)
+  # rootかアイデアページかで分岐、SNS事にURL取得
+  def route_share_type(sns, root)
+    if root
+      t(".share.#{sns}")
+    else
+      t(".share.#{sns}", url: request.url)
+    end
   end
 end

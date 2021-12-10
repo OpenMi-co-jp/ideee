637d0de1ca142598dedeb2a52ec61d8f74782b4e
なるpro <t.naruhiro.1026@gmail.com>
2021-12-10 20:58:57 +0900

    #289 ref: cssのコンポーネント化/helperの簡易化

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -1,21 +1,24 @@
 module ShareHelper
-  def twitter_share(content, root = false)
+  def twitter_share(item)
     # footerでのアプリ自体のシェアか、アイデアのシェアかどうかで分岐
-    if root
+    if item.nil?
       url = full_url
-      text = content
+      text = 'アイデアとエンジニアのマッチングサイトideee'
       twitter_id = 'ideee_tech'
     else
       url = request.url
-      text = content.name
-      twitter_id = content&.user.twitter_id
+      text = item.name
+      twitter_id = item&.user.twitter_id
     end
     "https://twitter.com/intent/tweet?text=#{text}&hashtags=ideee&via=#{twitter_id}&related=ideee_tech&url=#{url}"
   end
 
-  def sns_share(media, root = false)
-    link_to root ? t(".share.#{media}") : share_page(media), target: '_blank' do
-      image_tag "#{media}_icon.png", class: "share__icon", alt: "#{media} share"
+  def sns_share(media)
+    link_url = t('.page') == 'footer' ? t(".share.#{media}") : share_page(media)
+    link_to link_url, target: '_blank' do
+      image_tag "#{media}_icon.png",
+        class: "c-share__icon #{'idea-show__share-icon' if t('.page') == 'idea-show'}",
+        alt: "#{media.capitalize} share"
     end
   end
 

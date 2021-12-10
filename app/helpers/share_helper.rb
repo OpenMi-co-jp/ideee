91a6dfc86c2e44597eb58f6e3f4c615577abfb2b
なるpro <t.naruhiro.1026@gmail.com>
2021-12-10 21:29:12 +0900

    #289 ref: sns-share-iconのtwitterを取り込み

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -1,28 +1,19 @@
 module ShareHelper
-  def twitter_share(item)
-    # footerでのアプリ自体のシェアか、アイデアのシェアかどうかで分岐
-    if item.nil?
-      url = full_url
-      text = 'アイデアとエンジニアのマッチングサイトideee'
-      twitter_id = 'ideee_tech'
-    else
-      url = request.url
-      text = item.name
-      twitter_id = item&.user.twitter_id
-    end
-    "https://twitter.com/intent/tweet?text=#{text}&hashtags=ideee&via=#{twitter_id}&related=ideee_tech&url=#{url}"
-  end
-
-  def sns_share(media)
-    link_url = t('.page') == 'footer' ? t(".share.#{media}") : share_page(media)
-    link_to link_url, target: '_blank' do
-      image_tag "#{media}_icon.png",
+  def sns_share_icon(media)
+    link_to link_url(media), target: '_blank' do
+      image_tag "sns-icons/#{media}_icon.png",
         class: "c-share__icon #{'idea-show__share-icon' if t('.page') == 'idea-show'}",
         alt: "#{media.capitalize} share"
     end
   end
 
-  def share_page(media)
-    t(".share.#{media}", url: request.url)
+  def link_url(media)
+    if t('.page') == 'footer'
+      t(".share.#{media}")
+    elsif media == 'twitter'
+      t(".share.twitter", url: request.url, text: @idea.name, twitter_id: @idea&.user.twitter_id)
+    else
+      t(".share.#{media}", url: request.url)
+    end
   end
 end

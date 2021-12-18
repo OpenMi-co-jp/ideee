20ac7404adb7f3426c3efc6e015aeeac23dab3ca
sayahaya <sayahaya1129@gmail.com>
2021-12-18 09:07:26 +0900

    fix: copy.js修正 #397

--- app/javascript/channels/copy.js
+++ app/javascript/channels/copy.js
@@ -1,11 +1,6 @@
 $(document).on ('turbolinks:load', function(){
 
-  $(".c-url-copy").on("click", function() {
-    url = window.location.href;
-    navigator.clipboard.writeText(url);
-  });
-
-  $(".c-url-copy__mobile").on("click", function() {
+  $(".c-url-copy, .c-url-copy__mobile").on("click", function() {
     url = window.location.href;
     navigator.clipboard.writeText(url);
   });

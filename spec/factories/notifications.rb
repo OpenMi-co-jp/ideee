e13550efefb8a0acc1b44b572840fac216ceb47c
松嶋翔太 <matsushimashouta@MBP-SM.local>
2021-12-19 17:16:35 +0900

    add: notifications model 作成

---
+++ spec/factories/notifications.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id         :bigint           not null, primary key
+#  action     :string(255)
+#  checked    :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  comment_id :integer
+#  idea_id    :integer
+#  like_id    :integer
+#  visited_id :integer
+#  visiter_id :integer
+#
+FactoryBot.define do
+  factory :notification do
+    visiter_id { 1 }
+    visited_id { 1 }
+    idea_id { 1 }
+    comment_id { 1 }
+    like_id { 1 }
+    action { "MyString" }
+    checked { false }
+  end
+end

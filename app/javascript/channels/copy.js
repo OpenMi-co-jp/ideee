dc4c5fa330dd8ea9e304585f4ba259a27d32ffeb
sayahaya <sayahaya1129@gmail.com>
2021-12-19 12:07:27 +0900

    fix: 不要なクラス削除 #397

--- app/javascript/channels/copy.js
+++ app/javascript/channels/copy.js
@@ -1,6 +1,6 @@
 $(document).on ('turbolinks:load', function(){
 
-  $(".url-copy-js, .c-url-copy__mobile").on("click", function() {
+  $(".url-copy-js").on("click", function() {
     url = window.location.href;
     navigator.clipboard.writeText(url);
   });

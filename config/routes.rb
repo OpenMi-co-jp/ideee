b34f128c82a7faa152f1e3ebdc2c90f3b3c6f55f
松嶋翔太 <matsushimashouta@MBP-SM.local>
2021-12-19 18:07:28 +0900

    add: コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     end
   end
   resources :difficultys, only: %i[create]
+  resources :notifications, only: %i[index]
   get 'login', to: 'devise/sessions#new'
   post 'login', to: 'devise/sessions#create'
   get 'logout', to: 'devise/sessions#destroy'

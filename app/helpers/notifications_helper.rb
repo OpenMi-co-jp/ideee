79039c9a85bc07f00f956aa23f91368bd705aaf6
SakitaDaiki0623 <kurukuruskt28@gmail.com>
2021-12-28 08:27:54 +0900

    change: 通知一覧をパーシャルに移行

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,17 +1,2 @@
 module NotificationsHelper
-  def notification_text(notification)
-    if notification.action_like?
-      notification_like_text(notification)
-    elsif notification.action_comment?
-      notification_comment_text(notification)
-    end
-  end
-
-  def notification_like_text(notification)
-    "#{notification.visitor.name}さんがあなたの投稿にいいねしました。"
-  end
-
-  def notification_comment_text(notification)
-    "#{notification.visitor.name}さんがあなたの投稿にコメントしました。"
-  end
 end

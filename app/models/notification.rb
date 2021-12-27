4500d7cd3c3536ceddb9214dcf0bd6f2dc38222a
SakitaDaiki0623 <kurukuruskt28@gmail.com>
2021-12-27 14:42:50 +0900

    #241 add: アソシエーションを整理

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -14,9 +14,9 @@
 #  visiter_id :integer
 #
 class Notification < ApplicationRecord
-  belongs_to :idea
-  belongs_to :comment
-  belongs_to :like
-  belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id'
-  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
+  belongs_to :idea, optional: true
+  belongs_to :comment, optional: true
+  belongs_to :like, optional: true
+  belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 end

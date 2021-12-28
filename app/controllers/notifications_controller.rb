ae741f82abf9efbbc9fe9db57bea0357e885c8ef
SakitaDaiki0623 <kurukuruskt28@gmail.com>
2021-12-28 09:01:21 +0900

    add: 通知一覧画面を実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,5 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = Kaminari.paginate_array(current_user.passive_notifications.order(created_at: :desc)).page(params[:page])
+  end
 end

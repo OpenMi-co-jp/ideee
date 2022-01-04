a848e59594be83420f46c8e3001db06c0fd67a21
なるpro <t.naruhiro.1026@gmail.com>
2022-01-05 08:57:53 +0900

    hot_fix: sns shareのバグを修正

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -11,9 +11,9 @@ module ShareHelper
     if t('.page') == 'footer'
       t(".share.#{media}")
     elsif media == 'twitter'
-      t(".share.twitter", url: request.url, text: @idea.name, twitter_id: @idea&.user.twitter_id)
+      t("sns.share.twitter", url: request.url, text: @idea.name, twitter_id: @idea&.user.twitter_id)
     else
-      t(".share.#{media}", url: request.url)
+      t("default.sns.share.#{media}", url: request.url)
     end
   end
 end

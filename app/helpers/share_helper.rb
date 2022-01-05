d0de126fa05c6eade5789f060cb450c7f94f4176
なるpro <t.naruhiro.1026@gmail.com>
2022-01-05 09:00:19 +0900

    hot_fix: twitterも含めたSNSシェアfix

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -11,7 +11,7 @@ module ShareHelper
     if t('.page') == 'footer'
       t(".share.#{media}")
     elsif media == 'twitter'
-      t("sns.share.twitter", url: request.url, text: @idea.name, twitter_id: @idea&.user.twitter_id)
+      t("default.sns.share.twitter", url: request.url, text: @idea.name, twitter_id: @idea&.user.twitter_id)
     else
       t("default.sns.share.#{media}", url: request.url)
     end

4c37f7a1e57499f3048a675a95a5a2431ea9cc44
なるpro <t.naruhiro.1026@gmail.com>
2022-02-19 02:11:58 +0900

    #269 fix: request sign_inのエラー対処

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-# FactoryBotのメソッドエラーに対処
+# request specのメソッドエラーに対処
+require 'devise'
 require 'support/test_helper'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,4 +1,9 @@
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::TestHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end

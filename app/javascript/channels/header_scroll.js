de39cd2c82623f596588c2ddde21c481b072b96b
まきの <katami4koji.923@gmail.com>
2022-03-20 00:17:31 +0900

    レイアウトくずれの修正　#464

--- app/javascript/channels/header_scroll.js
+++ app/javascript/channels/header_scroll.js
@@ -4,14 +4,12 @@ $(document).on ('turbolinks:load', function(){
 	function ScrollAnime() {
 		var elemTop = $('.header-body').offset().top;
 		var scroll = $(window).scrollTop();
-		if(scroll == beforePos) {
-
-		}else if(elemTop > scroll || 0 > scroll - beforePos){
+		if(elemTop > scroll || 0 > scroll - beforePos){
 			$('#header').removeClass('UpMove');
 			$('#header').addClass('DownMove');
 		}else {
 			$('#header').removeClass('DownMove');
-		$('#header').addClass('UpMove');
+			$('#header').addClass('UpMove');
 		}
 		beforePos = scroll;
 	}

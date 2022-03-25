8792fd32f4e1975b1befbb320da720abb45ea857
yamanoura <ryamanoura@gmail.com>
2022-03-26 03:09:32 +0900

    #517 update: 「下書き保存ボタン」、「登録するボタン」、「更新するボタン」の押下時にJSでバリデーションを行う

--- app/javascript/channels/idea_validation.js
+++ app/javascript/channels/idea_validation.js
@@ -2,17 +2,27 @@
 import Rails from "@rails/ujs";
 
 $(document).on ('turbolinks:load', function(){
-  if ($('body').data('page') != "ideas-new") { return }
+  if (!['ideas-edit', 'ideas-new'].includes($('body').data('page'))) { return }
 
-  // 登録のクリックがされた時のバリデーションチェック
-  $('#edit_user_submit').on('click', function() {
-    var invalid_num = 0;
-    invalid_num += M.invalid_input_num('#user_name');
-    invalid_num += M.invalid_input_num('#user_email');
-    invalid_num += M.invalidate_selected_num('#user_definition');
-    if(invalid_num == 0) Rails.fire($("#edit_user")[0],'submit');
+  // 「下書き保存ボタン」のクリックがされた時のバリデーションチェック
+  $('#save_draft_submit').on('click', function() {
+    invalid_idea();
+
   });
 
-  // select用のバリデーション
-  M.validation_select_set('#user_definition');
+  // 「登録するボタン」、「更新するボタン」のクリックがされた時のバリデーションチェック
+  $('#register_submit').on('click', function() {
+    invalid_idea();
+
+  });
+
+  function invalid_idea(){
+    var invalid_num = 0;
+    invalid_num += M.invalid_input_num('#idea_name');
+    invalid_num += M.invalid_input_num('#idea_background');
+    invalid_num += M.invalid_input_num('#idea_goal');
+    if(invalid_num == 0) Rails.fire($("#idea_form")[0],'submit');
+
+};
+
 })

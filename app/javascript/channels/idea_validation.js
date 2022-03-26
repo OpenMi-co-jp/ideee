f7d6948deeabb777d2405960de15112b61d6749f
yamanoura <ryamanoura@gmail.com>
2022-03-26 22:28:01 +0900

    #517 update: JSのコメントアウト見直し

--- app/javascript/channels/idea_validation.js
+++ app/javascript/channels/idea_validation.js
@@ -4,7 +4,7 @@ import Rails from "@rails/ujs";
 $(document).on ('turbolinks:load', function(){
   if (!['ideas-edit', 'ideas-new'].includes($('body').data('page'))) { return }
 
-  // 「下書き保存ボタン」、「登録するボタン」、「更新するボタン」のクリックがされた時のバリデーションチェック
+  // 「下書き保存ボタン」、「登録するボタン」、「公開するボタン」、「更新するボタン」のクリックがされた時のバリデーションチェック
   $("*[name=commit]").on('click', function() {
     var invalid_num = 0;
     invalid_num += M.invalid_input_num('#idea_name');

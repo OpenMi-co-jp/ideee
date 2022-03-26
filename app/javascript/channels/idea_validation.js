9b13eec98d37178b611c6dbab3b5955e1c66075b
yamanoura <ryamanoura@gmail.com>
2022-03-26 22:20:13 +0900

    #517 update: 下書き保存ができない問題を解消

--- app/javascript/channels/idea_validation.js
+++ app/javascript/channels/idea_validation.js
@@ -4,25 +4,12 @@ import Rails from "@rails/ujs";
 $(document).on ('turbolinks:load', function(){
   if (!['ideas-edit', 'ideas-new'].includes($('body').data('page'))) { return }
 
-  // 「下書き保存ボタン」のクリックがされた時のバリデーションチェック
-  $('#save_draft_submit').on('click', function() {
-    invalid_idea();
-
-  });
-
-  // 「登録するボタン」、「更新するボタン」のクリックがされた時のバリデーションチェック
-  $('#register_submit').on('click', function() {
-    invalid_idea();
-
-  });
-
-  function invalid_idea(){
+  // 「下書き保存ボタン」、「登録するボタン」、「更新するボタン」のクリックがされた時のバリデーションチェック
+  $("*[name=commit]").on('click', function() {
     var invalid_num = 0;
     invalid_num += M.invalid_input_num('#idea_name');
     invalid_num += M.invalid_input_num('#idea_background');
     invalid_num += M.invalid_input_num('#idea_goal');
-    if(invalid_num == 0) Rails.fire($("#idea_form")[0],'submit');
-
-};
+  });
 
 })

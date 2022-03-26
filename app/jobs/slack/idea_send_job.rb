6cd171fdb69559d2611780d9656571a901676bfd
なるpro <t.naruhiro.1026@gmail.com>
2022-03-27 05:33:57 +0900

    #623 add: send_weekly_mail_jobを追加

--- app/jobs/slack/idea_send_job.rb
+++ app/jobs/slack/idea_send_job.rb
@@ -6,9 +6,9 @@ module Slack
       # 本番用トライアル
       puts '-------------perform------'
       SlackNotifier.new.trial
-      # new_users = Analytics.new.daily_total_count('newUsers')
-      # sessions = Analytics.new.daily_total_count('sessions')
-      # SlackNotifier.new.send_analytics_report(new_users, sessions)
+      new_users = Analytics.new.daily_total_count('newUsers')
+      sessions = Analytics.new.daily_total_count('sessions')
+      SlackNotifier.new.send_analytics_report(new_users, sessions)
     end
   end
 end

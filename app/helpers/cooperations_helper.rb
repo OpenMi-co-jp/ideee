bb0bc17289e6f2918514c88d90997ca68eb85520
なるpro <t.naruhiro.1026@gmail.com>
2022-03-27 20:29:38 +0900

    #510 change: cooperation ja.ymlの変更

--- app/helpers/cooperations_helper.rb
+++ app/helpers/cooperations_helper.rb
@@ -1,7 +1,7 @@
 module CooperationsHelper
   def modal_link(method, color)
     link_to(
-      t("default.cooperation.#{method}"),
+      t("default.team.#{method}"),
       '#modal_team',
       data: { turbolinks: false },
       class: "waves-effect waves-light btn #{color} modal-trigger",

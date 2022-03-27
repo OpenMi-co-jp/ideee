812d92f02683decbe1f143d98ae4323633da1c6d
なるpro <t.naruhiro.1026@gmail.com>
2022-03-27 16:49:44 +0900

    merge: conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_201109) do
+ActiveRecord::Schema.define(version: 2022_03_27_044231) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -152,6 +152,29 @@ ActiveRecord::Schema.define(version: 2022_03_26_201109) do
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
+  create_table "team_users", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "team_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_team_users_on_team_id"
+    t.index ["user_id", "team_id"], name: "index_team_users_on_user_id_and_team_id", unique: true
+    t.index ["user_id"], name: "index_team_users_on_user_id"
+  end
+
+  create_table "teams", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "idea_id", null: false
+    t.integer "status", default: 0, null: false
+    t.string "requirement", null: false
+    t.string "offer", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_teams_on_idea_id"
+    t.index ["status", "user_id"], name: "index_teams_on_status_and_user_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
     t.string "encrypted_password", default: "", null: false
@@ -189,4 +212,6 @@ ActiveRecord::Schema.define(version: 2022_03_26_201109) do
   add_foreign_key "difficulties", "users"
   add_foreign_key "taggings", "ideas"
   add_foreign_key "taggings", "tags"
+  add_foreign_key "teams", "ideas"
+  add_foreign_key "teams", "users"
 end

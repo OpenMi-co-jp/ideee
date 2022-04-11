775b6ad4180503dd1d63cfee8d8e76514055e4f5
yamanoura <ryamanoura@gmail.com>
2022-04-12 02:36:39 +0900

    #518 update: ユーザ登録画面に文字数チェックの共通JS（submit時用）を呼び出すように修正

--- app/javascript/channels/user_validation.js
+++ app/javascript/channels/user_validation.js
@@ -23,12 +23,13 @@ $(document).on ('turbolinks:load', function(){
     $('#new_user_submit').on('click', function() {
       var invalid_num = 0;
       invalid_num += M.invalid_input_num('#user_email');
-
+      invalid_num += M.invalid_input_min_len_num('#user_password');
+      invalid_num += M.invalid_input_min_len_num('#user_password_confirmation');
       //if(invalid_num == 0) Rails.fire($("#edit_user")[0],'submit');
     });
 
-    M.validation_min_length_set('#user_password');
-    M.validation_min_length_set('#user_password_confirmation');
+    M.validation_min_len_set('#user_password');
+    M.validation_min_len_set('#user_password_confirmation');
 
   }
 

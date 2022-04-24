8945955f9d8efeff3b09bd63b42206285e34d9e2
yamanoura <ryamanoura@gmail.com>
2022-04-24 11:07:01 +0900

    submit時にvalidationを行う処理を除外

--- app/javascript/channels/user_validation.js
+++ app/javascript/channels/user_validation.js
@@ -19,19 +19,8 @@ $(document).on ('turbolinks:load', function(){
 
   if (['users/registrations-new', 'users/registrations-create'].includes($('body').data('page'))) {
 
-    // 登録のクリックがされた時のバリデーションチェック
-    $('#new_user_submit').on('click', function() {
-      var invalid_num = 0;
-      invalid_num += M.invalid_input_num('#user_email');
-      invalid_num += M.invalid_input_min_len_num('#user_password');
-      invalid_num += M.invalid_input_min_len_num('#user_password_confirmation');
-      console.log(invalid_num)
-      if(invalid_num == 0) Rails.fire($("#new_user")[0],'submit');
-    });
-
     M.validation_min_len_set('#user_password');
     M.validation_min_len_set('#user_password_confirmation');
-
   }
 
 })

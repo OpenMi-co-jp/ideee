9ebdd40dd0ccac0f0bd09cbe16404eb1b9769031
なるpro <t.naruhiro.1026@gmail.com>
2022-04-30 19:49:52 +0900

    #518 add: パスワードの新規発行ページにてのエラー文の追加

--- app/javascript/channels/user_registration_validation.js
+++ app/javascript/channels/user_registration_validation.js
@@ -1,5 +1,5 @@
 $(document).on ('turbolinks:load', function() {
-  if (!['users/registrations-new', 'users/registrations-create'].includes($('body').data('page'))) { return }
+  if (!['users/registrations-new', 'users/registrations-create', 'devise/passwords-edit'].includes($('body').data('page'))) { return }
 
   M.validation_min_len_set('#user_password');
   M.validation_min_len_set('#user_password_confirmation');

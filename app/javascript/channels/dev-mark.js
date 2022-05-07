5a2e6ae40e398f1f0d397d06b4ad23d107432134
なるpro <t.naruhiro.1026@gmail.com>
2022-05-07 14:30:32 +0900

    #483 change: jsをフォルダごとに住み分け

--- app/javascript/channels/dev-mark.js
+++ app/javascript/channels/dev-mark.js
@@ -1,9 +1,9 @@
-$(document).on ('turbolinks:load', function(){
-  // 開発環境でのみ読み込み
-  if (window.location.href.startsWith('http://localhost:3000')) {
+// 開発環境でのみ読み込み
+if (window.location.href.startsWith('http://localhost:3000')) {
+  $(document).on ('turbolinks:load', function(){
     // 開発環境のマークを取り除く
     $(".js-dev-close").on("click", function() {
       $('.c-dev-mark').hide();
     });
-  }
-})
+  })
+}

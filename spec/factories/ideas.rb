5d11fad07cee146ae7c75e01230aa2306ff431c0
yamanoura <ryamanoura@gmail.com>
2022-05-15 00:08:35 +0900

    GithubのURLをIdeaモデルに追加

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -8,6 +8,7 @@
 #  difficulty                                               :integer          default("not_yet")
 #  draft                                                    :boolean          default(FALSE)
 #  emailed_at(weeklyメールで新規アイデアとして送られた日時) :datetime
+#  github_url                                               :string(255)
 #  goal                                                     :string(255)
 #  hypothesis                                               :string(255)
 #  icon                                                     :string(255)

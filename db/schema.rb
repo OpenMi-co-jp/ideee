b1dc7d5955a66efdfeb64994382c00ef6ccb01e9
なるpro <t.naruhiro.1026@gmail.com>
2022-05-20 01:14:18 +0900

    change: deviseをtrackableに設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_01_042941) do
+ActiveRecord::Schema.define(version: 2022_05_19_160512) do
 
   create_table "action_text_rich_texts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -187,6 +187,11 @@ ActiveRecord::Schema.define(version: 2022_05_01_042941) do
     t.string "remote_url"
     t.string "site_url"
     t.boolean "defined"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

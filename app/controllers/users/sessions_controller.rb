16b645d658a38bf7aa315fa4dc22f105192dbb49
なるpro <t.naruhiro.1026@gmail.com>
2022-05-21 17:27:51 +0900

    #639 set: 前回のログイン方法を表示

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Users::SessionsController < Devise::SessionsController
+  after_action :set_login_cookie, only: %i[create]
+
   def new
     session.delete('devise.omniauth_data')
     set_minimum_password_length
     super
+  end
+
+  private
+
+  def set_login_cookie
+    # メールのログインのみsession newを使用
+    cookies[:devise_provider] = 'mail'
   end
 end

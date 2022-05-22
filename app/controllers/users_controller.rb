175a7081ad073c198bd9bedfb3963f4244ba20bb
yamanoura <ryamanoura@gmail.com>
2022-05-22 11:17:14 +0900

    #718 update: アイデア実現数を削除、性能対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,20 +11,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    published_list = @user.ideas.published.includes([:idea_tags]).order(published_at: 'DESC')
+    published_list = @user.ideas.includes([:user]).published.includes([:idea_tags]).order(published_at: 'DESC')
     @published_ideas = Kaminari.paginate_array(published_list).page(params[:published_page]).per(10)
-    @like_ideas = Kaminari.paginate_array(Idea.where(id: @user.likes.type_idea_ids).includes([:idea_tags])).page(params[:like_page]).per(10)
+    @like_ideas = Kaminari.paginate_array(Idea.includes([:user]).where(id: @user.likes.type_idea_ids).includes([:idea_tags])).page(params[:like_page]).per(10)
     # 自分のアイデア以外でコメントしたアイデアを表示
-    comment_idea_list = @user.comment_ideas.commented_others_ideas(@user)
+    comment_idea_list = @user.comment_ideas.includes([:user]).commented_others_ideas(@user)
     @commented_ideas = Kaminari.paginate_array(comment_idea_list).page(params[:comment_page]).per(10)
 
     # チーム開発参加をしたアイデアを表示
-    joined_team_idea_list = Idea.where(id:Team.where(id:TeamUser.where(user_id:@user.id).select('team_id')).select('idea_id'))
+    idea_ids = Team.includes([:members]).select{|t| t.members.include?(@user)}.pluck(:idea_id)
+    joined_team_idea_list = Idea.includes([:user]).where(id: idea_ids)
     @joined_team_ideas = Kaminari.paginate_array(joined_team_idea_list).page(params[:joined_team_page]).per(10)
-
-    # アイデア実現をしたアイデアを表示
-    approved_idea_list = @user.ideas.where(product_apply:2)
-    @approved_ideas = Kaminari.paginate_array(approved_idea_list).page(params[:approved_idea_page]).per(10)
   end
 
   def search

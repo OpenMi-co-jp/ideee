0a1ebe998b53f5e22b26c71486ef09ac1c9b92d0
yamanoura <ryamanoura@gmail.com>
2022-06-10 00:57:45 +0900

    #716 clean: rubocop対応

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,13 +1,11 @@
 module EmojiHelper
   def emojify(content)
-    if content.present?
-      h(content).to_str.gsub(/:([\w+-]+):/) do |match|
-        if emoji = Emoji.find_by_alias(Regexp.last_match(1))
-          %(<img alt="#{Regexp.last_match(1)}" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
-        else
-          match
-        end
-      end.html_safe
-    end
+    h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+      if emoji == Emoji.find_by_alias(Regexp.last_match(1))
+        %(<img alt="#{Regexp.last_match(1)}" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
+      else
+        match
+      end
+    end.html_safe
   end
 end

1ff59e9d0b54ae215a358e6c620d2d9a477f1b94
bumpfuji10 <bumpfuji10@gmail.com>
2022-06-16 18:10:11 +0900

    change:doneであればcheckedのCSS適用、failであればアラート

--- app/javascript/channels/common.js
+++ app/javascript/channels/common.js
@@ -14,13 +14,15 @@ $(document).on ('turbolinks:load', function(){
   $('#js_notification_check').on('click', function() {
     // checkedが付いていたら既読なのでreturn
     if ($(this).children('.checked').length != 0) { return }
-    $(this).find('.js_header__bell').addClass('checked');
-// .header__bellを使わない。新しくクラスを付与。上記２行を１行にまとめる。
-// doneの時にcssを付与。error時にはアラート。アラート探す。
-// comment.jsを真似する。
+
     $.ajax({
       url: '/notifications/check',
       type: 'POST'
+    }).done(function() {
+      $('#js_notification_check').find('.js_header__bell').addClass('checked')
+    })
+    .fail(function() {
+      alert('新地通知は0件で')
     })
   })
 });

0b73c64a28e5d4deb0523efd6febbfd41dfb17d9
naru20181117 <t.naruhiro.1026@gmail.com>
2022-07-17 18:50:53 +0900

    #754 fix: rubocopの指摘対応

--- app/models/scopes/idea.rb
+++ app/models/scopes/idea.rb
@@ -1,11 +1,13 @@
-module Scopes::Idea
-  extend ActiveSupport::Concern
+module Scopes
+  class Idea
+    extend ActiveSupport::Concern
 
-  included do
-    # ransackのソートに対応したscope
-    ['ASC', 'DESC'].each do |order|
-      I18n.t('activerecord.sorts').keys.each do |item|
-        Idea.classs_eval { scope item, -> { order("#{item}": order) }}
+    included do
+      # ransackのソートに対応したscope
+      %w[ASC DESC].each do |order|
+        I18n.t('activerecord.sorts').each_key do |item|
+          Idea.classs_eval { scope item, -> { order("#{item}": order) } }
+        end
       end
     end
   end

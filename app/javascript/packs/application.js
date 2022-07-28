b6812c7c311129b2579947b3a15b9b2b5ddc9aba
naru20181117 <t.naruhiro.1026@gmail.com>
2022-07-28 22:57:43 +0900

    #328 ref: sessionの持たせ方/命名を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,4 +36,4 @@ require("channels/carousel");
 require("channels/modal");
 require("channels/idea/validation");
 require("channels/header_scroll");
-require("channels/idea/board.js");
+require("channels/idea/modal_push");

7b1e6dec1883ee9428a1b9856cf05b517008231b
bumpfuji10 <bumpfuji10@gmail.com>
2022-07-31 18:24:36 +0900

    send_comment_emailのjobを修正

--- app/jobs/send_comment_email_job.rb
+++ app/jobs/send_comment_email_job.rb
@@ -1,13 +1,12 @@
 class SendCommentEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(idea, description)
+  def perform(current_user, idea, description)
     return unless Rails.env.production?
 
-    users = [idea.user].push(idea.comment_users.uniq).flatten
-    users.delete(self)
-    return if users.nil?
+    commented_users = idea.comment_users.where.not(id: current_user.id).distinct
+    return if commented_users.nil?
 
-    SendEmail.new.comment(users, self, idea, description)
+    SendEmail.new.comment(commented_users, current_user, idea, description)
   end
 end

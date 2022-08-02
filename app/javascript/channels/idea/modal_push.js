374182e90dc4f2dae49df3b01342f406ab035e67
まきの <katami4koji.923@gmail.com>
2022-08-03 00:29:43 +0900

    モーダルを実施するか判定して実施

--- app/javascript/channels/idea/modal_push.js
+++ app/javascript/channels/idea/modal_push.js
@@ -2,9 +2,10 @@ $(document).on ('turbolinks:load', function(){
   if ($('body').data('page') != "ideas-show") { return }
 
   const has_session = 'modal_session_check' + $('#modal_difficulty').data('id')
+  const modal_check = $('#modal_difficulty').data('modal')
 
   // ideaのセッションがあればシェアモーダルを出現
-  if (window.sessionStorage.getItem(has_session)){
+  if (window.sessionStorage.getItem(has_session) && modal_check){
     $('#modal_difficulty').modal('open');
   } else {
     sessionStorage.removeItem(has_session);

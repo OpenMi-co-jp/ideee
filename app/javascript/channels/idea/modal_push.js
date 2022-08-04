8a857f5d4b8f4fc1aa7e9de29eaa240e012e33f3
まきの <katami4koji.923@gmail.com>
2022-08-04 22:00:58 +0900

    モーダル表示処理のリファクタリング

--- app/javascript/channels/idea/modal_push.js
+++ app/javascript/channels/idea/modal_push.js
@@ -1,11 +1,12 @@
 $(document).on ('turbolinks:load', function(){
   if ($('body').data('page') != "ideas-show") { return }
+  // #modal_difficultyが読み込まれていなかったらreturn
+  if (!$('#modal_difficulty').length){ return }
 
   const has_session = 'modal_session_check' + $('#modal_difficulty').data('id')
-  const modal_check = $('#modal_difficulty').data('modal')
 
   // ideaのセッションがあればシェアモーダルを出現
-  if (window.sessionStorage.getItem(has_session) && modal_check){
+  if (window.sessionStorage.getItem(has_session)){
     $('#modal_difficulty').modal('open');
   } else {
     sessionStorage.removeItem(has_session);

25978b8a6f99017efad4285abc53a2f20eff684c
naru20181117 <t.naruhiro.1026@gmail.com>
2022-10-01 07:16:23 +0900

    fix: seed_fuのエラー対応

--- db/fixtures/development/09_team_user.rb
+++ db/fixtures/development/09_team_user.rb
@@ -1,11 +1,12 @@
 50.times do |n|
   num = n + 1
+  team = Team.all.sample
   TeamUser.seed(
     :id,
     {
       id: num,
-      team: Team.all.sample,
-      user: User.all.sample
+      team: team,
+      user: User.where.not(id: team.members.pluck(:id)).sample
     }
   )
 end

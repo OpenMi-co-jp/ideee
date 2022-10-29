822ab4ada2de560c1febf7b26f48002dba52dedd
松嶋翔太 <matsushimashouta@MBP-SM.local>
2022-10-29 14:21:59 +0900

    add:コメントモデルのテストケース追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it 'モデルの作成が有効であること' do
-    expect(FactoryBot.build(:comment)).to be_valid
+  let(:comment) { FactoryBot.create(:comment) }
+
+  describe "コメントが有効になること" do
+    it "いいねが有効であること" do
+      expect(comment).to be_valid
+    end
+  end
+
+  describe "コメントが無効になること" do
+    context "user_idがないとき" do
+      it "無効であること" do
+        comment.user_id = nil
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "コメントが空の時" do
+      it "無効であること" do
+        comment.description = ""
+        expect(comment).to be_invalid
+      end
+    end
   end
 end

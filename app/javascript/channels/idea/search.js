5f64d4ef6d90679062e39e57e5391b654517e24a
naru20181117 <t.naruhiro.1026@gmail.com>
2022-10-30 11:20:59 +0900

    #825 update end_dateのデフォルトを設定

--- app/javascript/channels/idea/search.js
+++ app/javascript/channels/idea/search.js
@@ -3,13 +3,19 @@ $(document).on ('turbolinks:load', function(){
 
   // 選択する日付の始まり
   $('.datepicker-start').datepicker({
-    defaultDate: new Date(),
-    // setDefaultDate: true,
+    format: 'yyyy/mm/dd',
+    setDefaultDate: true,
+    defaultDate: new Date(yearBefore),
   });
 
   // 選択する日付の終わり
   $('.datepicker-end').datepicker({
-    format: 'yyyy/mm/dd 00:00:00',
-    // setDefaultDate: true,
+    format: 'yyyy/mm/dd',
+    setDefaultDate: true,
+    defaultDate: new Date(),
   });
+
+  function yearBefore () {
+    return new Date().getFullYear() - 1, new Date().getMonth(), new Date().getDate()
+  }
 })

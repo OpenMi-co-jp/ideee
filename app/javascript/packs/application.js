ab646b6b1c33913400954dc3e2f69ff305dc3a51
naru20181117 <t.naruhiro.1026@gmail.com>
2022-11-27 14:39:02 +0900

    #825 ref: materializeを使わずに公開日の検索を設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,6 @@ require("channels/like");
 require("channels/idea/copy");
 require("channels/idea/image_appear");
 require("channels/idea/chips");
-require("channels/idea/search");
 require("channels/comment");
 require("channels/message");
 require("channels/sort");

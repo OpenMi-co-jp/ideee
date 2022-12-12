f27d2656ae2536a6d9a5b42d8a3ee252c3a7a654
松嶋翔太 <matsushimashouta@MBP-SM.local>
2022-12-12 23:28:49 +0900

    rubocopの修正

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Room, type: :model do
   end
 
   describe 'Roomが無効になること' do
-    context 'team_idがない時' do
+    context 'team_idがないとき' do
       it '無効であること' do
         room.team_id = nil
         expect(room).to be_invalid

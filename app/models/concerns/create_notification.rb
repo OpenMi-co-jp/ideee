6d509c8de52e54ee8e1c47fb802f91e8dc989270
naru20181117 <t.naruhiro.1026@gmail.com>
2022-12-14 19:26:18 +0900

    ref: create_notification周りのメソッドをリファクタリング

--- app/models/concerns/create_notification.rb
+++ app/models/concerns/create_notification.rb
@@ -1,42 +1,23 @@
 module CreateNotification
   extend ActiveSupport::Concern
 
-  def create_notification_like(item, like)
+  def create_notification_with_notificationable_type(item, type)
     Notification.find_or_create_by!(
       visitor: self,
       visited_id: item.user_id,
       idea: item.is_a?(Idea) ? item : item.idea,
-      notificatable_type: "Like#{like.likable_type}"
+      notificatable_type: type
     )
   end
 
   def create_notification_comment(idea, comment)
     user_ids = select_notify_commenters(idea)
     user_ids.each do |user_id|
-      create_notification(idea, user_id, comment)
+      create_notification(idea: idea, visited_id: user_id, notificatable: comment)
     end
   end
 
-  def create_notification_difficulty(difficulty)
-    create_notification(difficulty.idea, difficulty.idea.user_id, difficulty)
-  end
-
-  def create_notification_product_apply(idea)
-    Notification.find_or_create_by!(
-      visitor: self,
-      visited_id: idea.user_id,
-      idea: idea,
-      notificatable_type: 'product_apply'
-    )
-  end
-
-  def create_notification_team(idea, team_user)
-    create_notification(idea, idea.user_id, team_user)
-  end
-
-  private
-
-  def create_notification(idea, visited_id, notificatable)
+  def create_notification(idea:, visited_id:, notificatable:)
     active_notifications.find_or_create_by!(
       visitor: self,
       visited_id: visited_id,
@@ -44,6 +25,8 @@ module CreateNotification
       notificatable: notificatable
     )
   end
+
+  private
 
   def select_notify_commenters(idea)
     # アイデア作成者も含めたuser_id取得

9378133d17325c3b2d78a3b556d85fa40a6ea8c8
sayahaya <sayahaya1129@gmail.com>
2022-12-16 08:53:33 +0900

    #893 set: notificationのspecを設定

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'モデルの作成が有効であること' do
+    expect(FactoryBot.build(:notification)).to be_valid
+  end
+
+  describe 'scope' do
+    describe 'not_sent_likes' do
+        let!(:notification) { FactoryBot.create(:notification) }
+        subject { Notification.not_sent_likes }
+        it { is_expected.to include notification }
+    end
+  end
 end

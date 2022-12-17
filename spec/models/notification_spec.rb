dac4c37fd29f93cecbfa35b37627c606371ebef2
sayahaya <sayahaya1129@gmail.com>
2022-12-17 13:21:49 +0900

    #893 fix: rubocop指摘箇所修正2

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
   it 'モデルの作成が有効であること' do
-    expect(build(:notification)).to be_valid
+    expect(FactoryBot.build(:notification)).to be_valid
   end
 
   describe 'scope' do
     describe 'not_sent_likes' do
       subject { described_class.not_sent_likes }
 
-      let!(:notification) { create(:notification) }
+      let!(:notification) { FactoryBot.create(:notification) }
 
       it { is_expected.to include notification }
     end

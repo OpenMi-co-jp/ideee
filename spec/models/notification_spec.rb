b5fdd5d1ea4564bf641b3874b7aa4e1f7911ba95
sayahaya <sayahaya1129@gmail.com>
2022-12-24 12:28:02 +0900

    #893 add: rubocop_todo.ymlにファイル追加

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -11,16 +11,19 @@ RSpec.describe Notification, type: :model do
 
       context 'notificatable_typeがLikeIdeaかつsend_atが設定されていない場合' do
         let!(:notification) { FactoryBot.create(:notification) }
+
         it { is_expected.to include notification }
       end
 
       context 'notificatable_typeがLikeIdea以外の場合' do
         let!(:notification) { FactoryBot.create(:notification, notificatable_type: 'Comment') }
+
         it { is_expected.not_to include notification }
       end
 
       context 'send_atが設定されている場合' do
         let!(:notification) { FactoryBot.create(:notification, send_at: DateTime.now) }
+
         it { is_expected.not_to include notification }
       end
     end

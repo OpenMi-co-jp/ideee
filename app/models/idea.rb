4255de2593420d39e6fc615b6ccdd98228e7704b
naru20181117 <t.naruhiro.1026@gmail.com>
2022-12-26 18:15:37 +0900

    Revert "hot_fix: sidekiqの呼び出し方を確認"
    This reverts commit 52a094e9bbb302a26a301737a8c5537b2580fce9.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -128,7 +128,7 @@ class Idea < ApplicationRecord
   def send_draft_remind
     return unless Rails.env.production? || draft
 
-    RemindDraftJob.perform_in(1.week, id)
+    RemindDraftJob.set(wait: 1.week).perform_later(id)
   end
 
   def voted_percentage(level)

52a094e9bbb302a26a301737a8c5537b2580fce9
naru20181117 <t.naruhiro.1026@gmail.com>
2022-12-26 18:13:59 +0900

    hot_fix: sidekiqの呼び出し方を確認

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -128,7 +128,7 @@ class Idea < ApplicationRecord
   def send_draft_remind
     return unless Rails.env.production? || draft
 
-    RemindDraftJob.set(wait: 1.week).perform_later(id)
+    RemindDraftJob.perform_in(1.week, id)
   end
 
   def voted_percentage(level)

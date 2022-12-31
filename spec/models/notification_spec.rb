d6a497b10dab53fac636f3b564efde0585c86f9d
naru20181117 <t.naruhiro.1026@gmail.com>
2022-12-31 16:35:33 +0900

    fix: rubocopからの修正

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Notification, type: :model do
+RSpec.describe Notification do
   it 'モデルの作成が有効であること' do
     expect(FactoryBot.build(:notification)).to be_valid
   end
@@ -10,19 +10,19 @@ RSpec.describe Notification, type: :model do
       subject { described_class.not_sent_likes }
 
       context 'notificatable_typeがLikeIdeaかつsend_atが設定されていない場合' do
-        let!(:notification) { FactoryBot.create(:notification) }
+        let(:notification) { FactoryBot.create(:notification) }
 
         it { is_expected.to include notification }
       end
 
       context 'notificatable_typeがLikeIdea以外の場合' do
-        let!(:notification) { FactoryBot.create(:notification, notificatable_type: 'Comment') }
+        let(:notification) { FactoryBot.create(:notification, notificatable_type: 'Comment') }
 
         it { is_expected.not_to include notification }
       end
 
       context 'send_atが設定されている場合' do
-        let!(:notification) { FactoryBot.create(:notification, send_at: DateTime.now) }
+        let(:notification) { FactoryBot.create(:notification, send_at: DateTime.now) }
 
         it { is_expected.not_to include notification }
       end

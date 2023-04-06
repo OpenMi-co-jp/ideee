f5381cd73f075d330cb2a5b67b81d7b1f2d70473
naru20181117 <t.naruhiro.1026@gmail.com>
2023-04-06 17:56:49 +0900

    #523 set: next-appをベースとして作成

---
+++ front/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: { dirs: ["./src"] },
+  // nextでimageの最適化を行わない時はtrue
+  // https://nextjs.org/docs/api-reference/next/image
+  experimental: {
+    images: {
+      unoptimized: false,
+    },
+  },
+
+  // consoleを削除する
+  // https://nextjs.org/docs/advanced-features/compiler#remove-console
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+    // emotion: true,
+  },
+};
+
+module.exports = nextConfig

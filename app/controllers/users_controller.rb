d3e5fcf62f26cf7c82d42445e20a9e9405cc8d50
sayahaya <sayahaya1129@gmail.com>
2023-04-09 15:07:25 +0900

    ユーザーshowページで出ているエラー修正 #1016

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,8 +57,7 @@ class UsersController < ApplicationController
 
   def page_user
     @user = User.find_by(id: params[:id])
-    redirect_to root_path if @user.nil?
-    flash.now[:alert] = t('.fail')
+    redirect_to root_path, alert: t('page_user.fail') if @user.nil?
     # TODO: page.userはいろんなところにまたがっているので、i18を使用するのであれば各ページでalertの文言を用意しなければならない
   end
 

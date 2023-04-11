197ee7f61b480714da637953890912474e1391f0
naru20181117 <t.naruhiro.1026@gmail.com>
2023-04-11 22:04:51 +0900

    #1022 set: Header UIを設定

---
+++ front/src/components/Header/headerComponents/Ranking.tsx
@@ -0,0 +1,5 @@
+import { IconTrophy } from '@tabler/icons-react'
+
+export const Ranking = () => {
+  return <IconTrophy />
+}
---
+++ front/src/components/Header/headerComponents/UserIcon.tsx
@@ -0,0 +1,5 @@
+import { Avatar } from '@mantine/core'
+
+export const UserIcon = () => {
+  return <Avatar radius="xl" />
+}

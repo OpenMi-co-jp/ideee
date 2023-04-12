ddd24b063ee93ee8f4f39c88c0273e69e0fead70
naru20181117 <t.naruhiro.1026@gmail.com>
2023-04-12 12:07:38 +0900

    #1022 set: footer UIを設定

---
+++ front/src/components/Footer/FooterForm.tsx
@@ -0,0 +1,180 @@
+import {
+  createStyles,
+  Text,
+  Container,
+  ActionIcon,
+  Group,
+  rem,
+} from '@mantine/core'
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconNews,
+} from '@tabler/icons-react'
+import Image from 'next/image'
+import ideeeLogo from '../../../public/img/ideee_logo.webp'
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: rem(120),
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  logo: {
+    maxWidth: rem(200),
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  description: {
+    marginTop: rem(5),
+
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.xs,
+      textAlign: 'center',
+    },
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  groups: {
+    display: 'flex',
+    flexWrap: 'wrap',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  wrapper: {
+    width: rem(160),
+  },
+
+  link: {
+    display: 'block',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+    fontSize: theme.fontSizes.sm,
+    paddingTop: rem(3),
+    paddingBottom: rem(3),
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+
+  title: {
+    fontSize: theme.fontSizes.lg,
+    fontWeight: 700,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    marginBottom: `calc(${theme.spacing.xs} / 2)`,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  afterFooter: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: theme.spacing.xl,
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  social: {
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.xs,
+    },
+  },
+}))
+
+interface FooterLinksProps {
+  data: {
+    title: string
+    links: { label: string; link: string }[]
+  }[]
+}
+
+export const FooterForm = (footerData: FooterLinksProps) => {
+  const { classes } = useStyles()
+
+  const groups = footerData.data.map((data) => {
+    const links = data.links.map((link, index) => (
+      <Text<'a'>
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ))
+
+    return (
+      <div className={classes.wrapper} key={data.title}>
+        <Text className={classes.title}>{data.title}</Text>
+        {links}
+      </div>
+    )
+  })
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          <Image src={ideeeLogo} alt="ideeeのロゴ" />
+          <Text size="xs" color="dimmed" className={classes.description}>
+            あなたの才能で世界をちょっとだけ良くする
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text color="dimmed" size="sm">
+          © 2022 ideee.tech All rights reserved.
+        </Text>
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg">
+            <IconBrandTwitter size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <IconBrandYoutube size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <IconNews size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  )
+}

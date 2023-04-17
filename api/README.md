0f7b0ba2905197cce1841b974a112e1b5bc6ef02
sayahaya <sayahaya1129@gmail.com>
2023-04-17 09:20:31 +0900

    コンフリ解消

--- api/README.md
+++ api/README.md
@@ -5,32 +5,21 @@
 
 <!-- TODO Readme書く -->
 
-## アプリURL
-
-https://www.ideee.tech/
-
-## BIツールURL
-https://ideee-metabase.herokuapp.com/
-
 ## セットアップの情報
 
 全セットアップ方法
 https://www.notion.so/ideee/Engineering-Wiki-80fac88f11804c7d86ee1ac06bcfc75f
-
-### 静的解析ツール
-
-Dockerfileを解析
-1. インストールしていない場合
-```
-brew install hadolint
-```
-2. 解析実行
-```
-api % hadolint Dockerfile
-```
 
 ## versions
 
 - Rails (7.0.4)
 - Ruby 3.0.2p107
 - mysql2 (0.5.3)
+
+* System dependencies
+
+# ER図
+
+`migrate時にER図を自動生成する`
+
+![](./ideee_erd.png)
---
+++ front/src/components/Footer/FooterData.ts
@@ -0,0 +1,29 @@
+export const footerData = {
+  data: [
+    {
+      title: 'About',
+      links: [
+        { label: 'ideeeについて', link: '#' },
+        { label: 'アイデアの探し方', link: '#' },
+        { label: '月間トレンドまとめ', link: '#' },
+        { label: '個人開発の落とし穴', link: '#' },
+      ],
+    },
+    {
+      title: 'Legal',
+      links: [
+        { label: 'よくある質問', link: '#' },
+        { label: '利用規約', link: '#' },
+        { label: 'プライバシーポリシー', link: '#' },
+        { label: '運営者', link: '#' },
+      ],
+    },
+    {
+      title: 'Links',
+      links: [
+        { label: 'アイデアランキング', link: '#' },
+        { label: 'ユーザーランキング', link: '#' },
+      ],
+    },
+  ],
+}

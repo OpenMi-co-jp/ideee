aa377939fa5eb6449f698d456b5d5fa9715f966c
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-01 19:49:16 +0900

    set: userのmutationsを設定

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -3,5 +3,9 @@ module Types
     field :create_idea, mutation: Mutations::Idea::Create, description: 'アイデア作成'
     field :update_idea, mutation: Mutations::Idea::Update, description: 'アイデア更新'
     field :destroy_idea, mutation: Mutations::Idea::Destroy, description: 'アイデア削除'
+
+    field :create_user, mutation: Mutations::User::Create, description: 'ユーザー作成'
+    field :update_user, mutation: Mutations::User::Update, description: 'ユーザー更新'
+    field :destroy_user, mutation: Mutations::User::Destroy, description: 'ユーザー削除'
   end
 end

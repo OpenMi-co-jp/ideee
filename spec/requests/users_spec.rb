961516d83610a211937e7db01386fdee9e3fce73
daichi1998928 <daichi.tennis.928@icloud.com>
2023-05-05 11:49:53 +0900

    add: #1046 monthly_commenterのテストコード追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -94,4 +94,38 @@ RSpec.describe 'Users' do
       end
     end
   end
+
+  describe 'GET #monthly_commenter' do
+    subject(:get_monthly_commenter) { get monthly_commenter_users_path }
+
+    context '直近１ヶ月にコメントしたユーザーが存在しない場合' do
+      let!(:comment) { FactoryBot.create(:comment, created_at: 1.years.ago) }
+      let!(:commenter) { comment.user }
+
+      it 'リクエストが成功すること' do
+        get_monthly_commenter
+        expect(response).to have_http_status :ok
+      end
+
+      it 'ユーザーの情報が表示されないこと' do
+        get_monthly_commenter
+        expect(response.body).not_to include commenter.name
+      end
+    end
+
+    context '直近1ヶ月にコメントしたユーザーが存在する場合' do
+      let!(:comment) { FactoryBot.create(:comment) }
+      let!(:commenter) { comment.user }
+
+      it 'リクエストが成功すること' do
+        get_monthly_commenter
+        expect(response).to have_http_status :ok
+      end
+
+      it 'コメントしたユーザーの名前が表示されていること' do
+        get_monthly_commenter
+        expect(response.body).to include commenter.name
+      end
+    end
+  end
 end

3062a8c9d82ae70cc991d48add73166ed5b970c6
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-09 17:32:37 +0900

    #1037 fix: OmniAuthのエラーに対処

--- api/config/application.rb
+++ api/config/application.rb
@@ -31,5 +31,12 @@ module Ideee
 
     # rails7以降はデフォルトをvips指定とsるのでmini_magickを指定する
     config.active_storage.variant_processor = :mini_magick
+
+    config.api_only = true
+
+    # OmniAuthのエラーに対処
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end

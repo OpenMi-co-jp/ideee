8846952acf414faeef5a11571d60a446f965f5c4
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-12 17:29:46 +0900

    Revert "hot_fix: herokuのmysqlが落ちる問題に対処"
    This reverts commit fa4671f73c1ec4f78c69df38be058e5124b167b1.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,37 +4,37 @@
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  confirmation_sent_at   :datetime
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  current_sign_in_at     :datetime
-#  current_sign_in_ip     :string(255)
-#  defined                :boolean
-#  definition             :integer
-#  description            :string(200)
-#  email                  :string(255)
-#  encrypted_password     :string(255)      default(""), not null
-#  icon                   :string(255)
-#  ideas_num              :integer          default(0)
-#  last_sign_in_at        :datetime
-#  last_sign_in_ip        :string(255)
-#  name                   :string(30)       default("")
-#  point                  :integer          default(0)
-#  provider               :string(255)      default("email"), not null
-#  remember_created_at    :datetime
-#  remote_url             :string(255)
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string(255)
-#  sign_in_count          :integer          default(0), not null
-#  site_url               :string(255)
-#  tokens                 :json
-#  uid                    :string(255)      default(""), not null
-#  unconfirmed_email      :string(255)
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  github_id              :string(255)
-#  twitter_id             :string(255)
+#  id                       :bigint           not null, primary key
+#  confirmation_sent_at     :datetime
+#  confirmation_token       :string(255)
+#  confirmed_at             :datetime
+#  current_sign_in_at       :datetime
+#  current_sign_in_ip       :string(255)
+#  defined                  :boolean
+#  definition               :integer
+#  description              :string(200)
+#  email                    :string(255)
+#  encrypted_password       :string(255)      default(""), not null
+#  icon                     :string(255)
+#  ideas_num                :integer          default(0)
+#  last_sign_in_at          :datetime
+#  last_sign_in_ip          :string(255)
+#  name                     :string(30)       default("")
+#  point                    :integer          default(0)
+#  provider                 :string(255)      default("email"), not null
+#  remember_created_at      :datetime
+#  remote_url               :string(255)
+#  reset_password_sent_at   :datetime
+#  reset_password_token     :string(255)
+#  sign_in_count            :integer          default(0), not null
+#  site_url                 :string(255)
+#  tokens(認証用トークン)   :json
+#  uid                      :string(255)      default(""), not null
+#  unconfirmed_email        :string(255)
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  github_id                :string(255)
+#  twitter_id               :string(255)
 #
 # Indexes
 #

3f71a383440fdfef564424fcea8cb33a6e2f4354
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-12 12:01:08 +0900

    #1037 fix: rspecを修正

--- api/spec/requests/difficulties_spec.rb
+++ api/spec/requests/difficulties_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Difficulties' do
   before { sign_in user }
 
   describe 'POST #create' do
-    subject(:create_difficultys) { post difficultys_path, params: { idea_id: idea.id, level: Difficulty.levels.keys.sample } }
+    subject(:create_difficultys) { post difficultys_path, params: { idea_id: idea.id, user_id: user.id, level: Difficulty.levels.keys.sample } }
 
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do

2317c1b7b896731117da452601d5f5ab439ce3ef
榑林晃平 <kurebayashi@for-it.co.jp>
2023-05-18 20:41:43 +0900

    add: アイデア投稿時に、モーダルを表示するようにした

--- app/javascript/channels/idea/modal_push.js
+++ app/javascript/channels/idea/modal_push.js
@@ -10,8 +10,39 @@ $(document).on ('turbolinks:load', function(){
     $('#modal_difficulty').modal('open');
   } else {
     sessionStorage.removeItem(has_session);
+    sessionStorage.removeItem('ideaShare');
   }
 
   // ideaのid毎にsessionを設定
   window.sessionStorage.setItem(has_session, true);
+
+  const ideaShareItem = JSON.parse(window.sessionStorage.getItem('ideaShare'));
+  // sessionStorateに既にideaShareItemがいる場合
+  if (ideaShareItem){
+    // シェアモーダルが１度も表示されなかった場合
+    if(ideaShareItem.isDisplay === false) {
+      $('#modal_share').modal('open');
+      ideaShareItem.isDisplay = true;
+      setJsonSessionToStorage(ideaShareItem);
+    }
+  }
+
+  const initIdeaShare = {
+    isShare: false,
+    isDisplay: false
+  };
+
+  const getedIitIdeaShare = window.sessionStorage.getItem('ideaShare');
+
+  if (!getedIitIdeaShare) {
+    $('#modal_share').modal('open');
+    initIdeaShare.isDisplay = true;
+    setJsonSessionToStorage(initIdeaShare);
+  } else {
+    // 既にモーダルが表示済みの場合
+    if (JSON.parse(getedIitIdeaShare).isDisplay === true) { return; }
+    setJsonSessionToStorage(initIdeaShare);
+  }
 })
+
+const setJsonSessionToStorage = value =>  window.sessionStorage.setItem('ideaShare', JSON.stringify(value));

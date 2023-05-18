7c44eaa37525a52a719cb59f88434097ad13a0c6
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-18 18:43:39 +0900

    #1069 change: default_urlを変更してCSRF設定をONに変更

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -75,7 +75,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'https://www.ideee.tech/' }
+  # TODO: 本番へ移行時にドメイン設定を変更
+  config.action_mailer.default_url_options = { host: 'https://ideee-demo.herokuapp.com/' }
+  # config.action_mailer.default_url_options = { host: 'https://www.ideee.tech/' }
 
   config.action_mailer.delivery_method = :smtp
 

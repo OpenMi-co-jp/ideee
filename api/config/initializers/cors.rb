7505800b153098c97038f36a9d4017529a4c06f0
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-18 18:55:30 +0900

    #1069 set: vercelのpreviewモードでアクセルできるように修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # TODO: 本番へ移行時にドメイン設定を変更
-    origins 'http://localhost:3000', 'https://ideee.vercel.app'
+    origins ['http://localhost:3000', 'https://ideee.vercel.app', /ideee-(.*)-narucel\.vercel\.app/]
     resource '*',
       headers: :any,
       expose: %w[access-token expiry token-type uid client authorization],

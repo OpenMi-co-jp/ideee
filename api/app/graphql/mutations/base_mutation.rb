90c4281c6bce4ea4153bce74cd95407b7bc9324a
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-20 18:03:49 +0900

    #1069 change: graphqlのauthチェックをmutationに限定

--- api/app/graphql/mutations/base_mutation.rb
+++ api/app/graphql/mutations/base_mutation.rb
@@ -4,5 +4,12 @@ module Mutations
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
+
+    def self.authorized?(object, context)
+      unless super && context[:current_user].present?
+        raise GraphQL::ExecutionError, "Authentication required"
+        return false
+      end
+    end
   end
 end

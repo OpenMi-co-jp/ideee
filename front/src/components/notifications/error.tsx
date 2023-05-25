6bac50702ab5797468f06e738b8795b143697f10
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-25 14:37:19 +0900

    #1080 set: notificationの共通メソッドを作成

---
+++ front/src/components/notifications/error.tsx
@@ -0,0 +1,18 @@
+import { showNotification } from '@mantine/notifications'
+
+type NotificationProps = {
+  message?: string,
+  action?: string,
+}
+
+export const showError = ({ message = '失敗しました', action = '' }: NotificationProps = {}) => {
+  const actionWord = action ? action + 'に' : ''
+  showNotification({
+    color: 'red',
+    title: `${actionWord}失敗しました`,
+    message,
+    withCloseButton: true,
+    autoClose: 5000,
+    loading: false,
+  })
+}

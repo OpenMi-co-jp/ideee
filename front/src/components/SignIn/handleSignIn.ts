7aa9d61af4d91f370d854eb99364b9a92af73b80
naru20181117 <t.naruhiro.1026@gmail.com>
2023-05-25 14:38:39 +0900

    #1080 set: change: ステータスを確認してnotificationを場合分け

--- front/src/components/SignIn/handleSignIn.ts
+++ front/src/components/SignIn/handleSignIn.ts
@@ -1,5 +1,4 @@
-import { notifications } from '@mantine/notifications'
-// import { IconCheck, IconX } from '@tabler/icons-react'
+import { showSuccess, showError } from '@/components/notifications'
 import Cookies from 'js-cookie'
 
 type FormValues = {
@@ -19,35 +18,17 @@ export const handleSignIn = async (props: FormValues) => {
         body: JSON.stringify({ email, password }),
       }
     )
-    const data = response.json()
-    const token = response.headers.get('authorization')
-    if (token) {
-      Cookies.set('authToken', String(token), { expires: 7, secure: true })
+    if (response.status === 200) {
+      const token = response.headers.get('authorization')
+      if (token) {
+        Cookies.set('authToken', String(token), { expires: 7, secure: true })
+      }
+      showSuccess({ action: 'ログイン' })
+    } else {
+      throw new Error('Request failed with status code: ' + response.status)
     }
-
-    notifications.show({
-      id: 'error-notification',
-      withCloseButton: true,
-      autoClose: 5000,
-      title: 'ログインに成功しました',
-      message: 'ログインに成功しました',
-      color: 'green',
-      style: { backgroundColor: 'green' },
-      sx: { backgroundColor: 'green' },
-      loading: false,
-    })
   } catch (error) {
     console.error(error)
-    notifications.show({
-      id: 'error-notification',
-      withCloseButton: true,
-      autoClose: 5000,
-      title: 'ログインに失敗しました',
-      message: 'ログインに失敗しました',
-      color: 'red',
-      style: { backgroundColor: 'red' },
-      sx: { backgroundColor: 'red' },
-      loading: false,
-    })
+    showError({ action: 'ログイン' })
   }
 }

9b6841029f04231d8fb5ce4b5d0d695652e63ad0
Yo Kamada <bumpfuji10@gmail.com>
2023-06-04 23:22:51 +0900

    add: comment, commentsのquery resolverをquery_typeに登録

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -13,5 +13,9 @@ module Types
     field :notifications, resolver: Resolvers::Notification::NotificationsResolver, description: '通知一覧'
 
     field :team, resolver: Resolvers::Team::TeamResolver, description: 'チームオブジェクト'
+
+    field :comment, resolver: Resolvers::Comment::CommentResolver, description: 'コメントオブジェクト'
+    field :comments, resolver: Resolvers::Comment::CommentsResolver, description: 'コメント一覧'
+
   end
 end

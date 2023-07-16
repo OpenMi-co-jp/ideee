2637e96e789215e27b6b384c8cf022665e2505c1
daichi1998928 <daichi.tennis.928@icloud.com>
2023-07-16 19:14:03 +0900

    不要なコメント削除

--- api/app/admin/ideas.rb
+++ api/app/admin/ideas.rb
@@ -1,7 +1,5 @@
 ActiveAdmin.register Idea do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   permit_params :name, :icon, :background, :issue, :goal, :wish_function, :hypothesis, :target, :monetize, :similar, :github_url, :note, :view, :stance, :user_id, :commit, :product_url
 
   index do

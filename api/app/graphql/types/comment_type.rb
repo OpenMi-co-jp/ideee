e4f687f301e986a0d46161164d8c70cb4bf9a998
Yo Kamada <bumpfuji10@gmail.com>
2023-07-30 18:56:35 +0900

    fix: rubocop

--- api/app/graphql/types/comment_type.rb
+++ api/app/graphql/types/comment_type.rb
@@ -2,11 +2,11 @@
 
 module Types
   class CommentType < Types::BaseObject
-    field :id, ID, null: false
-    field :description, String, null: false
-    field :user_id, Integer, null: false
-    field :idea_id, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :id, ID, null: false, description: 'コメントID'
+    field :description, String, null: false, description: '本文'
+    field :user_id, Integer, null: false, description: 'ユーザーID'
+    field :idea_id, Integer, null: false, description: 'アイデアID'
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: '作成日'
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, description: '更新日'
   end
 end

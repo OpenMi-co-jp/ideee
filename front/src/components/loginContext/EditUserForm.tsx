7e868b9de1cad91491cfb2f4caabf64c70af0300
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-07-31 01:13:25 +0900

    add: #1078 Imgの追加

--- front/src/components/loginContext/EditUserForm.tsx
+++ front/src/components/loginContext/EditUserForm.tsx
@@ -9,96 +9,110 @@ import {
   Grid,
   Text,
   Checkbox,
+  Image,
 } from '@mantine/core'
 
 
 const EditUserForm = () => {
   return (
     <>
-      <Paper p="md" radius="md" shadow="md">
-      </Paper>
-      <Paper p="md" radius="md" shadow="md">
-        <Grid grow>
-          <Grid.Col span={4}>
-            <div>
-              <div>
-                仮のアイコン画像
-              </div>
-              <Button type="submit" >
-                Choose File
-              </Button>
-            </div></Grid.Col>
-          <Grid.Col span={4}>
-            <Title order={2} mb={30}>
-              ユーザー情報編集
-            </Title>
-            <Input.Wrapper
-              id="input-username"
-              withAsterisk
-              label="ユーザー名"
-            >
-              <Input
-              />
-            </Input.Wrapper>
-            <Input.Wrapper
-              id="input-email"
-              withAsterisk
-              label="メールアドレス"
-            >
-              <Input
-              />
-            </Input.Wrapper>
-          </Grid.Col>
-        </Grid>
-        <Textarea
-          label="自己紹介"
+      <div>
+        <Image
+          height={198}
+          width={1235}
+          src="/img/edit-form-header.webp"
+          alt="edit form header"
         />
-        <Grid grow>
-          <Grid.Col span={4}>
-            <Input.Wrapper
-              id="input-twitter"
-              label="Twitter"
-            >
-              <Input
-              />
-            </Input.Wrapper></Grid.Col>
-          <Grid.Col span={4}>
-            <Input.Wrapper
-              id="input-github"
-              label="GitHub"
-            >
-              <Input
-              />
-            </Input.Wrapper></Grid.Col>
-        </Grid>
-        <Grid id="my-grid">
-          <Col span={4}>
-            <div>
-              <Text fz="md">アイデアマン/エンジニア</Text>
-              <Checkbox />
-            </div>
-          </Col>
-          <Col span={4}>
-            <div>
-              <Text fz="md">アイデアマン</Text>
-              <Checkbox />
-            </div>
-          </Col>
-          <Col span={4}>
-            <div>
-              <Text fz="md">エンジニア</Text>
-              <Checkbox />
-            </div>
-          </Col>
-        </Grid>
-        <Grid gutter="md">
-          <Col style={{ marginTop: '1rem' }}>
-            <Button type="submit" >
-              送信
-            </Button>
-          </Col>
-        </Grid>
-      </Paper>
+        <Paper p="md" radius="xl" shadow="md" 
+          style={{width:1136,height:1210}}>
+          <Grid grow>
+            <Grid.Col span={4}>
+              <div>
+                <Image
+                  height={310}
+                  width={310}
+                  radius={50}
+                  src="/img/user-prof.webp"
+                  alt="user prof"
+                />
+                <Button type="submit" >
+                  Choose File
+                </Button>
+              </div>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Title order={2} mb={30}>
+                ユーザー情報編集
+              </Title>
+              <Input.Wrapper
+                id="input-username"
+                withAsterisk
+                label="ユーザー名"
+              >
+                <Input
+                />
+              </Input.Wrapper>
+              <Input.Wrapper
+                id="input-email"
+                withAsterisk
+                label="メールアドレス"
+              >
+                <Input
+                />
+              </Input.Wrapper>
+            </Grid.Col>
+          </Grid>
+          <div>
+            <Text fz="md">自己紹介</Text>
+            <Textarea />
+          </div>
+          <Grid grow>
+            <Grid.Col span={4}>
+              <Input.Wrapper
+                id="input-twitter"
+                label="Twitter"
+              >
+                <Input
+                />
+              </Input.Wrapper></Grid.Col>
+            <Grid.Col span={4}>
+              <Input.Wrapper
+                id="input-github"
+                label="GitHub"
+              >
+                <Input
+                />
+              </Input.Wrapper></Grid.Col>
+          </Grid>
+          <Grid id="my-grid">
+            <Col span={4}>
+              <div>
+                <Text fz="md">アイデアマン/エンジニア</Text>
+                <Checkbox />
+              </div>
+            </Col>
+            <Col span={4}>
+              <div>
+                <Text fz="md">アイデアマン</Text>
+                <Checkbox />
+              </div>
+            </Col>
+            <Col span={4}>
+              <div>
+                <Text fz="md">エンジニア</Text>
+                <Checkbox />
+              </div>
+            </Col>
+          </Grid>
+          <Grid gutter="md">
+            <Col style={{ marginTop: '1rem' }}>
+              <Button type="submit" >
+                送信
+              </Button>
+            </Col>
+          </Grid>
+        </Paper>
+      </div>
     </>
   )
 }

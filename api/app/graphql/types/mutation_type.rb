9491ac4ea33894f0c42fe29b44ebeac955420ced
Yo Kamada <bumpfuji10@gmail.com>
2023-07-30 18:39:31 +0900

    add: deleteMutation

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -9,5 +9,7 @@ module Types
     field :destroy_user, mutation: Mutations::User::Destroy, description: 'ユーザー削除'
 
     field :create_comment, mutation: Mutations::Comment::Create, description: 'コメント作成'
+    field :update_comment, mutation: Mutations::Comment::Update, description: 'コメント更新'
+    field :destroy_comment, mutation: Mutations::Comment::Destroy, description: 'コメント削除'
   end
 end

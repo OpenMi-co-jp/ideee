1cc978a3bf582aed8fab137f39cc9d600493d264
Yo Kamada <bumpfuji10@gmail.com>
2023-07-30 18:39:31 +0900

    add: deleteMutation

--- api/app/graphql/mutations/comment/update.rb
+++ api/app/graphql/mutations/comment/update.rb
@@ -1,16 +1,22 @@
 module Mutations
-  class Comment::Create < BaseMutation
-    graphql_name 'CreateComment'
+  class Comment::Update < BaseMutation
+    graphql_name 'UpdateComment'
 
-    argument :description, String, null: false
-    argument :user_id, Integer, null: false
-    argument :idea_id, Integer, null: false
+    argument :id, ID, required: true, description: 'コメントID'
+    argument :description, String, required: true, description: 'コメント'
+    argument :user_id, Integer, required: true, description: '【必須】ユーザーID'
+    argument :idea_id, Integer, required: true, description: '【必須】アイデアID'
 
     field :comment, Types::CommentType, null: false, description: 'コメントオブジェクト'
     field :success, Boolean, null: false, description: '成功フラグ'
 
     def resolve(**args)
-      comment = ::Comment.create!(**args)
+      comment = ::Comment.find(args[:id])
+      comment.update!(
+        description: args[:description],
+        user_id: args[:user_id],
+        idea_id: args[:idea_id]
+      )
       {
         comment:,
         success: true

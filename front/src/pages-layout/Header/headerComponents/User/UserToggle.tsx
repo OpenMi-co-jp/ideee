9494a878aaa8d065056da2784c2a7a9daed29b0e
sayahaya <sayahaya1129@gmail.com>
2023-07-31 23:24:23 +0900

    #1030 sign_upページの遷移追加

--- front/src/pages-layout/Header/headerComponents/User/UserToggle.tsx
+++ front/src/pages-layout/Header/headerComponents/User/UserToggle.tsx
@@ -15,15 +15,8 @@ export const UserToggle = () => {
         } else {
           return (
             <>
-              <Button
-                onClick={() => {
-                  return openModal({
-                    title: <Title>ユーザー作成</Title>,
-                    children: <SignUpForm />,
-                  })
-                }}
-              >
-                ユーザー作成
+              <Button component="a" href="/user/sign_up">
+                ユーザー登録
               </Button>
               <Button component="a" href="/user/sign_in">
                 ログイン

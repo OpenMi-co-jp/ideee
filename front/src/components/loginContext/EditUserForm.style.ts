2ae286d7ec1f90579d8e774ef459962b503dc363
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-02 02:14:50 +0900

    add: #1078 EditUserFormstyledコンポーネント作成,中寄せ,丸角下のみ修正

---
+++ front/src/components/loginContext/EditUserForm.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+
+export const EditUserFromWrapper = styled.div`
+  display:flex;
+  flex-flow:column;
+  align-items:center;
+
+  .mantine-Paper-root{
+    border-radius: 0 0 2em 2em;
+  }
+
+`

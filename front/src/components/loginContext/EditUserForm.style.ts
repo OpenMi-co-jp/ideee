851ec04e0d7b02ccb4207943500ec24fa4a7689c
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-04 00:31:20 +0900

    update: #1078 input枠線下線のみ

--- front/src/components/loginContext/EditUserForm.style.ts
+++ front/src/components/loginContext/EditUserForm.style.ts
@@ -17,4 +17,15 @@ export const EditUserFromWrapper = styled.div`
   .mantine-InputWrapper-label {
     font-size: 20px;
   }
+  .mantine-Input-input{
+    background-color: rgb(217, 217, 217);
+    border-radius: 0;
+    border-bottom-color: #000000;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+  }
+  .mantine-gszoqu:focus, .mantine-gszoqu:focus-within{
+    border-bottom-color: #000000;
+  }
 `

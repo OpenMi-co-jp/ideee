f6d4f74923f2155daf2330d3d6d53056d4aed227
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-07 02:11:21 +0900

    update: #1035 MainVisualのスタイリング

--- front/src/components/features/MainVisual.tsx
+++ front/src/components/features/MainVisual.tsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import Image from 'next/image'
+import { Title, Button } from '@mantine/core'
 
 const MainVisual = () => {
   return (
-    <div>MainVisual</div>
+    <>
+      <div >
+        <Image
+          src={'/img/ideeemv.webp'}
+          alt='MvImage'
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          style={{
+            maxHeight: '500px',
+            zIndex: '-300',
+            top: '150px'
+          }}
+          fill
+        />
+      </div>
+      <div
+        style={{
+          width: '350px',
+          paddingTop: '200px',
+          paddingBottom: '220px',
+          textAlign: 'center',
+        }}>
+        <div
+          style={{
+            display: 'flex',
+            flexFlow: 'column',
+            paddingBottom: '20px',
+          }}>
+          <Title
+            style={{
+              color: '#FF862F',
+              textAlign: 'center',
+              fontFamily: 'Inter',
+              fontSize: '32px',
+              fontStyle: 'normal',
+              fontWeight: 700,
+              lineHeight: 'normal',
+            }}>
+            あなたのアイデアを
+          </Title>
+          <Title
+            style={{
+              color: '#FF862F',
+              textAlign: 'center',
+              fontFamily: 'Inter',
+              fontSize: '32px',
+              fontStyle: 'normal',
+              fontWeight: 700,
+              lineHeight: 'normal',
+            }}>
+            エンジニアと盛り上げる
+          </Title>
+        </div>
+        <Button color='orange'>IDEEEについて｜⇢</Button>
+      </div>
+    </>
   )
 }
 

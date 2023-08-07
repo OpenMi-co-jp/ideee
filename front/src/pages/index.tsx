d65fc298731c715fab381e35b8a7a74cad6c1643
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-08 02:44:00 +0900

    update: #1035仮組みの外し

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -23,12 +23,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ width: '100%', height: '100vh' }}>
-        <MainVisual />
-        <NewIdea />
-        <SearchTags />
-        <Philosophy />
-      </div>
+      <MainVisual />
+      <NewIdea />
+      <SearchTags />
+      <Philosophy />
     </>
   )
 }

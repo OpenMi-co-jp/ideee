c275df93170d87aaf037bfadd9cb019261ab0f6d
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-09 01:21:28 +0900

    add: #1035 新しいアイデア3パターン設置

--- front/src/components/features/NewIdea3.tsx
+++ front/src/components/features/NewIdea3.tsx
@@ -1,21 +1,21 @@
 import { Box, Title, Text, Paper, Avatar, Flex } from '@mantine/core'
 import React from 'react'
 
-const NewIdea = () => {
+const NewIdea3 = () => {
   return (
     <>
       <Box
         style={{
           borderRadius: '40px',
-          border: '3px solid #F2CE0D',
-          background: '#FFF',
+          border: '3px solid #DD633CB8',
+          background: '#DD633CB8',
           marginTop: '50px',
           marginBottom: '50px',
         }}
       >
         <Title
           style={{
-            color: '#DCCC39',
+            color: '#000000',
             fontFamily: 'Inter',
             fontSize: '28px',
             fontStyle: 'normal',
@@ -37,166 +37,7 @@ const NewIdea = () => {
             margin: '15px 0 25px 0',
           }}
         >
-          <Paper
-            shadow="md"
-            radius="md"
-            p="md"
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              margin: '15px 45px',
-            }}
-          >
-            <Avatar
-              radius="xl"
-              size={60}
-              style={{
-                marginRight: '10px',
-              }}
-            />
-            <Text
-              style={{
-                color: '#000',
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 300,
-                lineHeight: 'normal',
-                marginRight: '15px',
-              }}
-            >
-              暇を提供するアプリ
-            </Text>
-            <Avatar
-              radius="xl"
-              size={24}
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-              style={{
-                marginTop: '25px',
-              }}
-            />
-          </Paper>
-          <Paper
-            shadow="md"
-            radius="md"
-            p="md"
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              margin: '15px 45px',
-            }}
-          >
-            <Avatar
-              radius="xl"
-              size={60}
-              style={{
-                marginRight: '10px',
-              }}
-            />
-            <Text
-              style={{
-                color: '#000',
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 300,
-                lineHeight: 'normal',
-                marginRight: '15px',
-              }}
-            >
-              暇を提供するアプリ
-            </Text>
-            <Avatar
-              radius="xl"
-              size={24}
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-              style={{
-                marginTop: '25px',
-              }}
-            />
-          </Paper>
-          <Paper
-            shadow="md"
-            radius="md"
-            p="md"
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              margin: '15px 45px',
-            }}
-          >
-            <Avatar
-              radius="xl"
-              size={60}
-              style={{
-                marginRight: '10px',
-              }}
-            />
-            <Text
-              style={{
-                color: '#000',
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 300,
-                lineHeight: 'normal',
-                marginRight: '15px',
-              }}
-            >
-              暇を提供するアプリ
-            </Text>
-            <Avatar
-              radius="xl"
-              size={24}
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-              style={{
-                marginTop: '25px',
-              }}
-            />
-          </Paper>
-          <Paper
-            shadow="md"
-            radius="md"
-            p="md"
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              margin: '15px 45px',
-            }}
-          >
-            <Avatar
-              radius="xl"
-              size={60}
-              style={{
-                marginRight: '10px',
-              }}
-            />
-            <Text
-              style={{
-                color: '#000',
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 300,
-                lineHeight: 'normal',
-                marginRight: '15px',
-              }}
-            >
-              暇を提供するアプリ
-            </Text>
-            <Avatar
-              radius="xl"
-              size={24}
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-              style={{
-                marginTop: '25px',
-              }}
-            />
-          </Paper>
+
           <Paper
             shadow="md"
             radius="md"
@@ -443,4 +284,4 @@ const NewIdea = () => {
   )
 }
 
-export default NewIdea
+export default NewIdea3
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -4,7 +4,9 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import testQuery from '@/components/test'
 import MainVisual from '@/components/features/MainVisual'
-import NewIdea from '@/components/features/NewIdea'
+import NewIdea1 from '@/components/features/NewIdea1'
+import NewIdea2 from '@/components/features/NewIdea2'
+import NewIdea3 from '@/components/features/NewIdea3'
 import SearchTags from '@/components/features/SearchTags'
 import Philosophy from '@/components/features/Philosophy'
 
@@ -24,8 +26,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainVisual />
-      <NewIdea />
+      <NewIdea1 />
       <SearchTags />
+      <NewIdea2 />
+      <NewIdea3 />
       <Philosophy />
     </>
   )

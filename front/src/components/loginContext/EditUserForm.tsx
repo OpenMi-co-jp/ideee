6ab9b8199e8eced8b9dc39decd2585316e9ac7f9
naru20181117 <t.naruhiro.1026@gmail.com>
2023-08-12 19:10:21 +0900

    #1078 update: ユーザー編集フォームを修正

--- front/src/components/loginContext/EditUserForm.tsx
+++ front/src/components/loginContext/EditUserForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  Card,
   Input,
   Button,
   Textarea,
@@ -10,62 +11,68 @@ import {
   Text,
   Checkbox,
   Image,
+  FileInput,
+  rem,
 } from '@mantine/core'
-import { EditUserFromWrapper } from './EditUserForm.style'
+import { TextForm } from '@/components/ReactFormSet'
+import { useForm } from 'react-hook-form'
+import { IconUpload } from '@tabler/icons-react'
+
+type UserEditFormValues = {
+  name: string
+  email: string
+}
 
 const EditUserForm = () => {
+  const iconRef = '/img/undefined_user_icon.webp'
+  const form = useForm<UserEditFormValues>({
+    defaultValues: {},
+    mode: 'onChange',
+  })
+
+  const onSubmit = () => {
+    console.log('-------submit------')
+  }
   return (
     <>
-      <EditUserFromWrapper>
-        <Image
-          height={198}
-          width={1235}
-          src="/img/edit-form-header.webp"
-          alt="edit form header"
-        />
+      <Image
+        src="/img/edit-form-header.webp"
+        fit="scale-down"
+        alt="user edit header img"
+      />
+      <Card padding="xl" style={{ backgroundColor: '#fff' }}>
         <Paper
           p="xl"
           shadow="md"
           style={{
-            width: 1136,
-            height: 1210,
-            backgroundColor: '#D9D9D9',
+            backgroundColor: '#EFEFEF',
             paddingTop: '50px',
           }}
         >
+          <form onSubmit={form.handleSubmit(onSubmit)}></form>
           <Grid grow>
-            <Grid.Col span="content">
-              <div
-                style={{
-                  display: 'flex',
-                  flexFlow: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Image
-                  height={310}
-                  width={310}
-                  radius={50}
-                  src="/img/user-prof.webp"
-                  alt="user prof"
-                />
-                <Button
-                  type="submit"
-                  style={{
-                    fontSize: '20px',
-                    width: '160px',
-                    height: '35px',
-                    flexShrink: 0,
-                    marginTop: '1rem',
-                    fontWeight: 500,
-                    color: '#000000',
-                    border: '1px solid #000',
-                    background: '#C2C2C2',
-                  }}
-                >
-                  Choose File
-                </Button>
-              </div>
+            <Grid.Col
+              span="content"
+              style={{
+                display: 'flex',
+                flexFlow: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Image
+                height={150}
+                width={150}
+                radius={50}
+                src={iconRef}
+                alt="user prof"
+              />
+              <FileInput
+                placeholder="画像をアップロード"
+                label="ユーザーアイコン"
+                radius="md"
+                mt="xl"
+                icon={<IconUpload size={rem(14)} />}
+              />
             </Grid.Col>
             <Grid.Col span={5}>
               <Title
@@ -73,57 +80,35 @@ const EditUserForm = () => {
                 mb={30}
                 style={{
                   textAlign: 'center',
-                  fontSize: '36px',
                   fontWeight: '500',
                 }}
               >
                 ユーザー情報編集
               </Title>
-              <Input.Wrapper
-                id="input-username"
-                withAsterisk
+              <TextForm
+                form={form}
+                name="name"
                 label="ユーザー名"
-              >
-                <Input
-                  style={{
-                    paddingTop: 50,
-                  }}
-                />
-              </Input.Wrapper>
-              <Input.Wrapper
-                id="input-email"
-                withAsterisk
-                label="メールアドレス"
-                style={{
-                  paddingTop: 10,
-                }}
-              >
-                <Input
-                  style={{
-                    paddingTop: 50,
-                  }}
-                />
-              </Input.Wrapper>
+                required
+                my="lg"
+              />
+              <TextForm
+                form={form}
+                name="email"
+                label="メールアドレス"
+                required
+                my="lg"
+              />
             </Grid.Col>
           </Grid>
-          <div>
-            <Text
-              fz="md"
-              style={{
-                textAlign: 'center',
-                fontSize: '20px',
-                marginTop: 30,
-                marginBottom: 20,
-              }}
-            >
-              自己紹介
-            </Text>
-            <Textarea
-              style={{
-                marginBottom: '20px',
-              }}
-            />
-          </div>
+          <Text fz="md" my="lg">
+            自己紹介
+          </Text>
+          <Textarea
+            style={{
+              marginBottom: '20px',
+            }}
+          />
           <Grid grow>
             <Grid.Col span={4}>
               <Input.Wrapper id="input-twitter" label="Twitter ID">
@@ -144,34 +129,27 @@ const EditUserForm = () => {
               </Input.Wrapper>
             </Grid.Col>
           </Grid>
-          <Grid
-            id="my-grid"
-            style={{
-              paddingTop: 55,
-            }}
-          >
-            <Col span={4}>
-              <div
+          <Grid id="my-grid" justify="center" pt={40} grow>
+            <Col
+              span={6}
+              style={{
+                display: 'flex',
+                flexFlow: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Text
+                fz="md"
+                pb={20}
                 style={{
-                  display: 'flex',
-                  flexFlow: 'column',
-                  alignItems: 'center',
+                  fontSize: '20px',
                 }}
               >
-                <Text
-                  fz="md"
-                  style={{
-                    textAlign: 'center',
-                    fontSize: '20px',
-                    paddingBottom: 25,
-                  }}
-                >
-                  アイデアマン/エンジニア
-                </Text>
-                <Checkbox color="orange" size="xl" />
-              </div>
+                エンジニア
+              </Text>
+              <Checkbox color="orange" size="xl" label="🛠" />
             </Col>
-            <Col span={4}>
+            <Col span={6}>
               <div
                 style={{
                   display: 'flex',
@@ -181,70 +159,32 @@ const EditUserForm = () => {
               >
                 <Text
                   fz="md"
+                  pb={20}
                   style={{
-                    textAlign: 'center',
                     fontSize: '20px',
-                    paddingBottom: 25,
                   }}
                 >
                   アイデアマン
                 </Text>
-                <Checkbox color="orange" size="xl" />
-              </div>
-            </Col>
-            <Col span={4}>
-              <div
-                style={{
-                  display: 'flex',
-                  flexFlow: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Text
-                  fz="md"
-                  style={{
-                    textAlign: 'center',
-                    fontSize: '20px',
-                    paddingBottom: 25,
-                  }}
-                >
-                  エンジニア
-                </Text>
-                <Checkbox color="orange" size="xl" />
+                <Checkbox color="orange" size="xl" label="💡" />
               </div>
             </Col>
           </Grid>
           <Grid gutter="md">
-            <Col
-              style={{
-                marginTop: '1rem',
-                textAlign: 'center',
-              }}
-            >
+            <Col mt="lg">
               <Button
                 type="submit"
-                style={{
-                  height: 60,
-                  width: 230,
-                  border: '1px solid #EE8D8D',
-                  background: '#F4AE7B',
-                  borderRadius: '20px',
-                  color: '#FFF',
-                  textAlign: 'center',
-                  fontFamily: 'Inter',
-                  fontSize: '40px',
-                  fontStyle: 'normal',
-                  fontWeight: 500,
-                  lineHeight: 'normal',
-                  marginTop: 150,
-                }}
+                fullWidth
+                my={40}
+                size="lg"
+                style={{ background: '#F4AE7B' }}
               >
-                送信
+                登録
               </Button>
             </Col>
           </Grid>
         </Paper>
-      </EditUserFromWrapper>
+      </Card>
     </>
   )
 }

d9b6de346fbfd6c8d2a046527abbdfaea2f11909
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-13 00:41:49 +0900

    update: #1035 各要素中央寄せ調節

--- front/src/components/features/Philosophy.tsx
+++ front/src/components/features/Philosophy.tsx
@@ -5,33 +5,49 @@ import { Box, Text, Title, Divider, Flex } from '@mantine/core'
 const Philosophy = () => {
   return (
     <>
-      <Box>
+      <Box
+        style={{
+          padding: '50px 0',
+        }}
+      >
         <Title
           style={{
             fontFamily: 'Inter',
-            fontSize: '25px',
+            fontSize: '32px',
             fontStyle: 'normal',
             fontWeight: 700,
             lineHeight: 'normal',
+            textAlign: 'center',
+            marginBottom: '30px',
+            letterSpacing: '3px',
           }}
         >
           アイデアをデザインしエンジニアの出会いを盛り上げる
         </Title>
         <Divider style={{
           border: 0,
-          borderTopWidth: '0.0625rem',
+          borderTopWidth: '0.125rem',
           borderTopColor: '#000000',
           borderTopStyle: 'solid',
-          margin: 0,
-          width: '30%',
+          margin: '0 auto',
+          paddingBottom: '50px',
+          width: '55%',
         }} />
         <Flex
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
           style={{
             fontFamily: 'Inter',
             fontSize: '14px',
             fontStyle: 'normal',
             fontWeight: 700,
             lineHeight: 'normal',
+            paddingBottom: '50px',
+            justifyContent: 'space-between',
+            width: '50%',
+            margin: '0 auto',
           }}
         >
           <Text>
@@ -50,41 +66,61 @@ const Philosophy = () => {
             <span style={{ color: '#FDB221' }} >E</span>ncourage
           </Text>
         </Flex>
-        <Text
+        <Flex
           style={{
-            fontFamily: 'Inter',
-            fontSize: '25px',
-            fontStyle: 'normal',
-            fontWeight: 700,
-            lineHeight: 'normal',
+            position: 'relative',
+            marginTop: '80px',
           }}
         >
-          あなたの「困ってる」を形に
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'Inter',
-            fontSize: '25px',
-            fontStyle: 'normal',
-            fontWeight: 700,
-            lineHeight: 'normal',
-          }}
-        >
-          コードで社会に貢献してみませんか？
-        </Text>
-
-        <Flex style={{ position: 'relative' }}>
-          <Image
-            src={'/img/people.webp'}
-            alt="people"
-            width={430}
-            height={300}
+          <Flex
+            justify="center"
+            direction="column"
+            wrap="wrap"
             style={{
-              margin: '0 auto',
-              zIndex: 500,
-              marginTop: '18vh',
+              margin: '-20px auto 0 auto',
             }}
-          />
+          >
+            <Text
+              style={{
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 700,
+                lineHeight: 'normal',
+                zIndex: 500,
+                textAlign: 'center',
+                paddingBottom: '5px',
+                letterSpacing: '3px',
+              }}
+            >
+              あなたの「困ってる」を形に
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 700,
+                lineHeight: 'normal',
+                zIndex: 500,
+                textAlign: 'center',
+                letterSpacing: '3px',
+              }}
+            >
+              コードで社会に貢献してみませんか？
+            </Text>
+            <Image
+              src={'/img/people.webp'}
+              alt="people"
+              width={430}
+              height={300}
+              style={{
+                margin: '0 auto',
+                zIndex: 500,
+                marginTop: '16vh',
+              }}
+            />
+          </Flex>
           <div
             style={{
               width: '500px',

e720a1bba79e084453282db9e9c1f6e994e1e800
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-14 00:22:22 +0900

    add:#1035チーム開発募集中のアイデアコンポーネント追加

---
+++ front/src/components/features/TeamIdea.tsx
@@ -0,0 +1,206 @@
+import React from 'react'
+import { Box, Title, Text, Paper, Avatar, Flex } from '@mantine/core'
+
+const TeamIdea = () => {
+  return (
+    <>
+      <Box
+        style={{
+          borderRadius: '40px',
+          border: '3px solid #E5AA37',
+          background: '#FFF',
+          marginTop: '50px',
+          marginBottom: '50px',
+        }}
+      >
+        <Title
+          style={{
+            color: '#E5AA37',
+            fontFamily: 'Inter',
+            fontSize: '28px',
+            fontStyle: 'normal',
+            fontWeight: 800,
+            lineHeight: 'normal',
+            marginTop: '24px',
+            marginLeft: '80px',
+            marginBottom: '15px',
+          }}
+        >
+          チーム開発募集中のアイデア
+        </Title>
+        <Flex
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+          style={{
+            margin: '15px 0 25px 0',
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="md"
+            p="md"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: '15px 45px',
+            }}
+          >
+            <Avatar
+              radius="xl"
+              size={60}
+              style={{
+                marginRight: '10px',
+              }}
+            />
+            <Text
+              style={{
+                color: '#000',
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: 'normal',
+                marginRight: '15px',
+              }}
+            >
+              暇を提供するアプリ
+            </Text>
+            <Avatar
+              radius="xl"
+              size={24}
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+              style={{
+                marginTop: '25px',
+              }}
+            />
+          </Paper>
+          <Paper
+            shadow="md"
+            radius="md"
+            p="md"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: '15px 45px',
+            }}
+          >
+            <Avatar
+              radius="xl"
+              size={60}
+              style={{
+                marginRight: '10px',
+              }}
+            />
+            <Text
+              style={{
+                color: '#000',
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: 'normal',
+                marginRight: '15px',
+              }}
+            >
+              暇を提供するアプリ
+            </Text>
+            <Avatar
+              radius="xl"
+              size={24}
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+              style={{
+                marginTop: '25px',
+              }}
+            />
+          </Paper>
+          <Paper
+            shadow="md"
+            radius="md"
+            p="md"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: '15px 45px',
+            }}
+          >
+            <Avatar
+              radius="xl"
+              size={60}
+              style={{
+                marginRight: '10px',
+              }}
+            />
+            <Text
+              style={{
+                color: '#000',
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: 'normal',
+                marginRight: '15px',
+              }}
+            >
+              暇を提供するアプリ
+            </Text>
+            <Avatar
+              radius="xl"
+              size={24}
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+              style={{
+                marginTop: '25px',
+              }}
+            />
+          </Paper>
+          <Paper
+            shadow="md"
+            radius="md"
+            p="md"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: '15px 45px',
+            }}
+          >
+            <Avatar
+              radius="xl"
+              size={60}
+              style={{
+                marginRight: '10px',
+              }}
+            />
+            <Text
+              style={{
+                color: '#000',
+                fontFamily: 'Inter',
+                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: 'normal',
+                marginRight: '15px',
+              }}
+            >
+              暇を提供するアプリ
+            </Text>
+            <Avatar
+              radius="xl"
+              size={24}
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+              style={{
+                marginTop: '25px',
+              }}
+            />
+          </Paper>
+        </Flex>
+      </Box>
+    </>
+  )
+}
+
+export default TeamIdea

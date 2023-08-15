ad011fcbb4473c547d60b5c20fea753e51022157
Yo Kamada <bumpfuji10@gmail.com>
2023-08-15 23:53:17 +0900

    fix: Rubocop

--- api/spec/factories/teams.rb
+++ api/spec/factories/teams.rb
@@ -30,7 +30,7 @@ FactoryBot.define do
     offer { 'test_offer' }
     requirement { 'test_requirement' }
     status { 'active' }
-    association :owner, factory: :user
+    owner { association(:user) }
     idea
     owner_id { idea.user.id }
   end

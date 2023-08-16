16549be7b432b04de57cce2ab608f8d6b0d156ff
Yo Kamada <bumpfuji10@gmail.com>
2023-08-16 22:51:17 +0900

    fix: typo

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -1,8 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    field :destroy, mutation: Mutations::Team::Destroy
-    field :update, mutation: Mutations::Team::Update
-    field :create, mutation: Mutations::Team::Create
+    field :create_team, mutation: Mutations::Team::Create, description: 'チーム作成'
+    field :update_team, mutation: Mutations::Team::Update, description: 'チーム更新'
+    field :destroy_team, mutation: Mutations::Team::Destroy, description: 'チーム削除'
 
     field :create_idea, mutation: Mutations::Idea::Create, description: 'アイデア作成'
     field :update_idea, mutation: Mutations::Idea::Update, description: 'アイデア更新'

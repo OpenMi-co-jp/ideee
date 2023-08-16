2b5b5826e01138cb1c7fe6a3e707692490c7484c
Yo Kamada <bumpfuji10@gmail.com>
2023-08-16 22:21:52 +0900

    add: 各Mutationを用意

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -1,5 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
+    field :destroy, mutation: Mutations::Team::Destroy
+    field :update, mutation: Mutations::Team::Update
+    field :create, mutation: Mutations::Team::Create
+
     field :create_idea, mutation: Mutations::Idea::Create, description: 'アイデア作成'
     field :update_idea, mutation: Mutations::Idea::Update, description: 'アイデア更新'
     field :destroy_idea, mutation: Mutations::Idea::Destroy, description: 'アイデア削除'

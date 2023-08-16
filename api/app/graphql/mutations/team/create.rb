8e1fce56b8b27ada8a5b8d64abf36486fc3e021b
Yo Kamada <bumpfuji10@gmail.com>
2023-08-16 22:47:16 +0900

    add:teamの crateMutation追加

--- api/app/graphql/mutations/team/create.rb
+++ api/app/graphql/mutations/team/create.rb
@@ -1,14 +1,30 @@
 module Mutations
   class Team::Create < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    graphql_name 'CreateTeam'
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :owner_id, ID, required: true, description: '【必須】オーナーID'
+    argument :idea_id, ID, required: true, description: '【必須】アイデアID'
+    argument :status, Integer, required: true, description: '【必須】チームステータス'
+    argument :requirement, String, required: true, description: '【必須】お願いすること'
+    argument :offer, String, required: true, description: '【必須】(メンバーが)得られるもの'
+    argument :members_num, Integer, required: true, description: 'メンバー数'
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
-  end
+    field :team, Type::TeamType, null: false, description: 'チームオブジェクト'
+    field :success, Boolean, null: false, description: '成功フラグ'
+
+    def resolve(**args)
+      team = ::Team.new(
+        owner_id, args[:owner_id],
+        idea_id, args[:idea_id],
+        status, args[:status],
+        requirement, args[:requirement],
+        offer, args[:offer],
+        members_num, args[:members_num]
+      )
+      team.save!
+      {
+        team:,
+        success: true
+      }
+    end
 end

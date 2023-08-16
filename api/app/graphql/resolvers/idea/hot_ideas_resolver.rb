691f0bef947b5c99ccc4c3d97b86020d0169a231
naru20181117 <t.naruhiro.1026@gmail.com>
2023-08-17 00:24:07 +0900

    #1136 set: rootページでのアイデア用のresolverを設定

---
+++ api/app/graphql/resolvers/idea/hot_ideas_resolver.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class Idea::HotIdeasResolver < BaseResolver
+    graphql_name 'GetHotIdeas'
+
+    type [Types::IdeaType], null: false
+
+    def resolve
+      new_ideas = ::Idea.eager_load(:user).published.last(4)
+      hot_ideas = ::Idea.published.recent_select.eager_load([:user]).order(published_at: 'DESC').first(6)
+      new_ideas.concat(hot_ideas).shuffle
+    end
+  end
+end

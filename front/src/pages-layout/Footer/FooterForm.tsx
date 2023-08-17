074710d40adc7850a4b219e163096d1827692312
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-17 23:39:34 +0900

    update: #1035 コード整形

--- front/src/pages-layout/Footer/FooterForm.tsx
+++ front/src/pages-layout/Footer/FooterForm.tsx
@@ -166,13 +166,11 @@ export const FooterForm = (footerData: FooterLinksProps) => {
 
         <Group spacing={0} className={classes.social} position="right" noWrap>
           <ActionIcon size="lg">
-
             <IconBrandTwitter size="1.05rem" stroke={1.5} />
           </ActionIcon>
 
           <ActionIcon size="lg">
             <IconNews size="1.05rem" stroke={1.5} />
-
           </ActionIcon>
         </Group>
       </Container>

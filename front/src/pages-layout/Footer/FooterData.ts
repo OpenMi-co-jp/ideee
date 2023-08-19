95b8caa3cea562621a0677b51eabf3109e1881b9
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-08-19 22:08:14 +0900

    update: #1138 footerLinkの遷移先パス変更

--- front/src/pages-layout/Footer/FooterData.ts
+++ front/src/pages-layout/Footer/FooterData.ts
@@ -25,7 +25,7 @@ export const footerData = {
           label: 'よくある質問',
           link: 'https://www.ideee.tech/frequent_questions',
         },
-        { label: '利用規約', link: 'https://www.ideee.tech/terms_of_service' },
+        { label: '利用規約', link: '/documentpages/termOfService'},
         {
           label: 'プライバシーポリシー',
           link: 'https://www.ideee.tech/privacy_policy',

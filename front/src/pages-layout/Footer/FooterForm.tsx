bdd0ef76e137ba9543b5b8394c1a62e39d52583d
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-08-19 22:51:18 +0900

    clean: #1138 コード補正

--- front/src/pages-layout/Footer/FooterForm.tsx
+++ front/src/pages-layout/Footer/FooterForm.tsx
@@ -125,11 +125,8 @@ export const FooterForm = (footerData: FooterLinksProps) => {
 
   const groups = footerData.data.map((data) => {
     const links = data.links.map((link, index) => (
-      <CustomLink key={index}
-        href={link.link}>
-        <Anchor className={classes.link}>
-          {link.label}
-        </Anchor>
+      <CustomLink key={index} href={link.link}>
+        <Anchor className={classes.link}>{link.label}</Anchor>
       </CustomLink>
     ))
 
--- front/src/pages/customlink.tsx
+++ front/src/pages/customlink.tsx
@@ -1,19 +1,10 @@
-import React,{ ReactNode } from 'react';
-
+import React, { ReactNode } from 'react'
 
 type CustomLinkProps = {
-  href: string;
-children: ReactNode;
-};
-
+  href: string
+  children: ReactNode
+}
 
 export const CustomLink: React.FC<CustomLinkProps> = ({ href, children }) => {
-    return (
-      <div onClick={() => window.location.href = href}>
-        {children}
-      </div>
-    );
-  }
-
-  
-  
+  return <div onClick={() => (window.location.href = href)}>{children}</div>
+}

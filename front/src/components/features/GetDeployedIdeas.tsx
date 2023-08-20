d20ca99a725201045b926dc191dfb9dc098f87bc
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-08-21 01:00:34 +0900

    update:#1035getDeployedIdeasの実装

---
+++ front/src/components/features/GetDeployedIdeas.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { Box, Title, Text, Paper, Avatar, Flex } from '@mantine/core'
+import { useGetDeployedIdeasQuery } from '@/lib/generated/client'
+
+const GetDeployedIdeas = () => {
+  const { loading, data } = useGetDeployedIdeasQuery()
+  if (loading) return <p>Loading...</p>
+  const DeployedIdeas = data?.deployedIdeas
+  return (
+    <>
+      <Box
+        style={{
+          borderRadius: '40px',
+          border: '3px solid #ED3C3C',
+          background: '#FFF',
+          marginTop: '50px',
+          marginBottom: '50px',
+        }}
+      >
+        <Title
+          style={{
+            color: '#9E9595',
+            fontFamily: 'Inter',
+            fontSize: '28px',
+            fontStyle: 'normal',
+            fontWeight: 800,
+            lineHeight: 'normal',
+            marginTop: '24px',
+            marginLeft: '80px',
+            marginBottom: '15px',
+          }}
+        >
+          実現したアイデア
+        </Title>
+        <Flex
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+          style={{
+            margin: '15px 0 25px 0',
+          }}
+        >
+          {DeployedIdeas?.map((DeployedIdea) => {
+            return (
+              <Paper
+                key={DeployedIdea.id}
+                shadow="md"
+                radius="md"
+                p="md"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  margin: '15px 45px',
+                }}
+              >
+                <Avatar
+                  radius="xl"
+                  size={60}
+                  style={{
+                    marginRight: '10px',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontFamily: 'Inter',
+                    fontSize: '25px',
+                    fontStyle: 'normal',
+                    fontWeight: 300,
+                    lineHeight: 'normal',
+                    marginRight: '15px',
+                  }}
+                >
+                  {DeployedIdea.name}
+                </Text>
+                <Avatar
+                  radius="xl"
+                  size={24}
+                  src={DeployedIdea.user.icon}
+                  style={{
+                    marginTop: '25px',
+                  }}
+                />
+              </Paper>
+            )
+          })}
+        </Flex>
+      </Box>
+    </>
+  )
+}
+
+export default GetDeployedIdeas

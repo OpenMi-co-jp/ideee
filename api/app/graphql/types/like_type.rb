8c3b45af6d5951811b3c22f5e6d05fd5e23572ef
sayahaya <sayahaya1129@gmail.com>
2023-08-25 09:24:32 +0900

    add: LikeTypeを追加 #1101

---
+++ api/app/graphql/types/like_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class LikeType < Types::BaseObject
+    field :id, ID, null: false
+    field :user_id, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :likable_type, String, null: false
+    field :likable_id, Integer, null: false
+  end
+end

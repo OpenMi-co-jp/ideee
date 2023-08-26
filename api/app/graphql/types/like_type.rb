688bfe815cffcb6382d354d05993fa7741335207
sayahaya <sayahaya1129@gmail.com>
2023-08-26 10:40:10 +0900

    fix: rubocop #1101

--- api/app/graphql/types/like_type.rb
+++ api/app/graphql/types/like_type.rb
@@ -2,11 +2,11 @@
 
 module Types
   class LikeType < Types::BaseObject
-    field :id, ID, null: false
-    field :user_id, Integer
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :likable_type, String, null: false
-    field :likable_id, Integer, null: false
+    field :id, ID, null: false, description: 'コメントID'
+    field :user_id, Integer, description: 'ユーザーID'
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: '作成日'
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, description: '更新日'
+    field :likable_type, String, null: false, description: 'いいねをされたエンティティのタイプ（例: "記事", "コメント"）'
+    field :likable_id, Integer, null: false, description: 'いいねをされたエンティティのID'
   end
 end

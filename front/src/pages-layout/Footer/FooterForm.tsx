72a8c42b6877f61644fd1903a5c95c0863df088c
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-08-27 20:41:52 +0900

    update: #1139 FooterForm.tsxのAnchorタグをLinkに変更

--- front/src/pages-layout/Footer/FooterForm.tsx
+++ front/src/pages-layout/Footer/FooterForm.tsx
@@ -9,6 +9,7 @@ import {
 } from '@mantine/core'
 import { IconBrandTwitter } from '@tabler/icons-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const useStyles = createStyles((theme) => ({
   footer: {
@@ -124,9 +125,9 @@ export const FooterForm = (footerData: FooterLinksProps) => {
 
   const groups = footerData.data.map((data) => {
     const links = data.links.map((link, index) => (
-      <Anchor key={index} className={classes.link} href={link.link}>
+      <Link key={index} className={classes.link} href={link.link}>
         {link.label}
-      </Anchor>
+      </Link>
     ))
 
     return (

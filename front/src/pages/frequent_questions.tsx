62beb1ad0598b82c06827fed7b5ec5e7cb91aff2
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-08-28 12:05:35 +0900

    add: #1137 frequent_question.tsxを追加し内部リンクを設定

---
+++ front/src/pages/frequent_questions.tsx
@@ -0,0 +1,8 @@
+import { Container, Paper, Text, Title } from '@mantine/core'
+import Link from 'next/link'
+
+export default function FrequentQuestion(){
+    return(
+        <Title fz={27} fw={100}>よくある質問</Title>
+    )
+}

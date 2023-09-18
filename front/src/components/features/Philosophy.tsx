22f2365004cc8346edcba4b9d0f232fb92d96495
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-19 00:35:15 +0900

    #1035 update: useStylesの適応

--- front/src/components/features/Philosophy.tsx
+++ front/src/components/features/Philosophy.tsx
@@ -1,149 +1,214 @@
 import React from 'react'
 import Image from 'next/image'
-import { Box, Text, Title, Divider, Flex } from '@mantine/core'
+import { createStyles, Box, Text, Title, Divider, Flex } from '@mantine/core'
+
+const useStyles = createStyles(() => ({
+  Box: {
+    padding: '50px 0',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  Title: {
+    fontSize: '32px',
+    textAlign: 'center',
+    marginBottom: '30px',
+    letterSpacing: '3px',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  Divider: {
+    border: 0,
+    borderTopWidth: '0.125rem',
+    borderTopColor: '#000000',
+    borderTopStyle: 'solid',
+    margin: '0 auto',
+    paddingBottom: '50px',
+    width: '55%',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  SubTitle: {
+    fontSize: '14px',
+    fontWeight: 700,
+    paddingBottom: '50px',
+    justifyContent: 'space-between',
+    width: '50%',
+    margin: '0 auto',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  SubTitleInitialCommon: {
+    color: '#FDB221'
+  },
+
+  ImageWrapper: {
+    position: 'relative',
+    marginTop: '80px',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  ImageInnerWrapper: {
+    margin: '-20px auto 0 auto',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  ImageTitle: {
+    fontSize: '25px',
+    fontWeight: 700,
+    zIndex: 500,
+    textAlign: 'center',
+    paddingBottom: '5px',
+    letterSpacing: '3px',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  PeopleImage: {
+    margin: '0 auto',
+    zIndex: 500,
+    marginTop: '16vh',
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  PeopleLeftImage: {
+    width: '500px',
+    height: '435px',
+    borderRadius: '50%',
+    position: 'absolute',
+    background: 'rgba(239, 213, 120, 0.49)',
+    top: 0,
+    left: 0,
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+  PeopleRightImage: {
+    width: '500px',
+    height: '435px',
+    borderRadius: '50%',
+    position: 'absolute',
+    background: 'rgba(215, 145, 145, 0.50)',
+    top: '50px',
+    left: '450px',
+
+    '@media (max-width: 649px)': {
+
+    },
+    '@media (max-width: 1000px) and (min-width:650px)': {
+
+    },
+  },
+
+}))
 
 const Philosophy = () => {
+  const { classes } = useStyles()
   return (
     <>
-      <Box
-        style={{
-          padding: '50px 0',
-        }}
-      >
-        <Title
-          style={{
-            fontFamily: 'Inter',
-            fontSize: '32px',
-            fontStyle: 'normal',
-            fontWeight: 700,
-            lineHeight: 'normal',
-            textAlign: 'center',
-            marginBottom: '30px',
-            letterSpacing: '3px',
-          }}
-        >
+      <Box className={classes.Box}>
+        <Title className={classes.Title} >
           アイデアをデザインしエンジニアの出会いを盛り上げる
         </Title>
-        <Divider
-          style={{
-            border: 0,
-            borderTopWidth: '0.125rem',
-            borderTopColor: '#000000',
-            borderTopStyle: 'solid',
-            margin: '0 auto',
-            paddingBottom: '50px',
-            width: '55%',
-          }}
-        />
+        <Divider className={classes.Divider} />
         <Flex
-          justify="center"
           align="center"
           direction="row"
           wrap="wrap"
-          style={{
-            fontFamily: 'Inter',
-            fontSize: '14px',
-            fontStyle: 'normal',
-            fontWeight: 700,
-            lineHeight: 'normal',
-            paddingBottom: '50px',
-            justifyContent: 'space-between',
-            width: '50%',
-            margin: '0 auto',
-          }}
+          className={classes.SubTitle}
         >
           <Text>
-            <span style={{ color: '#FDB221' }}>I</span>dea
+            <span className={classes.SubTitleInitialCommon}>I</span>dea
           </Text>
           <Text>
-            <span style={{ color: '#FDB221' }}>D</span>esign
+            <span className={classes.SubTitleInitialCommon}>D</span>esign
           </Text>
           <Text>
-            <span style={{ color: '#FDB221' }}>E</span>ngineer
+            <span className={classes.SubTitleInitialCommon}>E</span>ngineer
           </Text>
           <Text>
-            <span style={{ color: '#FDB221' }}>E</span>ncounter
+            <span className={classes.SubTitleInitialCommon}>E</span>ncounter
           </Text>
           <Text>
-            <span style={{ color: '#FDB221' }}>E</span>ncourage
+            <span className={classes.SubTitleInitialCommon}>E</span>ncourage
           </Text>
         </Flex>
-        <Flex
-          style={{
-            position: 'relative',
-            marginTop: '80px',
-          }}
-        >
+        <Flex className={classes.ImageWrapper}>
           <Flex
             justify="center"
             direction="column"
             wrap="wrap"
-            style={{
-              margin: '-20px auto 0 auto',
-            }}
+            className={classes.ImageInnerWrapper}
           >
-            <Text
-              style={{
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 700,
-                lineHeight: 'normal',
-                zIndex: 500,
-                textAlign: 'center',
-                paddingBottom: '5px',
-                letterSpacing: '3px',
-              }}
-            >
+            <Text className={classes.ImageTitle} >
               あなたの「困ってる」を形に
             </Text>
-            <Text
-              style={{
-                fontFamily: 'Inter',
-                fontSize: '25px',
-                fontStyle: 'normal',
-                fontWeight: 700,
-                lineHeight: 'normal',
-                zIndex: 500,
-                textAlign: 'center',
-                letterSpacing: '3px',
-              }}
-            >
+            <Text className={classes.ImageTitle} >
               コードで社会に貢献してみませんか？
             </Text>
             <Image
+              className={classes.PeopleImage}
               src={'/img/bottom_vision_image.webp'}
               alt="bottom_vision_image"
               width={430}
               height={300}
-              style={{
-                margin: '0 auto',
-                zIndex: 500,
-                marginTop: '16vh',
-              }}
             />
           </Flex>
           <div
-            style={{
-              width: '500px',
-              height: '435px',
-              borderRadius: '50%',
-              position: 'absolute',
-              background: 'rgba(239, 213, 120, 0.49)',
-              top: 0,
-              left: 0,
-            }}
+            className={classes.PeopleLeftImage}
           />
           <div
-            style={{
-              width: '500px',
-              height: '435px',
-              borderRadius: '50%',
-              position: 'absolute',
-              background: 'rgba(215, 145, 145, 0.50)',
-              top: '50px',
-              left: '450px',
-            }}
+            className={classes.PeopleRightImage}
           />
         </Flex>
       </Box>

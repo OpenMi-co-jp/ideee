a15d285f3130cbfccb6e9d380c9c02eea20554ac
naru20181117 <t.naruhiro.1026@gmail.com>
2023-09-20 10:32:13 +0900

    delete: 不必要なデフォルトファイルを削除

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,24 +1,17 @@
 import { useState, useEffect } from 'react'
-import { Inter } from 'next/font/google'
-import testQuery from '@/components/test'
 import TopVision from '@/components/features/TopVision'
 import GetPopularTags from '@/components/features/GetPopularTags'
 import Philosophy from '@/components/features/Philosophy'
 import GetActiveTeamIdeas from '@/components/features/GetActiveTeamIdeas'
 import GetDeployedIdeas from '@/components/features/GetDeployedIdeas'
 import GetHotIdeas from '@/components/features/GetHotIdeas'
-import { Loader } from '@mantine/core'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data, loading } = testQuery()
   const [LSLoggedIn, setLSLoggedIn] = useState(false)
 
   useEffect(() => {
     setLSLoggedIn(localStorage.getItem('loggedIn') === 'true')
   }, [])
-  if (loading) return <Loader color="yellow" />
 
   return (
     <>

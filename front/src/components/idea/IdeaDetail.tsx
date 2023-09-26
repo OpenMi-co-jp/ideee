e8aab165363cf17da51b6621c26003b70a6dffb6
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-26 17:55:58 +0900

    #1102  add:ログイン仮設置

--- front/src/components/idea/IdeaDetail.tsx
+++ front/src/components/idea/IdeaDetail.tsx
@@ -1,14 +1,24 @@
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Image, Title, Group, Button } from '@mantine/core'
 import { UserIcon, AccompaniedTags, IdeaContent } from './'
+import { useLoggedIn } from '@/components/loginContext'
 
 const IdeaDetail = () => {
   const router = useRouter()
+
+  const { loggedIn } = useLoggedIn()
+  const [LSLoggedIn, setLSLoggedIn] = useState(false)
+
   const { id } = router.query
+  useEffect(() => {
+    setLSLoggedIn(localStorage.getItem('loggedIn') === 'true')
+  }, [])
 
   return (
     <>
-      <Group>
+      {(LSLoggedIn || loggedIn) && 'ログイン中'}
+      <Group position="center">
         <Image
           height={50}
           width={50}

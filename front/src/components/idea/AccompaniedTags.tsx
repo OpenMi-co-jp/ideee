b93cc47e5bcb3f4083242094121393183a42976b
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-30 03:34:56 +0900

    #1102update:useGetIdeaQueryの追加

--- front/src/components/idea/AccompaniedTags.tsx
+++ front/src/components/idea/AccompaniedTags.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { Loader } from '@mantine/core'
+import { useGetIdeaQuery } from '@/lib/generated/client'
 
 export const AccompaniedTags = () => {
-  return <div>AccompaniedTags</div>
+  const router = useRouter()
+  const { data, loading, error } = useGetIdeaQuery({
+    variables: {
+      id: router.query.id as string,
+    },
+  })
+
+  if (loading) return <Loader color="yellow" />
+
+  return (
+    <>
+      {data?.idea.ideaTags?.map((tag) => {
+        return <div key={tag.id}>{tag.name}</div>
+      })}
+    </>
+  )
 }

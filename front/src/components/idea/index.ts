58afe04be45962655c175fee66be577fff0c7bdb
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-30 03:49:44 +0900

    #1102 update:コード整形

--- front/src/components/idea/index.ts
+++ front/src/components/idea/index.ts
@@ -6,4 +6,3 @@ export { UserIcon } from './UserIcon'
 export { AccompaniedTags } from './AccompaniedTags'
 export { IdeaContent } from './IdeaContent'
 export { SearchedIdeas, IdeaNotFound, SearchInput } from './search'
-

853d62168a990302ff6db3a2202636489439d6ff
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-30 03:40:30 +0900

    #1102 update:useGetIdeaQueryの追加

--- front/src/components/idea/IdeaContent.tsx
+++ front/src/components/idea/IdeaContent.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
+import { Loader } from '@mantine/core'
+import { useGetIdeaQuery } from '@/lib/generated/client'
 export const IdeaContent = () => {
-  return <div>IdeaContent</div>
+  const router = useRouter()
+  const { data, loading, error } = useGetIdeaQuery({
+    variables: {
+      id: router.query.id as string,
+    },
+  })
+
+  if (loading) return <Loader color="yellow" />
+
+  return (
+    <>
+      <div>{data?.idea.background}</div>
+      <div>{data?.idea.goal}</div>
+      <div>{data?.idea.issue}</div>
+      <div>{data?.idea.wishFunction}</div>
+      <div>{data?.idea.hypothesis}</div>
+      <div>{data?.idea.target}</div>
+      <div>{data?.idea.monetize}</div>
+      <div>{data?.idea.similar}</div>
+      <div>{data?.idea.note}</div>
+    </>
+  )
 }

33fef55aaf367ebd7d480a3b6665fb9a24626b25
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-30 03:13:02 +0900

    #1102 update:[id].tsx中身のコード戻し

--- front/src/pages/idea/[id].tsx
+++ front/src/pages/idea/[id].tsx
@@ -1,3 +1,54 @@
-import IdeaDetail from '@/components/idea/IdeaDetail'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Image, Title, Group, Button, Loader } from '@mantine/core'
+import { UserIcon, AccompaniedTags, IdeaContent } from '@/components/idea'
+import { useLoggedIn } from '@/components/loginContext'
+import { useGetIdeaQuery, } from '@/lib/generated/client'
+
+const IdeaDetail = () => {
+  const router = useRouter()
+
+  const { loggedIn } = useLoggedIn()
+  const [LSLoggedIn, setLSLoggedIn] = useState(false)
+
+  const { data, loading, error } = useGetIdeaQuery({
+    variables: {
+      id: router.query.id as string,
+    },
+  })
+
+  if (loading) return <Loader color='yellow' />
+
+  useEffect(() => {
+    setLSLoggedIn(localStorage.getItem('loggedIn') === 'true')
+  }, [])
+
+  return (
+    <>
+      {(LSLoggedIn || loggedIn) && (
+        <>
+          <Group position="center">
+            <Image
+              height={50}
+              width={50}
+              radius={50}
+              src={data?.idea.icon}
+              alt="user prof"
+            />
+            <Title>{data?.idea.name}</Title>
+          </Group>
+          <UserIcon />
+          <AccompaniedTags />
+          <IdeaContent />
+          <Group position="center">
+            <Button type="submit" size="lg">
+              送信
+            </Button>
+          </Group>
+        </>
+      )}
+    </>
+  )
+}
 
 export default IdeaDetail

eb26166957940d8132f2d2664a216fd9191a88ca
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-09-30 03:26:49 +0900

    #1102update:useGetIdeaQueryの追加

--- front/src/components/idea/UserIcon.tsx
+++ front/src/components/idea/UserIcon.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { Loader } from '@mantine/core'
+import { useGetIdeaQuery } from '@/lib/generated/client'
 
 export const UserIcon = () => {
-  return <div>UserIcon</div>
+  const router = useRouter()
+  const { data, loading, error } = useGetIdeaQuery({
+    variables: {
+      id: router.query.id as string,
+    },
+  })
+
+  if (loading) return <Loader color="yellow" />
+
+  return (
+    <>
+      <div>{data?.idea.user.icon}</div>
+      <div>{data?.idea.user.name}</div>
+    </>
+  )
 }

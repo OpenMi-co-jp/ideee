4e59f00a9819e05feea594d3649e68452bc57fab
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-02 18:44:44 +0900

    #1102 update:IdeaComtentのスタイリング適応

--- front/src/components/idea/IdeaContent.tsx
+++ front/src/components/idea/IdeaContent.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { Loader } from '@mantine/core'
+import { Group, Loader, Paper, Text } from '@mantine/core'
 import { useGetIdeaQuery } from '@/lib/generated/client'
+import { FormatDate } from '@/utils/common'
 export const IdeaContent = () => {
   const router = useRouter()
   const { data, loading, error } = useGetIdeaQuery({
@@ -12,17 +13,33 @@ export const IdeaContent = () => {
 
   if (loading) return <Loader color="yellow" />
 
+  const createdAt = data?.idea.createdAt ? new Date(data.idea.createdAt) : null;
+
   return (
     <>
-      <div>{data?.idea.background}</div>
-      <div>{data?.idea.goal}</div>
-      <div>{data?.idea.issue}</div>
-      <div>{data?.idea.wishFunction}</div>
-      <div>{data?.idea.hypothesis}</div>
-      <div>{data?.idea.target}</div>
-      <div>{data?.idea.monetize}</div>
-      <div>{data?.idea.similar}</div>
-      <div>{data?.idea.note}</div>
+      <Paper bg="#A5A5A52E" radius="md" p="xl">
+        <Text size="lg">背景</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.background}</Text>
+        <Text size="lg">ゴール</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.goal}</Text>
+        <Text size="lg">イシュー</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.issue}</Text>
+        <Text size="lg">メイン機能</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.wishFunction}</Text>
+        <Text size="lg">仮説(数値的)</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.hypothesis}</Text>
+        <Text size="lg">ターゲット(ペルソナ)</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.target}</Text>
+        <Text size="lg">マネタイズ方法</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.monetize}</Text>
+        <Text size="lg">類似サービス</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.similar}</Text>
+        <Text size="lg">補足</Text>
+        <Text pb="lg" pl='lg'>{data?.idea.note}</Text>
+      </Paper>
+      <Group py="lg" mr="lg" position="right">
+        <Text size="md">{FormatDate(createdAt)}</Text>
+      </Group>
     </>
   )
 }

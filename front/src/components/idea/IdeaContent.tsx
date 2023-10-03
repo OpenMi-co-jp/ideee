477d84b9c6bd391da86d4693d932976d5c705b84
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-04 00:11:12 +0900

    #1102 update:IdeaContentのリファクタリング

--- front/src/components/idea/IdeaContent.tsx
+++ front/src/components/idea/IdeaContent.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { Group, Loader, Paper, Text } from '@mantine/core'
 import { useGetIdeaQuery } from '@/lib/generated/client'
 import { FormatDate } from '@/utils/common'
+import { IdeaSection } from './IdeaSection'
 export const IdeaContent = () => {
   const router = useRouter()
   const { data, loading, error } = useGetIdeaQuery({
@@ -14,46 +15,28 @@ export const IdeaContent = () => {
   if (loading) return <Loader color="yellow" />
 
   const createdAt = data?.idea.createdAt ? new Date(data.idea.createdAt) : null
+  const sections = [
+    { title: '背景', content: data?.idea.background },
+    { title: 'ゴール', content: data?.idea.goal },
+    { title: 'イシュー', content: data?.idea.issue },
+    { title: 'メイン機能', content: data?.idea.wishFunction },
+    { title: '仮説(数値的)', content: data?.idea.hypothesis },
+    { title: 'ターゲット(ペルソナ)', content: data?.idea.target },
+    { title: 'マネタイズ方法', content: data?.idea.monetize },
+    { title: '類似サービス', content: data?.idea.similar },
+    { title: '補足', content: data?.idea.note },
+  ]
 
   return (
     <>
       <Paper bg="#A5A5A52E" radius="md" p="xl">
-        <Text size="lg">背景</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.background}
-        </Text>
-        <Text size="lg">ゴール</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.goal}
-        </Text>
-        <Text size="lg">イシュー</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.issue}
-        </Text>
-        <Text size="lg">メイン機能</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.wishFunction}
-        </Text>
-        <Text size="lg">仮説(数値的)</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.hypothesis}
-        </Text>
-        <Text size="lg">ターゲット(ペルソナ)</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.target}
-        </Text>
-        <Text size="lg">マネタイズ方法</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.monetize}
-        </Text>
-        <Text size="lg">類似サービス</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.similar}
-        </Text>
-        <Text size="lg">補足</Text>
-        <Text pb="lg" pl="lg">
-          {data?.idea.note}
-        </Text>
+        {sections.map((section, index) => (
+          <IdeaSection
+            key={index}
+            title={section.title}
+            content={section.content}
+          />
+        ))}
       </Paper>
       <Group py="lg" mr="lg" position="right">
         <Text size="md">{FormatDate(createdAt)}</Text>

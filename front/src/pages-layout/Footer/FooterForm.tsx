3311b42aa6811dd365fc3a4823696f77f10d0c74
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-07 03:54:54 +0900

    #1178 update:コメントアウトしたもともとのコードの削除

--- front/src/pages-layout/Footer/FooterForm.tsx
+++ front/src/pages-layout/Footer/FooterForm.tsx
@@ -1,5 +1,4 @@
 import {
-  // createStyles,
   Text,
   Container,
   ActionIcon,
@@ -13,108 +12,6 @@ import { IconBrandTwitter, IconNews } from '@tabler/icons-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-// const useStyles = createStyles((theme) => ({
-//   footer: {
-//     marginTop: rem(120),
-//     paddingTop: `calc(${theme.spacing.xl} * 2)`,
-//     paddingBottom: `calc(${theme.spacing.xl} * 2)`,
-//     backgroundColor:
-//       theme.colorScheme === 'dark'
-//         ? theme.colors.dark[6]
-//         : theme.colors.gray[0],
-//     borderTop: `${rem(1)} solid ${
-//       theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-//     }`,
-//   },
-
-//   logo: {
-//     maxWidth: rem(200),
-
-//     [theme.fn.smallerThan('sm')]: {
-//       display: 'flex',
-//       flexDirection: 'column',
-//       alignItems: 'center',
-//     },
-//   },
-
-//   description: {
-//     marginTop: rem(5),
-
-//     [theme.fn.smallerThan('sm')]: {
-//       marginTop: theme.spacing.xs,
-//       textAlign: 'center',
-//     },
-//   },
-
-//   inner: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-
-//     [theme.fn.smallerThan('sm')]: {
-//       flexDirection: 'column',
-//       alignItems: 'center',
-//     },
-//   },
-
-//   groups: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-
-//     [theme.fn.smallerThan('sm')]: {
-//       display: 'none',
-//     },
-//   },
-
-//   wrapper: {
-//     width: rem(160),
-//   },
-
-//   link: {
-//     display: 'block',
-//     color:
-//       theme.colorScheme === 'dark'
-//         ? theme.colors.dark[1]
-//         : theme.colors.gray[6],
-//     fontSize: theme.fontSizes.sm,
-//     paddingTop: rem(3),
-//     paddingBottom: rem(3),
-
-//     '&:hover': {
-//       textDecoration: 'underline',
-//     },
-//   },
-
-//   title: {
-//     fontSize: theme.fontSizes.lg,
-//     fontWeight: 700,
-//     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-//     marginBottom: `calc(${theme.spacing.xs} / 2)`,
-//     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-//   },
-
-//   afterFooter: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     marginTop: theme.spacing.xl,
-//     paddingTop: theme.spacing.xl,
-//     paddingBottom: theme.spacing.xl,
-//     borderTop: `${rem(1)} solid ${
-//       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-//     }`,
-
-//     [theme.fn.smallerThan('sm')]: {
-//       flexDirection: 'column',
-//     },
-//   },
-
-//   social: {
-//     [theme.fn.smallerThan('sm')]: {
-//       marginTop: theme.spacing.xs,
-//     },
-//   },
-// }))
-
 interface FooterLinksProps {
   data: {
     title: string
@@ -123,12 +20,10 @@ interface FooterLinksProps {
 }
 
 export const FooterForm = (footerData: FooterLinksProps) => {
-  // const { classes } = useStyles()
   const { colorScheme } = useMantineColorScheme()
   const groups = footerData.data.map((data) => {
     const links = data.links.map((link, index) => (
       <Link key={index}
-        // className={classes.link} 
         href={link.link}>
         <Text
           style={(theme) => ({
@@ -153,15 +48,11 @@ export const FooterForm = (footerData: FooterLinksProps) => {
     ))
 
     return (
-      <div
-        // className={classes.wrapper} 
-        key={data.title}>
         <Box key={data.title}
           style={{
             width: rem(160),
           }}>
           <Text
-            // className={classes.title}
             style={(theme) => ({
               fontSize: theme.fontSizes.lg,
               fontWeight: 700,
@@ -172,14 +63,11 @@ export const FooterForm = (footerData: FooterLinksProps) => {
           >{data.title}</Text>
           {links}
         </Box>
-      </div>
     )
   })
 
   return (
-    <footer
-    // className={classes.footer}
-    >
+    <footer>
       <Box
         style={(theme) => ({
           marginTop: rem(120),
@@ -195,7 +83,6 @@ export const FooterForm = (footerData: FooterLinksProps) => {
         })}
       >
         <Container
-          // className={classes.inner}
           style={(theme) => ({
             display: 'flex',
             justifyContent: 'space-between',
@@ -206,12 +93,10 @@ export const FooterForm = (footerData: FooterLinksProps) => {
             // },
           })}
         >
-          <div
-          // className={classes.logo}
-          >
             <Box
               style={(theme) => ({
                 maxWidth: rem(200),
+
                 // [theme.fn.smallerThan('sm')]: {
                 //   display: 'flex',
                 //   flexDirection: 'column',
@@ -226,9 +111,9 @@ export const FooterForm = (footerData: FooterLinksProps) => {
                 height={50}
               />
               <Text size="xs" c="dimmed"
-                // className={classes.description}
                 style={(theme) => ({
                   marginTop: rem(5),
+
                   // [theme.fn.smallerThan('sm')]: {
                   //   marginTop: theme.spacing.xs,
                   //   textAlign: 'center',
@@ -238,9 +123,6 @@ export const FooterForm = (footerData: FooterLinksProps) => {
                 あなたの才能で世界をちょっとだけ良くする
               </Text>
             </Box>
-          </div>
-          <div // className={classes.groups}
-          >
             <Box
               style={(theme) => ({
                 display: 'flex',
@@ -253,10 +135,8 @@ export const FooterForm = (footerData: FooterLinksProps) => {
             >
               {groups}
             </Box>
-          </div>
         </Container>
         <Container
-          // className={classes.afterFooter}
           style={(theme) => ({
             display: 'flex',
             justifyContent: 'space-between',
@@ -278,7 +158,6 @@ export const FooterForm = (footerData: FooterLinksProps) => {
 
           <Group
             gap={0}
-            // className={classes.social} 
             wrap='nowrap'
             align="start"
             style={(theme) => ({

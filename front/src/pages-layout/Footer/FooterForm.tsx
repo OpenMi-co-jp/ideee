9558d42d010cf1e06c5c3f38d962b2ea7a4b20af
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-07 17:20:07 +0900

    #1178 update:FooterのStyle更新

--- front/src/pages-layout/Footer/FooterForm.tsx
+++ front/src/pages-layout/Footer/FooterForm.tsx
@@ -6,6 +6,7 @@ import {
   rem,
   Box,
   useMantineColorScheme,
+  Flex,
 } from '@mantine/core'
 
 import { IconBrandTwitter, IconNews } from '@tabler/icons-react'
@@ -23,8 +24,7 @@ export const FooterForm = (footerData: FooterLinksProps) => {
   const { colorScheme } = useMantineColorScheme()
   const groups = footerData.data.map((data) => {
     const links = data.links.map((link, index) => (
-      <Link key={index}
-        href={link.link}>
+      <Link key={index} href={link.link}>
         <Text
           style={(theme) => ({
             display: 'block',
@@ -39,7 +39,6 @@ export const FooterForm = (footerData: FooterLinksProps) => {
             '&:hover': {
               textDecoration: 'underline',
             },
-
           })}
         >
           {link.label}
@@ -48,61 +47,54 @@ export const FooterForm = (footerData: FooterLinksProps) => {
     ))
 
     return (
-        <Box key={data.title}
-          style={{
-            width: rem(160),
-          }}>
-          <Text
-            style={(theme) => ({
-              fontSize: theme.fontSizes.lg,
-              fontWeight: 700,
-              fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-              marginBottom: `calc(${theme.spacing.xs} / 2)`,
-              color: colorScheme === 'dark' ? theme.white : theme.black,
-            })}
-          >{data.title}</Text>
-          {links}
-        </Box>
+      <Box
+        key={data.title}
+        style={{
+          width: rem(160),
+        }}
+      >
+        <Text
+          style={(theme) => ({
+            fontSize: theme.fontSizes.lg,
+            fontWeight: 700,
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            marginBottom: `calc(${theme.spacing.xs} / 2)`,
+            color: colorScheme === 'dark' ? theme.white : theme.black,
+          })}
+        >
+          {data.title}
+        </Text>
+        {links}
+      </Box>
     )
   })
 
   return (
     <footer>
       <Box
+        mt={rem(120)}
         style={(theme) => ({
-          marginTop: rem(120),
           paddingTop: `calc(${theme.spacing.xl} * 2)`,
           paddingBottom: `calc(${theme.spacing.xl} * 2)`,
           backgroundColor:
             colorScheme === 'dark'
               ? theme.colors.dark[6]
               : theme.colors.gray[0],
-          borderTop: `${rem(1)} solid ${colorScheme === 'dark' ?
-            theme.colors.dark[5] : theme.colors.gray[2]
-            }`,
+          borderTop: `${rem(1)} solid ${
+            colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+          }`,
         })}
       >
-        <Container
-          style={(theme) => ({
-            display: 'flex',
-            justifyContent: 'space-between',
-
-            // [theme.fn.smallerThan('sm')]: {
-            // flexDirection: 'column',
-            // alignItems: 'center',
-            // },
-          })}
-        >
-            <Box
-              style={(theme) => ({
-                maxWidth: rem(200),
-
-                // [theme.fn.smallerThan('sm')]: {
-                //   display: 'flex',
-                //   flexDirection: 'column',
-                //   alignItems: 'center',
-                // },
-              })}
+        <Container>
+          <Flex
+            justify="space-between"
+            direction={{ base: 'column', sm: 'row' }}
+            align={{ base: 'center', sm: 'flex-start' }}
+          >
+            <Flex
+              maw={rem(200)}
+              direction={'column'}
+              align={{ base: 'center', sm: 'flex-start' }}
             >
               <Image
                 src="/img/IdeeeLogo.webp"
@@ -110,85 +102,72 @@ export const FooterForm = (footerData: FooterLinksProps) => {
                 width={150}
                 height={50}
               />
-              <Text size="xs" c="dimmed"
-                style={(theme) => ({
-                  marginTop: rem(5),
-
-                  // [theme.fn.smallerThan('sm')]: {
-                  //   marginTop: theme.spacing.xs,
-                  //   textAlign: 'center',
-                  // },
-                })}
+              <Text
+                size="xs"
+                c="dimmed"
+                mt={{ base: 'xs', sm: rem(5) }}
+                ta={{ base: 'center', sm: 'left' }}
               >
                 あなたの才能で世界をちょっとだけ良くする
               </Text>
-            </Box>
-            <Box
-              style={(theme) => ({
-                display: 'flex',
-                flexWrap: 'wrap',
-
-                // [theme.fn.smallerThan('sm')]: {
-                // display: 'none',
-                // },
-              })}
-            >
+            </Flex>
+            <Flex wrap="wrap" display={{ base: 'none', sm: 'flex' }}>
               {groups}
-            </Box>
+            </Flex>
+          </Flex>
         </Container>
         <Container
           style={(theme) => ({
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
             marginTop: theme.spacing.xl,
             paddingTop: theme.spacing.xl,
             paddingBottom: theme.spacing.xl,
-            borderTop: `${rem(1)} solid ${colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-              }`,
-
-            // [theme.fn.smallerThan('sm')]: {
-            // flexDirection: 'column',
-            // },
+            borderTop: `${rem(1)} solid ${
+              colorScheme === 'dark'
+                ? theme.colors.dark[4]
+                : theme.colors.gray[2]
+            }`,
           })}
         >
-          <Text c="dimmed" size="sm">
-            © 2022 ideee.tech All rights reserved.
-          </Text>
-
-          <Group
-            gap={0}
-            wrap='nowrap'
-            align="start"
-            style={(theme) => ({
-              // [theme.fn.smallerThan('sm')]: {
-              //   marginTop: theme.spacing.xs,
-              // },
-            })}
+          <Flex
+            justify="space-between"
+            align="center"
+            direction={{ base: 'column', sm: 'row' }}
           >
-            <ActionIcon size="lg"
-              c="dimmed"
-              style={(theme) => ({
-                backgroundColor:
-                  colorScheme === 'dark'
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-              })}
+            <Text c="dimmed" size="sm">
+              © 2022 ideee.tech All rights reserved.
+            </Text>
+            <Group
+              gap={0}
+              wrap="nowrap"
+              align="start"
+              mt={{ base: 'xs', sm: 0 }}
             >
-              <IconBrandTwitter size="1.05rem" stroke={1.5} />
-            </ActionIcon>
-            <ActionIcon size="lg"
-              c="dimmed"
-              style={(theme) => ({
-                backgroundColor:
-                  colorScheme === 'dark'
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-              })}
-            >
-              <IconNews size="1.05rem" stroke={1.5} />
-            </ActionIcon>
-          </Group>
+              <ActionIcon
+                size="lg"
+                c="dimmed"
+                style={(theme) => ({
+                  backgroundColor:
+                    colorScheme === 'dark'
+                      ? theme.colors.dark[6]
+                      : theme.colors.gray[0],
+                })}
+              >
+                <IconBrandTwitter size="1.05rem" stroke={1.5} />
+              </ActionIcon>
+              <ActionIcon
+                size="lg"
+                c="dimmed"
+                style={(theme) => ({
+                  backgroundColor:
+                    colorScheme === 'dark'
+                      ? theme.colors.dark[6]
+                      : theme.colors.gray[0],
+                })}
+              >
+                <IconNews size="1.05rem" stroke={1.5} />
+              </ActionIcon>
+            </Group>
+          </Flex>
         </Container>
       </Box>
     </footer>

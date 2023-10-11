56db2afdf4f715e3ca982d59a15e43072b89f397
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-11 17:58:28 +0900

    #1178 upate:エラー修正及びリファクタリング

--- front/src/components/features/TopVision.tsx
+++ front/src/components/features/TopVision.tsx
@@ -1,117 +1,71 @@
 // import Image from 'next/image'
-import { Title, Text, Button } from '@mantine/core'
+import { Title, Text, Button, Box, BackgroundImage } from '@mantine/core'
 import Link from 'next/link'
 
 export const TopVision = () => {
   // TODO: 現在のユーザー数を設定予定
-  // TODO: BackgroundImageが横幅いっぱいにならない
   const userNum = 937
   return (
-    <Box>
+    <div style={{
+      position: 'relative',
+      display: 'flex', 
+      justifyContent: 'center', 
+    }}>
       <BackgroundImage
         src={'/img/top_vision_image.webp'}
+        h='350px'
+        w='100vw'
         style={{
-          top: 150,
-          maxHeight: '500px',
+          position: 'absolute',
+          objectFit: 'cover',
           zIndex: -300,
-
-          '@media (max-width: 649px)': {
-            maxHeight: '250px',
-            objectFit: 'cover',
-          },
-          '@media (max-width: 1000px) and (min-width:650px)': {
-            maxHeight: '350px',
-          },
+        }} />
+      <Box
+        w='400px'
+        pt='50px'
+        mr="auto"
+        style={{
+          textAlign: 'center',
         }}
       >
         <Box
+          pb='55px'
           style={{
-            width: '400px',
-            paddingTop: '150px',
-            paddingBottom: '130px',
-            textAlign: 'center',
-
-            '@media (max-width: 649px)': {
-              width: 'auto',
-              paddingTop: '70px',
-              paddingBottom: '30px',
-            },
-            '@media (max-width: 1000px) and (min-width:650px)': {
-              paddingTop: '100px',
-            },
+            display: 'flex',
+            flexFlow: 'column',
           }}
         >
-          <Box
-            style={{
-              display: 'flex',
-              flexFlow: 'column',
-              paddingBottom: '55px',
-
-              '@media (max-width: 649px)': {
-                paddingBottom: '40px',
-                textShadow:
-                  '8px 8px 12px white, -8px 8px 12px white, 8px -8px 12px white, -8px -8px 12px white',
-              },
-              '@media (max-width: 1000px) and (min-width:650px)': {
-                paddingBottom: '40px',
-              },
-            }}
+          <Title
+            c='#FF862F'
+            fz={{base: '32px', md: '34px'}}
           >
-            <Title
-              style={{
-                color: '#FF862F',
-                '@media (max-width: 649px)': {
-                  fontSize: '26px',
-                },
-                '@media (max-width: 1000px) and (min-width:650px)': {
-                  fontSize: '28px',
-                },
-              }}
-            >
-              あなたのアイデアを
-            </Title>
-            <Title
-              style={{
-                color: '#FF862F',
-                '@media (max-width: 649px)': {
-                  fontSize: '26px',
-                },
-                '@media (max-width: 1000px) and (min-width:650px)': {
-                  fontSize: '28px',
-                },
-              }}
-            >
-              エンジニアと盛り上げる
-            </Title>
-          </Box>
-          <Link href="/about">
-            <Button
-              variant="gradient"
-              gradient={{ from: 'orange', to: 'yellow' }}
-            >
-              IDEEEについて｜⇢
-            </Button>
-          </Link>
-          <Text
-            style={{
-              paddingTop: '35px',
-              color: '#818181',
-              fontSize: '20px',
-              fontWeight: 700,
-
-              '@media (max-width: 649px)': {
-                fontSize: '16px',
-              },
-              '@media (max-width: 1000px) and (min-width:650px)': {
-                fontSize: '18px',
-              },
-            }}
+            あなたのアイデアを
+          </Title>
+          <Title
+            c='#FF862F'
+            fz={{base: '32px', md: '34px'}}
           >
-            現在のユーザー数:{' '}
-            <b style={{ color: '#FF862F', fontSize: '2rem' }}>{userNum}</b>人
-          </Text>
+            エンジニアと盛り上げる
+          </Title>
         </Box>
-      </BackgroundImage>
-    </Box>
+        <Link href="/about">
+          <Button
+            variant="gradient"
+            gradient={{ from: 'orange', to: 'yellow' }}
+          >
+            IDEEEについて｜⇢
+          </Button>
+        </Link>
+        <Text
+          c='#818181'
+          pt='30px'
+          fz='20px'
+          fw='bold'
+        >
+          現在のユーザー数:{' '}
+          <b style={{ color: '#FF862F', fontSize: '2rem' }}>{userNum}</b>人
+        </Text>
+      </Box>
+    </div>
   )
 }

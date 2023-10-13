28e1769621ffbc4f55f05d39c11577fc33ba3fea
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-13 18:13:40 +0900

    #1178 update:下部画像zindex修正、レスポンシブ修正途中

--- front/src/components/features/Philosophy.tsx
+++ front/src/components/features/Philosophy.tsx
@@ -64,7 +64,7 @@ export const Philosophy = () => {
               ta="center"
               pb="5px"
               lts="3px"
-              style={{ zIndex: 500 }}
+              style={{ zIndex: 100 }}
             >
               あなたの「困ってる」を形に
             </Text>
@@ -74,7 +74,7 @@ export const Philosophy = () => {
               ta="center"
               pb="5px"
               lts="3px"
-              style={{ zIndex: 500 }}
+              style={{ zIndex: 100 }}
             >
               コードで社会に貢献してみませんか？
             </Text>
@@ -89,7 +89,7 @@ export const Philosophy = () => {
               mt={{ base: '100px', md: '150px' }}
               style={{
                 margin: '0 auto',
-                zIndex: 500,
+                zIndex: 100,
               }}
             />
           </Flex>
@@ -97,7 +97,8 @@ export const Philosophy = () => {
             w={{ base: '285px', sm: '333px', md: '500px' }}
             h={{ base: '250px', sm: '290px', md: '435px' }}
             top={0}
-            left={{ base: '-60px', sm: 0 }}
+            // left={{ base: '-60px', sm: 0 }}
+            left={{ base: '-140px', xs: '0px' ,md: '0px' }}
             bg={'rgba(239, 213, 120, 0.49)'}
             style={{
               borderRadius: '50%',
@@ -109,7 +110,8 @@ export const Philosophy = () => {
             h={{ base: '250px', sm: '290px', md: '435px' }}
             bg={'rgba(215, 145, 145, 0.50)'}
             top={50}
-            left={{ base: '180px', sm: '300px', md: '450px' }}
+            // left={{ base: '180px', sm: '300px', md: '450px' }}
+            left={{ base: '130px', sm: '300px', md: '450px' }}
             style={{
               borderRadius: '50%',
               position: 'absolute',

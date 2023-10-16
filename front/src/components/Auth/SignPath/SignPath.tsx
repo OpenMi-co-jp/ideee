ce3ccd4d6931f4a42007a35ae4bb458d4ea1ecf0
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-17 00:25:46 +0900

    #1178 update: 下部ログインして続きを確認部分の修正

--- front/src/components/Auth/SignPath/SignPath.tsx
+++ front/src/components/Auth/SignPath/SignPath.tsx
@@ -1,47 +1,46 @@
-import { Title, Card, Image, Button, Grid } from '@mantine/core'
+import { Title, Card, Image, Button, Grid, Flex } from '@mantine/core'
 import Link from 'next/link'
 import type { CustomNextPage } from 'next'
 
 export const SignPath: CustomNextPage = () => {
   return (
-    <Card padding="xl">
       <Grid grow gutter="xs">
-        <Grid.Col span={12} offset={6}>
+        <Grid.Col span={6} >
           <Image
             mx="auto"
-            height="15rem"
+            height="240px"
             fit="contain"
             src="/img/userAuth.webp"
             alt="user auth image"
-            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
           />
         </Grid.Col>
-        <Grid.Col span={12} offset={6}>
+        <Grid.Col span={6} >
           <Card radius="0.5rem" shadow="xl">
             <Title order={3} m="lg">
               ログインして続きを確認
             </Title>
-            <Link href="/user/sign_up">
-              <Button
-                m="lg"
-                variant="gradient"
-                gradient={{ from: 'yellow', to: 'orange' }}
-              >
-                ユーザー登録
-              </Button>
-            </Link>
-            <Link href="/user/sign_in">
-              <Button
-                m="lg"
-                variant="gradient"
-                gradient={{ from: 'orange', to: 'yellow' }}
-              >
-                ログイン
-              </Button>
-            </Link>
+            <Flex>
+              <Link href="/user/sign_up">
+                <Button
+                  m="lg"
+                  variant="gradient"
+                  gradient={{ from: 'yellow', to: 'orange' }}
+                >
+                  ユーザー登録
+                </Button>
+              </Link>
+              <Link href="/user/sign_in">
+                <Button
+                  m="lg"
+                  variant="gradient"
+                  gradient={{ from: 'orange', to: 'yellow' }}
+                >
+                  ログイン
+                </Button>
+              </Link>
+            </Flex>
           </Card>
         </Grid.Col>
       </Grid>
-    </Card>
   )
 }

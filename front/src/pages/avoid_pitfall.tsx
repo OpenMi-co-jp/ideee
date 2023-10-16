cc771ccf38f3c98324c5928513d2a236c0eebfe4
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-17 01:19:59 +0900

    #1178 fix:ログイン状態ローカル参照のロジック部分がなかったので追加

--- front/src/pages/avoid_pitfall.tsx
+++ front/src/pages/avoid_pitfall.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Container, Paper } from '@mantine/core'
 import { useLoggedIn } from '@/components/loginContext'
 import { SignPath } from '@/components/Auth/SignPath'
@@ -6,6 +7,11 @@ import { HiddenContent } from '@/components/avoidPitfall'
 
 function AvoidPitfall() {
   const { loggedIn } = useLoggedIn()
+  const [LSLoggedIn, setLSLoggedIn] = useState(false)
+  useEffect(() => {
+    setLSLoggedIn(localStorage.getItem('loggedIn') === 'true')
+  }, [])
+
 
   return (
     <Container>
@@ -13,7 +19,7 @@ function AvoidPitfall() {
         <Container
           style={{
             height: '100%',
-            ...(!loggedIn
+            ...(!(loggedIn|| LSLoggedIn)
               ? {
                   WebkitMaskImage:
                     'linear-gradient(to bottom, transparent, white 0%, white 0%, transparent)',
@@ -27,7 +33,7 @@ function AvoidPitfall() {
         </Container>
 
         {(() => {
-          if (loggedIn) {
+          if (loggedIn|| LSLoggedIn) {
             return <HiddenContent />
           } else {
             return <SignPath />

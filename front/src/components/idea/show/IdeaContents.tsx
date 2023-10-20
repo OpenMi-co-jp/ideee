497c36d3ccf2d154a3c6b4376f31defc6676413a
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-20 21:34:55 +0900

    #1178 add:非ログイン時にIdeaの一覧の一部を見せるようにする

--- front/src/components/idea/show/IdeaContents.tsx
+++ front/src/components/idea/show/IdeaContents.tsx
@@ -1,25 +1,43 @@
+import { useState, useEffect } from 'react'
+import { useLoggedIn } from '@/components/loginContext'
 import { Button, Paper, Anchor } from '@mantine/core'
 import { IconApps, IconBrandGithub } from '@tabler/icons-react'
 import { IdeaContentSet } from './IdeaContentSet'
 import type { GetIdeaQuery } from '@/lib/generated/client'
 import { useIdea } from '@/context/IdeaContext'
 
-const getSections = (idea: GetIdeaQuery['idea']) => [
-  { label: '背景', content: idea?.background },
-  { label: 'ゴール', content: idea?.goal },
-  { label: 'イシュー', content: idea?.issue },
-  { label: 'メイン機能', content: idea?.wishFunction },
-  { label: '仮説(数値的)', content: idea?.hypothesis },
-  { label: 'ターゲット(ペルソナ)', content: idea?.target },
-  { label: 'マネタイズ方法', content: idea?.monetize },
-  { label: '類似サービス', content: idea?.similar },
-  { label: '補足', content: idea?.note },
-]
+const getSections = (idea: GetIdeaQuery['idea'], isLogin: boolean) => {
+  if (isLogin) {
+    return [
+      { label: '背景', content: idea?.background },
+      { label: 'ゴール', content: idea?.goal },
+      { label: 'イシュー', content: idea?.issue },
+      { label: 'メイン機能', content: idea?.wishFunction },
+      { label: '仮説(数値的)', content: idea?.hypothesis },
+      { label: 'ターゲット(ペルソナ)', content: idea?.target },
+      { label: 'マネタイズ方法', content: idea?.monetize },
+      { label: '類似サービス', content: idea?.similar },
+      { label: '補足', content: idea?.note },
+    ]
+  } else {
+    return [
+      { label: '背景', content: idea?.background },
+      { label: 'ゴール', content: idea?.goal },
+    ]
+  }
+}
 
 export const IdeaContents = () => {
+  const { loggedIn } = useLoggedIn()
+  const [LSLoggedIn, setLSLoggedIn] = useState(false)
+
   const idea = useIdea()
 
-  const sections = getSections(idea)
+  useEffect(() => {
+    setLSLoggedIn(localStorage.getItem('loggedIn') === 'true')
+  }, [])
+
+  const sections = getSections(idea, LSLoggedIn)
 
   return (
     <>
@@ -27,33 +45,37 @@ export const IdeaContents = () => {
         {sections.map((section, index) => (
           <IdeaContentSet key={index} {...section} />
         ))}
-        {idea?.productUrl && (
-          <Anchor href={idea?.productUrl} target="_blank">
-            <Button
-              variant="gradient"
-              gradient={{ from: 'green', to: 'blue' }}
-              radius="xl"
-              size="md"
-              m="md"
-              leftIcon={<IconApps />}
-              mt="lg"
-            >
-              アプリを確認
-            </Button>
-          </Anchor>
-        )}
-        {idea?.githubUrl && (
-          <Anchor href={idea?.githubUrl} target="_blank">
-            <Button
-              color="dark"
-              radius="xl"
-              size="md"
-              leftIcon={<IconBrandGithub />}
-              mt="xl"
-            >
-              GitHubを確認
-            </Button>
-          </Anchor>
+        {(loggedIn || LSLoggedIn) && (
+          <>
+            {idea?.productUrl && (
+              <Anchor href={idea?.productUrl} target="_blank">
+                <Button
+                  variant="gradient"
+                  gradient={{ from: 'green', to: 'blue' }}
+                  radius="xl"
+                  size="md"
+                  m="md"
+                  leftIcon={<IconApps />}
+                  mt="lg"
+                >
+                  アプリを確認
+                </Button>
+              </Anchor>
+            )}
+            {idea?.githubUrl && (
+              <Anchor href={idea?.githubUrl} target="_blank">
+                <Button
+                  color="dark"
+                  radius="xl"
+                  size="md"
+                  leftIcon={<IconBrandGithub />}
+                  mt="xl"
+                >
+                  GitHubを確認
+                </Button>
+              </Anchor>
+            )}
+          </>
         )}
       </Paper>
     </>

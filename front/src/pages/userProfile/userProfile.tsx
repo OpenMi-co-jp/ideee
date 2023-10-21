8897b6ac0156c3c9628c1aef395c9e2918e6731e
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-10-21 09:48:04 +0900

    add: #1033 ユーザー詳細ページのインデックスを追加

--- front/src/pages/userProfile/userProfile.tsx
+++ front/src/pages/userProfile/userProfile.tsx
@@ -1,50 +1,26 @@
-import { Card, Title, Text, Center } from '@mantine/core'
-import React, { useState } from 'react';
-
-function userDetail() {
-    const [uploadedImage, setUploadedImage] = useState(iconRef);
-    const [submittedData, setSubmittedData] = useState(null);
-        const onSubmit = (data) => {
-            console.log('------submit------');
-            setSubmittedData(data);
-        }
+import { Box } from '@mantine/core'
+import { UserIcon } from '@/components/user/userDetails/userIcon'
+import { UserName } from '@/components/user/userDetails/userName'
+import { UserEmail } from '@/components/user/userDetails/userEmail'
+import { UserDescription } from '@/components/user/userDetails/userDescription'
+import { GithubProfileLink } from '@/components/user/link/gitHubProfileLink'
+import { TwitterProfileLink } from '@/components/user/link/twitterProfileLink'
+import { OtherLink } from '@/components/user/link/otherLink'
 
 
-    const handleFileChange = (event) => {
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                setUploadedImage(e.target.result);
-            };
-            reader.readAsDataURL(file);
-        }
-    }
-
+export function userDetail() {
     
     return (
-<Card miw="285" w="75%">
-    <Center>
-    <Title>ユーザー詳細</Title>
-    </Center>
-
-
-    <Title>ユーザー名</Title>
-    <Text>{submittedData.name}</Text>
-
-    <Title>プロフィール</Title>
-    <Text>{submittedData.profile}</Text>
-
-     <Title>X(旧twitter)リンク</Title>
-     <Text>{submittedData.twitterId}</Text>
-
-
-     <Title>GitHubリンク</Title>
-     <Title>{submittedData.githubId}</Title>
-
-
-     <Title>サイトURL</Title>
-     <Text>{submittedData.siteUrl}</Text>
-</Card>
+        <>
+           <Box>
+            <UserIcon/>
+            <UserName/>
+            <UserEmail/>
+            <UserDescription/>
+            <GithubProfileLink/>
+            <TwitterProfileLink/>
+            <OtherLink/>
+           </Box>
+        </>
     )
 }
-export default userDetail

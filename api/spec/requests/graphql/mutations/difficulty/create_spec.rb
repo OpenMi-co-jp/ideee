c05c3387c4193975ed5c9fb1cd4ec58f94159b9a
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-10-25 04:13:14 +0900

    Revert "#1100　add:param修正"
    This reverts commit c31aab0bfd9eb60b88b0b657afd13caeb3232a3c.

--- api/spec/requests/graphql/mutations/difficulty/create_spec.rb
+++ api/spec/requests/graphql/mutations/difficulty/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::Difficulty::Create do
-  subject(:graphql_post) { post graphql_path, params: { query: query, variables: variables.to_json }, headers: tokens }
+  subject(:graphql_post) { post graphql_path, params: { query:, variables: variables.to_json }, headers: tokens }
 
   let(:user)   { create(:user) }
   let(:tokens) { sign_in(user) }

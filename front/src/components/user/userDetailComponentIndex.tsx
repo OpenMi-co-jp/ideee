3f2a986f9a48dbf6444b57b716ced9dbdd613129
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-10-28 12:18:20 +0900

    add: #1033 ユーザー詳細データを表示する詳細データをまとめるコンポーネントを追加

---
+++ front/src/components/user/userDetailComponentIndex.tsx
@@ -0,0 +1,27 @@
+import UserName from '@/components/user/show/name'
+import GithubProfileLink from '@/components/user/link/gitHubProfileLink'
+import OtherLink from '@/components/user/link/otherLink'
+import TwitterProfileLink from '@/components/user/link/twitterProfileLink'
+import { Box, Container, Space } from '@mantine/core'
+
+export default function UserDetailComponentIndex() {
+return (
+<Box>
+<UserName />
+<div></div>
+<div>コントリビューション</div>
+<div>アイデアマンorエンジニア</div>
+
+<Container
+style={{ display: 'flex', flexDirection: 'row', gap: 'sm' }}
+mt="0.4rem"
+>
+<GithubProfileLink />
+<Space w="xs" />
+<TwitterProfileLink />
+<Space w="xs" />
+<OtherLink />
+</Container>
+</Box>
+)
+}

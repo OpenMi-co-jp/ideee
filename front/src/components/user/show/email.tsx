4b5093ef19e5b895a7229c986d8b5ffbf2b8c09f
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-10-28 12:04:11 +0900

    fix: #1034 ファイル名を変更、アイコンにホバーアクションを追加

---
+++ front/src/components/user/show/email.tsx
@@ -0,0 +1,18 @@
+//import { useUser } from '@/context/userProfileContext';
+import { Text, Anchor, Paper } from '@mantine/core';
+import { IconPaperBag } from '@tabler/icons-react';
+
+export default function UserEmail() {
+    //const user = useUser();
+    //if (!user || !user.email) {
+        //return null;
+    //}
+
+    return(
+        <Paper bg="transparent" ml="1rem">
+        <Text>mail:</Text>
+        <Anchor>
+        </Anchor>
+        </Paper>
+    )
+}

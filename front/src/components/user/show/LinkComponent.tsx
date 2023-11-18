64379bd3ff848be233cdcc5d2737293238752d56
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-11-19 00:43:34 +0900

    fix: #1033 各リンクでuseHoverを持たせるように修正

--- front/src/components/user/show/LinkComponent.tsx
+++ front/src/components/user/show/LinkComponent.tsx
@@ -7,94 +7,88 @@ import {
 import { useHover } from '@mantine/hooks'
 import { useUser } from '@/context/userProfileContext'
 
-export default function LinkComponent() {
-  const LinkTypes = {
-    TWITTER: 'twitterId',
-    GITHUB: 'githubId',
-    SITEURL: 'siteUrl',
-  }
-
+const GithubLink = () => {
   const user = useUser()
+  const { hovered, ref } = useHover()
 
-  const renderLink = (type: string) => {
-    const { hovered, ref } = useHover()
-    switch (type) {
-      case LinkTypes.GITHUB:
-        return (
-          <ActionIcon variant="subtle" color="transparent" mr="-0.3rem">
-            <Anchor
-              href={user?.githubId ? `https://github.com/${user.githubId}` : ''}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div ref={ref}>
-                <IconBrandGithub
-                  color="black"
-                  style={
-                    hovered
-                      ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
-                      : {}
-                  }
-                />
-              </div>
-            </Anchor>
-          </ActionIcon>
-        )
+  return (
+    <ActionIcon variant="subtle" color="transparent" mr="-0.4rem">
+      <Anchor
+        href={user?.githubId ? `https://github.com/${user.githubId}` : ''}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div ref={ref}>
+          <IconBrandGithub
+            color="black"
+            style={
+              hovered
+                ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
+                : {}
+            }
+          />
+        </div>
+      </Anchor>
+    </ActionIcon>
+  )
+}
 
-      case LinkTypes.TWITTER:
-        return (
-          <ActionIcon variant="subtle" color="transparent" mr="-0.3rem">
-            <Anchor
-              href={
-                user?.twitterId ? `https://twitter.com/${user.twitterId}` : ''
-              }
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div ref={ref}>
-                <IconBrandTwitter
-                  color="black"
-                  style={
-                    hovered
-                      ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
-                      : {}
-                  }
-                />
-              </div>
-            </Anchor>
-          </ActionIcon>
-        )
+const TwitterLink = () => {
+  const user = useUser()
+  const { hovered, ref } = useHover()
+  return (
+    <ActionIcon variant="subtle" color="transparent" mr="-0.4rem">
+      <Anchor
+        href={user?.twitterId ? `https://twitter.com/${user.twitterId}` : ''}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div ref={ref}>
+          <IconBrandTwitter
+            color="black"
+            style={
+              hovered
+                ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
+                : {}
+            }
+          />
+        </div>
+      </Anchor>
+    </ActionIcon>
+  )
+}
 
-      case LinkTypes.SITEURL:
-        return (
-          <ActionIcon variant="subtle" color="transparent">
-            <Anchor
-              href={user?.siteUrl ? `${user.siteUrl}` : ''}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div ref={ref}>
-                <IconLink
-                  color="black"
-                  style={
-                    hovered
-                      ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
-                      : {}
-                  }
-                />
-              </div>
-            </Anchor>
-          </ActionIcon>
-        )
-      default:
-        return null
-    }
-  }
+const SiteUrl = () => {
+  const user = useUser()
+  const { hovered, ref } = useHover()
+  return (
+    <ActionIcon variant="subtle" color="transparent">
+      <Anchor
+        href={user?.siteUrl ? `${user.siteUrl}` : ''}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div ref={ref}>
+          <IconLink
+            color="black"
+            style={
+              hovered
+                ? { transform: 'scale(1.2)', transition: 'all 0.05s' }
+                : {}
+            }
+          />
+        </div>
+      </Anchor>
+    </ActionIcon>
+  )
+}
+
+export default function LinkComponent() {
   return (
     <Group ml="-6px">
-      {renderLink(LinkTypes.GITHUB)}
-      {renderLink(LinkTypes.TWITTER)}
-      {renderLink(LinkTypes.SITEURL)}
+      <GithubLink />
+      <TwitterLink />
+      <SiteUrl />
     </Group>
   )
 }

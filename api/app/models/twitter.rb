409dee67b0dead815dba91ab584ea1d02d5bd35d
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-11-18 18:23:14 +0900

    #1209 update:&を付与

--- api/app/models/twitter.rb
+++ api/app/models/twitter.rb
@@ -29,7 +29,7 @@ class Twitter
 
     user = User.find(idea.user_id)
     twitter_user = user.twitter_id.present? ? "@#{user.twitter_id} " : ''
-    hashtags = ['ideee', *idea.idea_tags.pluck(:name)].map { "##{_1}" }.join(' ')
+    hashtags = ['ideee', *idea.idea_tags&.pluck(:name)].map { "##{_1}" }.join(' ')
 
     json_payload = { "text": "【新しいアイデア投稿】\n#{idea.name}\n#{twitter_user}#{hashtags}\n#{url}" }
 

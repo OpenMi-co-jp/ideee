8d3ab6921192cb5a5f96c92ea9067b27da7c4ff9
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-11-18 16:00:23 +0900

    #1185 update:GraphQLへcurrentyuserの追加

--- api/app/graphql/mutations/like/create.rb
+++ api/app/graphql/mutations/like/create.rb
@@ -3,7 +3,7 @@ module Mutations
     graphql_name 'CreateLike'
 
     argument :likable_type, String, required: true, description: 'いいねしたオブジェクトタイプ'
-    argument :user_id, Integer, required: true, description: 'ユーザーID'
+    # argument :user_id, Integer, required: true, description: 'ユーザーID'
     argument :likable_id, Integer, required: true, description: 'オブジェクトID'
 
     field :like, Types::LikeType, null: false, description: 'いいねのオブジェクト'
@@ -12,7 +12,7 @@ module Mutations
     def resolve(**args)
       like = ::Like.new(
         likable_type: args[:likable_type],
-        user_id: args[:user_id],
+        user_id: context[:current_user].id,
         likable_id: args[:likable_id]
       )
       like.save!

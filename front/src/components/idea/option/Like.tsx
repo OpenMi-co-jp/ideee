a9e236294cea2c4d606fa49aadecfdff317fc3cb
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-11-19 23:41:21 +0900

    #1185 update:フロント側いいねの作成削除ロジック追加

--- front/src/components/idea/option/Like.tsx
+++ front/src/components/idea/option/Like.tsx
@@ -1,12 +1,41 @@
 import { IconHeartFilled, IconHeart } from '@tabler/icons-react'
 import { Button } from '@mantine/core'
 import { useToggle } from '@mantine/hooks'
+import {
+  useCreateLikeMutation,
+  useDestroyLikeMutation,
+} from '@/lib/generated/client'
+import { useRouter } from 'next/router'
 
 export const Like = () => {
   const [like, toggleLike] = useToggle([false, true])
+  const { id } = useRouter().query
+
+  const [createLike, createResult] = useCreateLikeMutation({
+    variables: {
+      input: {
+        likableType: 'Idea',
+        likableId: Number(id),
+      },
+    },
+  })
+  const [destroyLike, destroyResult] = useDestroyLikeMutation({
+    variables: {
+      input: {
+        id: id as string,
+      },
+    },
+  })
+
+  const createLikeHandler = () => {
+    createLike()
+      .then(() => {
+        console.log(createResult.data?.createLike)
+      })
+  }
 
   return (
-    <Button onClick={() => toggleLike()} variant="transparent" px="xs">
+    <Button onClick={() => createLikeHandler()} variant="transparent" px="xs">
       {like ? (
         <IconHeartFilled style={{ color: 'black' }} />
       ) : (

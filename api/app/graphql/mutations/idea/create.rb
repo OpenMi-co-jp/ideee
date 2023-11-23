8bb2a4cf5e2da82f04e43f509a37186089026a11
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-11-23 22:38:11 +0900

    add:本番環境のみでPOST

--- api/app/graphql/mutations/idea/create.rb
+++ api/app/graphql/mutations/idea/create.rb
@@ -41,7 +41,7 @@ module Mutations
         # user_id: context[:current_user].id
       )
       idea.save!
-      if idea.save
+      if idea.save && ApplicationHelper.full_url == 'https://www.ideee.tech'
         url = "#{Rails.application.config.host}/ideas/#{idea.id}"
         TwitterJob::Tweet.new.perform(idea, url)
       end

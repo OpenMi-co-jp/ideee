ed459f2240f67ca8a1a81f46255c00e2a552821b
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-11-26 00:40:24 +0900

    ファイル名変更

--- front/src/components/user/show/iconComponent.tsx
+++ front/src/components/user/show/iconComponent.tsx
@@ -1,20 +1,29 @@
-import { Image } from '@mantine/core'
+import { Image, Card, rem } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 import { useUser } from '@/context/userProfileContext'
 
 export const IconComponent = () => {
+  const isMobile = useMediaQuery(`(max-width: ${rem(450)})`)
   const user = useUser()
   const icon = user?.icon
 
-  return (    
-    <>
+  return (
+    <Card
+      style={{
+        display: 'flex',
+        justifyContent: isMobile ? '' : 'flex-start',
+        borderRadius: '50%',
+      }}
+      ml={isMobile ? '-4.5%' : ''}
+      mb="lg"
+      w="16%"
+      h="16%"
+      miw="6rem"
+    >
       <Image
         src={icon ? icon : '/img/undefined_user_icon.webp'}
         alt="ユーザーアイコン"
-        my="lg"
-        w="16%"
-        h="16%"
-        miw="6rem"
       />
-    </>
+    </Card>
   )
 }

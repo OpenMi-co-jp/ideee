15eaa3a2d828cd57e6b137e8a5ff60a4b8d158da
naru20181117 <t.naruhiro.1026@gmail.com>
2023-11-25 21:02:56 +0900

    #1082 ユーザー作成時のSlack通知を設定

--- api/app/controllers/users/registrations_controller.rb
+++ api/app/controllers/users/registrations_controller.rb
@@ -30,7 +30,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render json: { success: false, errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
 
-    # Slack::SendNewJob.perform_later(resource, user_url(resource.id)) if Rails.env.production? && resource.present?
+    if Rails.env.production? && resource.present?
+      user_url = Rails.application.config.frontend_url + '/users/' + resource.id.to_s
+      Slack::SendNewJob.perform_later(resource, user_url)
+    end
   end
 
   # GET /resource/edit

7fbb2e2764f34bcd8728c517915b48bf92e8b460
naru20181117 <t.naruhiro.1026@gmail.com>
2023-11-25 21:02:16 +0900

    #1082 OmniauthのUI設定 & 軽微な修正

--- front/src/components/Auth/OmniAuth/OmniAuth.tsx
+++ front/src/components/Auth/OmniAuth/OmniAuth.tsx
@@ -1,14 +1,28 @@
-import { Button } from '@mantine/core'
+import { Button, Stack } from '@mantine/core'
 import { handleSignIn } from './hooks'
+import { IconBrandX, IconBrandGoogleFilled } from '@tabler/icons-react'
 
 export const OmniAuth = () => {
   const onGoogleLogin = () => handleSignIn({ provider: 'google_oauth2' })
   const onTwitterLogin = () => handleSignIn({ provider: 'twitter' })
 
   return (
-    <>
-      <Button onClick={onGoogleLogin}>Google ログインpost</Button>
-      <Button onClick={onTwitterLogin}>Twitter ログインpost</Button>
-    </>
+    <Stack my="xl" gap="lg">
+      <Button
+        leftSection={<IconBrandGoogleFilled />}
+        onClick={onGoogleLogin}
+        variant="gradient"
+        gradient={{ from: '#4285f4', to: '#34a853', deg: 90 }}
+      >
+        Google ログイン
+      </Button>
+      <Button
+        leftSection={<IconBrandX />}
+        onClick={onTwitterLogin}
+        style={{ backgroundColor: '#0f1419' }}
+      >
+        Twitter ログイン
+      </Button>
+    </Stack>
   )
 }
--- front/src/components/Auth/SignUp/SignUpForm.tsx
+++ front/src/components/Auth/SignUp/SignUpForm.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import { PasswordForm, TextForm } from '../../ReactFormSet'
 import { handleSignUp } from './hooks'
 import type { CustomNextPage } from 'next'
+import { OmniAuth } from '@/components/Auth/OmniAuth'
 
 type SignUpFormValues = {
   email: string
@@ -13,7 +14,6 @@ type SignUpFormValues = {
 
 export const SignUpForm: CustomNextPage = () => {
   const confirmSuccessUrl = process.env.NEXT_PUBLIC_FRONT_URL
-  console.log(confirmSuccessUrl)
   const form = useForm<SignUpFormValues>({
     defaultValues: {
       email: '',
@@ -31,6 +31,7 @@ export const SignUpForm: CustomNextPage = () => {
         <Title order={2} mb={30}>
           ユーザー登録
         </Title>
+        <OmniAuth />
         <TextForm form={form} name="email" label="メールアドレス" required />
         <PasswordForm form={form} name="password" label="パスワード" required />
         <PasswordForm

1a451675ffc6ca8f05cd86edbc1bc7dbcffe28d8
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-11-26 13:57:05 +0900

    update: #1033 スマホ版では要素が中央寄せになるように修正

--- front/src/components/user/show/Profile.tsx
+++ front/src/components/user/show/Profile.tsx
@@ -1,26 +1,38 @@
 import { LinkComponent } from '@/components/user/show'
 import { useUser } from '@/context/userProfileContext'
-import { Divider, Flex, Paper, Text } from '@mantine/core'
+import { Box, Divider, Flex, Paper, Text } from '@mantine/core'
 import { IconComponent } from '@/components/user/show/iconComponent'
+import { useMediaQuery } from '@mantine/hooks'
 
 export const Profile = () => {
   const user = useUser()
+  const matches = useMediaQuery('(min-width: 26em)');
 
   return (
     <>
-      <IconComponent />
-      <Flex direction={'column'} gap="xs">
-        <Text style={{ fontWeight: 'bold' }} fz="1.5rem" mx="2.3rem">
+    
+    
+    <Box mx="2rem">
+      <Flex justify={matches ? 'flex-start' : 'center'}>
+        <IconComponent />
+      </Flex>
+      <Flex
+        align={matches ? 'flex-start' : 'center'}
+        direction={'column'}
+      >
+        <Text style={{ fontWeight: 'bold' }} fz="1.5rem" mx="1rem" mb="xs">
           {user?.name}
         </Text>
         <LinkComponent />
       </Flex>
+    </Box>
       <Paper shadow="sm" p="md">
         <Text p="sm" w="100%">
           {user?.description}
         </Text>
       </Paper>
       <Divider />
-    </>
+      
+      </>
   )
 }

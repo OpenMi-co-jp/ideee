b3805ba8d821df4f1d005dc8c7feb12e1a60007c
hamadash <sho.hamada.92@gmail.com>
2023-11-26 14:13:19 +0900

    remove: 新基盤で不要なJSを削除 #1063

--- api/app/javascript/channels/idea/copy.js
+++
@@ -1,8 +0,0 @@
-$(document).on ('turbolinks:load', function() {
-  if ($('body').data('page') != "ideas-show") { return }
-
-  $(".url-copy-js").on("click", function() {
-    url = window.location.href;
-    navigator.clipboard.writeText(url);
-  });
-})

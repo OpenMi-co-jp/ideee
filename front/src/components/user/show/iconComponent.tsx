1d2ee0d87f0e97f821ea7a41b22db065e5340fcb
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-11-26 14:02:07 +0900

    fix: #1033 コード整形

--- front/src/components/user/show/iconComponent.tsx
+++ front/src/components/user/show/iconComponent.tsx
@@ -5,23 +5,18 @@ import { useMediaQuery } from '@mantine/hooks'
 export const IconComponent = () => {
   const user = useUser()
   const icon = user?.icon
-  
-  
 
   return (
-      <Image
-        src={icon ? icon : '/img/undefined_user_icon.webp'}
-        alt="ユーザーアイコン"
-        my="xs"
-        w="13%"
-        h="13%"
-        miw="6rem"
-        style={{
-          borderRadius: '50%',
-        }}
-      />
-      
-    
-    
+    <Image
+      src={icon ? icon : '/img/undefined_user_icon.webp'}
+      alt="ユーザーアイコン"
+      my="xs"
+      w="13%"
+      h="13%"
+      miw="6rem"
+      style={{
+        borderRadius: '50%',
+      }}
+    />
   )
 }

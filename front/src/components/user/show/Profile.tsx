8999b01627e3d4c08657417dcc837674cd6872fe
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-11-26 14:02:07 +0900

    fix: #1033 コード整形

--- front/src/components/user/show/Profile.tsx
+++ front/src/components/user/show/Profile.tsx
@@ -6,33 +6,27 @@ import { useMediaQuery } from '@mantine/hooks'
 
 export const Profile = () => {
   const user = useUser()
-  const matches = useMediaQuery('(min-width: 26em)');
+  const matches = useMediaQuery('(min-width: 26em)')
 
   return (
     <>
-    
-    
-    <Box mx="2rem">
-      <Flex justify={matches ? 'flex-start' : 'center'}>
-        <IconComponent />
-      </Flex>
-      <Flex
-        align={matches ? 'flex-start' : 'center'}
-        direction={'column'}
-      >
-        <Text style={{ fontWeight: 'bold' }} fz="1.5rem" mx="1rem" mb="xs">
-          {user?.name}
-        </Text>
-        <LinkComponent />
-      </Flex>
-    </Box>
+      <Box mx="2rem">
+        <Flex justify={matches ? 'flex-start' : 'center'}>
+          <IconComponent />
+        </Flex>
+        <Flex align={matches ? 'flex-start' : 'center'} direction={'column'}>
+          <Text style={{ fontWeight: 'bold' }} fz="1.5rem" mx="1rem" mb="xs">
+            {user?.name}
+          </Text>
+          <LinkComponent />
+        </Flex>
+      </Box>
       <Paper shadow="sm" p="md">
         <Text p="sm" w="100%">
           {user?.description}
         </Text>
       </Paper>
       <Divider />
-      
-      </>
+    </>
   )
 }

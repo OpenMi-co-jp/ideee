e3908331390e581df176d5e13c466bdfe6bee642
naru20181117 <t.naruhiro.1026@gmail.com>
2023-11-30 22:28:49 +0900

    #1185 fix: Rspecの修正

--- api/app/graphql/mutations/like/destroy.rb
+++ api/app/graphql/mutations/like/destroy.rb
@@ -2,13 +2,18 @@ module Mutations
   class Like::Destroy < BaseMutation
     graphql_name 'DestroyLike'
 
-    argument :id, ID, required: true, description: 'いいねID'
+    argument :likable_type, String, required: true, description: 'いいねしたオブジェクトタイプ'
+    argument :likable_id, Integer, required: true, description: 'オブジェクトID'
 
     field :success, Boolean, null: false, description: '成功フラグ'
-    field :errors, [String], null: false, description: 'エラーリスト'
+    field :errors, [String], null: true, description: 'エラーリスト'
 
     def resolve(**args)
-      like = ::Like.find_by(id: args[:id])
+      like = ::Like.find_by(
+        user_id: context[:current_user].id,
+        likable_type: args[:likable_type],
+        likable_id: args[:likable_id]
+      )
       like.destroy!
       {
         success: true

9d7da8403a7532a71164d63d8085c5d14490559c
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-02 20:38:17 +0900

    Commentで受け取るuserIdをidに変更

--- front/src/components/comment/Comment.tsx
+++ front/src/components/comment/Comment.tsx
@@ -5,12 +5,12 @@ import { UserIcon } from '@/components/user'
 import { TextWithLinks } from '@/utils/Text'
 
 export const Comment = ({ comment }: CommentType) => {
-  const { description, createdAt, user, userId } = comment
+  const { description, createdAt, user, id } = comment
   const commentCreatedAt = new Date(createdAt)
   return (
     <>
       <Center m="lg">
-        <Link href={`/users/${userId}`} passHref>
+        <Link href={`/users/${id}`} passHref>
           <UserIcon userIcon={user.icon} />
         </Link>
 

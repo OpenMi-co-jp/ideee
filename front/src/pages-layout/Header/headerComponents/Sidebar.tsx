5caef3e8c256d681a12e6c1a234871e9738f8e0a
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-02 09:27:25 +0900

    update: #1221 サイドバー各項目にリンクを追加

--- front/src/pages-layout/Header/headerComponents/Sidebar.tsx
+++ front/src/pages-layout/Header/headerComponents/Sidebar.tsx
@@ -6,8 +6,8 @@ import {
   Divider,
   NavLink,
   Menu,
-  MenuTarget,
   MenuDropdown,
+  Anchor,
 } from '@mantine/core'
 import {
   IconMenu2,
@@ -17,12 +17,12 @@ import {
   IconChevronRight,
   IconSun,
   IconBook,
+  IconBulb,
 } from '@tabler/icons-react'
 import { useMediaQuery } from '@mantine/hooks'
 import Link from 'next/link'
 import { useLoggedIn } from '@/components/loginContext'
 import { handleSignOut } from '@/components/Auth/SignOut/hooks'
-import { useState } from 'react'
 
 export const Sidebar = () => {
   const [opened, { open, close }] = useDisclosure(false)
@@ -33,203 +33,317 @@ export const Sidebar = () => {
 
   return (
     <>
-      <Drawer
-        size="xs"
-        position="right"
-        opened={opened}
-        onClose={close}
-        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
-      >
-        <Divider my="xs" label="Contents" labelPosition="left" color="orange" />
-        <Menu width={290} shadow="lg">
-          <Menu.Target>
-            <NavLink
-              px="2rem"
-              h="4rem"
-              label="About"
-              color="black"
-              leftSection={<IconSun size="1.3rem" stroke={2.5} />}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-              variant="subtle"
-              active
+      {isMobile && (
+        <>
+          <Drawer
+            size="xs"
+            position="right"
+            opened={opened}
+            onClose={close}
+            overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+          >
+            <Divider
+              my="xs"
+              label="Contents"
+              labelPosition="left"
+              color="orange"
             />
-          </Menu.Target>
-          <MenuDropdown>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              ideeeについて
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              アイデアの探し方
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              月間トレンドまとめ
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              個人顔発の落とし穴
-            </Menu.Item>
-          </MenuDropdown>
-        </Menu>
 
-        <Menu width={290} shadow="lg">
-          <Menu.Target>
-            <NavLink
-              px="2rem"
-              h="4rem"
-              label="Legal"
-              color="black"
-              leftSection={<IconBook size="1.3rem" stroke={2.5} />}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-              variant="subtle"
-              active
-            />
-          </Menu.Target>
-          <MenuDropdown>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              よくある質問
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              利用規約
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              プライバシーポリシー
-            </Menu.Item>
-            <Menu.Item
-              w={280}
-              h={60}
-              rightSection={
-                <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
-              }
-            >
-              運営者
-            </Menu.Item>
-          </MenuDropdown>
-        </Menu>
+            <Space />
 
-        {(() => {
-          if (loggedIn) {
-            return (
-              <>
-                <Divider
-                  my="xs"
-                  label="Login"
-                  labelPosition="left"
-                  color="orange"
-                />
-                <Link href="/user/sign_in">
-                  <NavLink
-                    px="2rem"
-                    h="4rem"
-                    label="ログイン"
-                    color="black"
-                    leftSection={<IconLogin2 size="1.3rem" stroke={2.5} />}
-                    rightSection={
-                      <IconChevronRight
-                        size="0.8rem"
-                        stroke={1.5}
-                        color="black"
-                      />
-                    }
-                    variant="subtle"
-                    active
-                  />
-                </Link>
-                <Space />
-                <Link href="/user/sign_up">
-                  <NavLink
-                    px="2rem"
-                    h="4rem"
-                    label="新規登録"
-                    color="black"
-                    leftSection={<IconUserPlus size="1.3rem" stroke={2.5} />}
-                    rightSection={
-                      <IconChevronRight
-                        size="0.8rem"
-                        stroke={1.5}
-                        color="black"
-                      />
-                    }
-                    variant="subtle"
-                    active
-                  />
-                </Link>
-              </>
-            )
-          } else {
-            return (
-              <>
-                <Divider
-                  my="xs"
-                  label="Logout"
-                  labelPosition="left"
-                  color="orange"
-                />
+            <Menu width={290} shadow="lg">
+              <Menu.Target>
                 <NavLink
                   px="2rem"
                   h="4rem"
-                  label="ログアウト"
+                  label="About"
                   color="black"
-                  leftSection={
-                    <IconLogout2 size="1.3rem" stroke={2.5} color="black" />
+                  leftSection={<IconSun size="1.3rem" stroke={2.5} />}
+                  rightSection={
+                    <IconChevronRight
+                      size="0.8rem"
+                      stroke={1.5}
+                      color="black"
+                    />
                   }
-                  rightSection={<IconChevronRight size="0.8rem" stroke={1.5} />}
                   variant="subtle"
                   active
-                  onClick={onSubmit}
                 />
-              </>
-            )
-          }
-        })()}
-      </Drawer>
-      <Button onClick={open} variant="outline" color="orange">
-        <IconMenu2 />
-      </Button>
+              </Menu.Target>
+              <MenuDropdown>
+                <Link href="/about">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                    >
+                      ideeeについて
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="https://www.ideee.tech/how_to_find_idea">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      アイデアの探し方
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="https://qiita.com/naruqiita/items/0ef4b963434226eacb6b">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      月間トレンドまとめ
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="/avoid_pitfall">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      個人顔発の落とし穴
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+              </MenuDropdown>
+            </Menu>
+
+            <Menu width={290} shadow="lg">
+              <Menu.Target>
+                <NavLink
+                  px="2rem"
+                  h="4rem"
+                  label="Legal"
+                  color="black"
+                  leftSection={<IconBook size="1.3rem" stroke={2.5} />}
+                  rightSection={
+                    <IconChevronRight
+                      size="0.8rem"
+                      stroke={1.5}
+                      color="black"
+                    />
+                  }
+                  variant="subtle"
+                  active
+                />
+              </Menu.Target>
+              <MenuDropdown>
+                <Link href="/frequent_questions">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      よくある質問
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="/terms_of_service">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      利用規約
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="/privacy_policy">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      プライバシーポリシー
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+
+                <Link href="https://naruhiro-portfolio.firebaseapp.com">
+                  <Anchor underline="never">
+                    <Menu.Item
+                      w={280}
+                      h={60}
+                      rightSection={
+                        <IconChevronRight
+                          size="0.8rem"
+                          stroke={1.5}
+                          color="black"
+                        />
+                      }
+                      type="button"
+                    >
+                      運営者
+                    </Menu.Item>
+                  </Anchor>
+                </Link>
+              </MenuDropdown>
+            </Menu>
+
+            {(() => {
+              if (loggedIn) {
+                return (
+                  <>
+                    <Divider
+                      my="xs"
+                      label="Login"
+                      labelPosition="left"
+                      color="orange"
+                    />
+                    <Link href="/user/sign_in">
+                      <Anchor underline="never">
+                        <NavLink
+                          px="2rem"
+                          h="4rem"
+                          label="ログイン"
+                          color="black"
+                          leftSection={
+                            <IconLogin2 size="1.3rem" stroke={2.5} />
+                          }
+                          rightSection={
+                            <IconChevronRight
+                              size="0.8rem"
+                              stroke={1.5}
+                              color="black"
+                            />
+                          }
+                          variant="subtle"
+                          active
+                        />
+                      </Anchor>
+                    </Link>
+                    <Space />
+                    <Link href="/user/sign_up">
+                      <Anchor underline="never">
+                        <NavLink
+                          px="2rem"
+                          h="4rem"
+                          label="新規登録"
+                          color="black"
+                          leftSection={
+                            <IconUserPlus size="1.3rem" stroke={2.5} />
+                          }
+                          rightSection={
+                            <IconChevronRight
+                              size="0.8rem"
+                              stroke={1.5}
+                              color="black"
+                            />
+                          }
+                          variant="subtle"
+                          active
+                        />
+                      </Anchor>
+                    </Link>
+                  </>
+                )
+              } else {
+                return (
+                  <>
+                    <Divider
+                      my="xs"
+                      label="Logout"
+                      labelPosition="left"
+                      color="orange"
+                    />
+
+                    <Anchor underline="never">
+                      <NavLink
+                        px="2rem"
+                        h="4rem"
+                        label="ログアウト"
+                        color="black"
+                        leftSection={
+                          <IconLogout2
+                            size="1.3rem"
+                            stroke={2.5}
+                            color="black"
+                          />
+                        }
+                        rightSection={
+                          <IconChevronRight size="0.8rem" stroke={1.5} />
+                        }
+                        variant="subtle"
+                        active
+                        onClick={onSubmit}
+                      />
+                    </Anchor>
+                  </>
+                )
+              }
+            })()}
+          </Drawer>
+          <Button onClick={open} variant="outline" color="orange">
+            <IconMenu2 />
+          </Button>
+        </>
+      )}
     </>
   )
 }

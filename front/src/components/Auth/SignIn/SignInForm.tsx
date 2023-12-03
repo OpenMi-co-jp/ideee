cc68bf4960b85a1dd859e0fc65bbe23026db11b4
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-04 03:18:01 +0900

    #1233 set: CurrentUserProviderにUIを調整

--- front/src/components/Auth/SignIn/SignInForm.tsx
+++ front/src/components/Auth/SignIn/SignInForm.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { PasswordForm, TextForm } from '@/components/ReactFormSet'
 import { handleSignIn } from './hooks'
-import { useLoggedIn } from '@/components/loginContext'
 import { OmniAuth } from '@/components/Auth/OmniAuth'
 import type { CustomNextPage } from 'next'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useCurrentUser } from '@/context/CurrentUserContext'
+import { useRouter } from 'next/router'
 
 type SignInFormValues = {
   email: string
@@ -15,7 +16,7 @@ type SignInFormValues = {
 }
 
 export const SignInForm: CustomNextPage = () => {
-  const { setLoggedIn } = useLoggedIn()
+  const { currentUser, setCurrentUser } = useCurrentUser()
 
   const signInSchema = z.object({
     email: z
@@ -33,7 +34,13 @@ export const SignInForm: CustomNextPage = () => {
     },
     mode: 'onChange',
   })
-  const onSubmit = (data: SignInFormValues) => handleSignIn(data, setLoggedIn)
+  const onSubmit = (data: SignInFormValues) =>
+    handleSignIn(data, setCurrentUser)
+  const router = useRouter()
+
+  if (currentUser) {
+    router.push('/')
+  }
 
   return (
     <form onSubmit={form.handleSubmit(onSubmit)}>

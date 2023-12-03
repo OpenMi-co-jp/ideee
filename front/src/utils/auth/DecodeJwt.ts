c32badf85dd301d86e3863e2eb85df0a813108a3
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-04 02:38:08 +0900

    #1233 set: DecodeJwtメソッドを設定

---
+++ front/src/utils/auth/DecodeJwt.ts
@@ -0,0 +1,18 @@
+// TODO: JWTのverifyメソッドを実装する
+// import { decode } from 'jsonwebtoken'
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
+export const DecodeJwt = (token: string) => {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      })
+      .join('')
+  )
+
+  return JSON.parse(jsonPayload)
+}

15b7c345965fd999ea6d0028d8cd5e2afcf214ee
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-05 06:00:24 +0900

    update: #1033 リンクデータがない時はアイコンがdisableになるよう設定

--- front/src/components/user/show/LinkComponent.tsx
+++ front/src/components/user/show/LinkComponent.tsx
@@ -10,13 +10,18 @@ import { useUser } from '@/context/userProfileContext'
 const GithubLink = () => {
   const user = useUser()
   const { hovered, ref } = useHover()
+  const isDisabled = !user?.githubId
+  const githubUrl = isDisabled ? '#' : `https://github.com/${user.githubId}`
+
 
   return (
     <ActionIcon variant="transparent">
       <Anchor
-        href={user?.githubId ? `https://github.com/${user.githubId}` : ''}
+        href={user?.githubId ? githubUrl : ''}
         target="_blank"
         rel="noopener noreferrer"
+        onClick={(e) => { if (isDisabled) e.preventDefault(); }}
+        style={isDisabled ? { pointerEvents: 'none', opacity: 0.5 } : {}}
       >
         <div ref={ref}>
           <IconBrandGithub
@@ -36,12 +41,17 @@ const GithubLink = () => {
 const TwitterLink = () => {
   const user = useUser()
   const { hovered, ref } = useHover()
+  const isDisabled = !user?.twitterId
+  const twitterUrl = isDisabled ? '#' : `https://twitter.com/${user.twitterId}`
+
   return (
     <ActionIcon variant="transparent">
       <Anchor
-        href={user?.twitterId ? `https://twitter.com/${user.twitterId}` : ''}
+        href={user?.twitterId ? twitterUrl : ''}
         target="_blank"
         rel="noopener noreferrer"
+        onClick={(e) => { if (isDisabled) e.preventDefault(); }}
+        style={isDisabled ? { pointerEvents: 'none', opacity: 0.5 } : {}}
       >
         <div ref={ref}>
           <IconBrandTwitter
@@ -61,12 +71,17 @@ const TwitterLink = () => {
 const SiteUrl = () => {
   const user = useUser()
   const { hovered, ref } = useHover()
+  const isDisabled = !user?.siteUrl
+  const SiteUrl = isDisabled ? '#' : '${user.siteUrl}'
+
   return (
     <ActionIcon variant="transparent">
       <Anchor
-        href={user?.siteUrl ? `${user.siteUrl}` : ''}
+        href={user?.siteUrl ? SiteUrl : ''}
         target="_blank"
         rel="noopener noreferrer"
+        onClick={(e) => { if (isDisabled) e.preventDefault(); }}
+        style={isDisabled ? { pointerEvents: 'none', opacity: 0.5 } : {}}
       >
         <div ref={ref}>
           <IconLink

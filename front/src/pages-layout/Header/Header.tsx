4f8204a828ca5124976733ea0c9a03c1d7d401ae
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-05 21:11:54 +0900

    ヘッダーのレスポンシブ対応

--- front/src/pages-layout/Header/Header.tsx
+++ front/src/pages-layout/Header/Header.tsx
@@ -1,14 +1,19 @@
 import { useMantineColorScheme, Box, Group, Portal, rem } from '@mantine/core'
 import type { FC } from 'react'
-import { useHeadroom } from '@mantine/hooks'
+import { useHeadroom, useMediaQuery } from '@mantine/hooks'
 import { SearchIcon } from './headerComponents'
 import { UserToggle, Notification, ServiceIcon } from './headerComponents'
 import { IdeaCreateButton } from '@/components/idea/createButton'
 import { Sidebar } from './headerComponents/Sidebar'
+import { useLoggedIn } from '@/components/loginContext'
 
 export const Header: FC = () => {
   const pinned = useHeadroom({ fixedAt: 120 })
   const { colorScheme } = useMantineColorScheme()
+  const { setLoggedIn } = useLoggedIn()
+  const { loggedIn } = useLoggedIn()
+  const isNarrowScreen = useMediaQuery('(max-width: 388px)')
+  const headerHeight = isNarrowScreen ? rem(115) : rem(60)
 
   return (
     <Portal>
@@ -20,7 +25,7 @@ export const Header: FC = () => {
           top: 0,
           left: 0,
           right: 0,
-          height: rem(60),
+          height: headerHeight,
           zIndex: 200, // modalのindexがが201のため
           transform: `translate3d(0, ${pinned ? 0 : rem(-110)}, 0)`,
           transition: 'transform 400ms ease',
@@ -33,7 +38,8 @@ export const Header: FC = () => {
           <ServiceIcon />
           <Group justify="center">
             <SearchIcon />
-            <Notification />
+            { loggedIn &&
+            <Notification />}
             <UserToggle />
             <IdeaCreateButton />
             <Sidebar />

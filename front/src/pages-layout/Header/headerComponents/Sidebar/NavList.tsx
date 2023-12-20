7bd4a6934b608a7ee827225f7515207edbf2cbb1
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-20 21:13:57 +0900

    コード整形

--- front/src/pages-layout/Header/headerComponents/Sidebar/NavList.tsx
+++ front/src/pages-layout/Header/headerComponents/Sidebar/NavList.tsx
@@ -1,9 +1,15 @@
 import { Anchor, NavLink, Divider } from '@mantine/core'
-import { IconBulb, IconChevronRight, IconLogin2, IconLogout2, IconUserPlus } from '@tabler/icons-react'
+import {
+  IconBulb,
+  IconChevronRight,
+  IconLogin2,
+  IconLogout2,
+  IconUserPlus,
+} from '@tabler/icons-react'
 import Link from 'next/link'
 import { useCurrentUser } from '@/context/CurrentUserContext'
 import { HandleSignOut } from '@/components/Auth/SignOut/hooks'
-import  React, { useState } from 'react'
+import React, { useState } from 'react'
 
 type NavItemProps = {
   item: {
@@ -26,22 +32,24 @@ const NavItem = ({ item }: NavItemProps) => {
   }
 
   return (
-  <Link href={href || '/'}>
-    <Anchor underline="never" onClick={handleClick}>
-      <NavLink
-        px="2rem"
-        h="4rem"
-        label={label}
-        color="black"
-        leftSection={<Icon size="1.3rem" stroke={2.5} />}
-        rightSection={<IconChevronRight size="0.8rem" stroke={1.5} color="black" />}
-        variant="subtle"
-        active
-      />
-    </Anchor>
-  </Link>
+    <Link href={href || '/'}>
+      <Anchor underline="never" onClick={handleClick}>
+        <NavLink
+          px="2rem"
+          h="4rem"
+          label={label}
+          color="black"
+          leftSection={<Icon size="1.3rem" stroke={2.5} />}
+          rightSection={
+            <IconChevronRight size="0.8rem" stroke={1.5} color="black" />
+          }
+          variant="subtle"
+          active
+        />
+      </Anchor>
+    </Link>
   )
-  }
+}
 
 export const NavList = () => {
   const { currentUser } = useCurrentUser()
@@ -55,44 +63,44 @@ export const NavList = () => {
       <Divider my="xs" label="Idea" labelPosition="left" color="orange" />
       <NavItem
         item={{
-        label: 'アイデア投稿',
-        href: '/',
-        icon: IconBulb,
+          label: 'アイデア投稿',
+          href: '/',
+          icon: IconBulb,
         }}
         closeDrawer={closeDrawer}
       />
       {currentUser ? (
-      <>
-      <Divider my="xs" label="Logout" labelPosition="left" color="orange" />
-      <NavItem
-        item={{
-        label: 'ログアウト',
-        href: '/',
-        icon: IconLogout2
-      }}
-      closeDrawer={closeDrawer}
-      />
-      </>
+        <>
+          <Divider my="xs" label="Logout" labelPosition="left" color="orange" />
+          <NavItem
+            item={{
+              label: 'ログアウト',
+              href: '/',
+              icon: IconLogout2,
+            }}
+            closeDrawer={closeDrawer}
+          />
+        </>
       ) : (
-      <>
-      <Divider my="xs" label="Login" labelPosition="left" color="orange" />
-      <NavItem
-      item={{
-        label: 'ログイン',
-        href: '/users/sign_in',
-        icon: IconLogin2
-      }}
-      closeDrawer={closeDrawer}
-      />
-      <NavItem
-        item={{
-        label: '新規登録',
-        href: '/users/sign_up',
-        icon: IconUserPlus
-      }}
-      closeDrawer={closeDrawer}
-      />
-      </>
+        <>
+          <Divider my="xs" label="Login" labelPosition="left" color="orange" />
+          <NavItem
+            item={{
+              label: 'ログイン',
+              href: '/users/sign_in',
+              icon: IconLogin2,
+            }}
+            closeDrawer={closeDrawer}
+          />
+          <NavItem
+            item={{
+              label: '新規登録',
+              href: '/users/sign_up',
+              icon: IconUserPlus,
+            }}
+            closeDrawer={closeDrawer}
+          />
+        </>
       )}
     </>
   )

696b156401b1ea9d43e1897b1414224a4d96d2c0
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-22 23:40:02 +0900

    不要なコードを削除

--- front/src/pages-layout/Header/headerComponents/Sidebar.tsx
+++ front/src/pages-layout/Header/headerComponents/Sidebar.tsx
@@ -2,20 +2,12 @@ import { useDisclosure } from '@mantine/hooks'
 import { Drawer, Button, Divider } from '@mantine/core'
 import { IconMenu2 } from '@tabler/icons-react'
 import { useMediaQuery } from '@mantine/hooks'
-import { CurrentUserProps, useCurrentUser } from '@/context/CurrentUserContext'
-import { HandleSignOut } from '@/components/Auth/SignOut/hooks'
 import { NavList } from './Sidebar/NavList'
 import { MenuList } from './Sidebar/MenuList'
 
-export const Sidebar = ({
-  currentUser,
-}: {
-  currentUser: CurrentUserProps | null
-}) => {
+export const Sidebar = () => {
   const [opened, { open, close }] = useDisclosure(false)
   const isMobile = useMediaQuery('(max-width: 47.99em)')
-  const { setCurrentUser } = useCurrentUser()
-  const onSubmit = () => HandleSignOut(setCurrentUser)
 
   return (
     <>
@@ -35,7 +27,7 @@ export const Sidebar = ({
               labelPosition="left"
               color="orange"
             />
-            <MenuList close={close} currentUser={currentUser} />
+            <MenuList close={close} />
           </Drawer>
           <Button onClick={open} variant="outline" color="orange">
             <IconMenu2 />

fbe468df2e5278bcb78fbec1e84ac0483adb9891
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-27 06:05:59 +0900

    #1221 ref: Headerの表示切り替えをリファクタリング

--- front/src/pages-layout/Header/headerComponents/Notification.tsx
+++ front/src/pages-layout/Header/headerComponents/Notification.tsx
@@ -1,17 +1,10 @@
 import { IconBell } from '@tabler/icons-react'
 import Link from 'next/link'
-import { useCurrentUser } from '@/context/CurrentUserContext'
 
 export const Notification = () => {
-  const { currentUser } = useCurrentUser()
-
   return (
-    <>
-      {currentUser && (
-        <Link href="/notifications" passHref>
-          <IconBell size={25} />
-        </Link>
-      )}
-    </>
+    <Link href="/notifications" passHref>
+      <IconBell size={25} />
+    </Link>
   )
 }

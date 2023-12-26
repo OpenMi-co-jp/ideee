4cfdedc503cf3dca9727bbbac914fa6fbcd1c2dd
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-27 05:50:15 +0900

    #1221 ref: 名前とcolor指定のリファクタリング

--- front/src/pages-layout/Header/headerComponents/Sidebar/MenuData.tsx
+++ front/src/pages-layout/Header/headerComponents/Sidebar/MenuData.tsx
@@ -5,28 +5,28 @@ export const menuData = [
     label: 'About',
     icon: <IconSun size="1.3rem" stroke={2.5} />,
     links: [
-      { href: '/about', text: 'ideeeについて' },
+      { href: '/about', title: 'ideeeについて' },
       {
         // TODO:
         // https://github.com/naru20181117/ideee/pull/1220でページ実装後に、ここのリンクを修正する
         href: 'https://www.ideee.tech/how_to_find_idea',
-        text: 'アイデアの探し方',
+        title: 'アイデアの探し方',
       },
       {
         href: 'https://qiita.com/naruqiita/items/0ef4b963434226eacb6b',
-        text: '月間トレンドまとめ',
+        title: '月間トレンドまとめ',
       },
-      { href: '/avoid_pitfall', text: '個人顔発の落とし穴' },
+      { href: '/avoid_pitfall', title: '個人顔発の落とし穴' },
     ],
   },
   {
     label: 'Legal',
     icon: <IconBook size="1.3rem" stroke={2.5} />,
     links: [
-      { href: '/frequent_questions', text: 'よくある質問' },
-      { href: '/terms_of_service', text: '利用規約' },
-      { href: '/privacy_policy', text: 'プライバシーポリシー' },
-      { href: 'https://naruhiro-portfolio.firebaseapp.com', text: '運営者' },
+      { href: '/frequent_questions', title: 'よくある質問' },
+      { href: '/terms_of_service', title: '利用規約' },
+      { href: '/privacy_policy', title: 'プライバシーポリシー' },
+      { href: 'https://naruhiro-portfolio.firebaseapp.com', title: '運営者' },
     ],
   },
 ]

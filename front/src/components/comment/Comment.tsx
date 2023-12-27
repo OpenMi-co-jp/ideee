09c0623691f40e3da5ac107eac1a574510f0e4a0
yusukenakagawa <yusukenakagawa7023@gmail.com>
2023-12-28 05:57:13 +0900

    currentUserでコメント表示位置が切り替わるよ表示位置が切り替わるように設定

--- front/src/components/comment/Comment.tsx
+++ front/src/components/comment/Comment.tsx
@@ -1,27 +1,74 @@
-import { Group, Text, Paper, Center } from '@mantine/core'
+import { Group, Text, Paper, Flex } from '@mantine/core'
 import { CommentType } from '@/types/idea'
 import { FormatDate } from '@/utils/common'
 import { UserIcon } from '@/components/user'
 import Link from 'next/link'
 import { TextWithLinks } from '@/utils/Text'
+import { useCurrentUser } from '@/context/CurrentUserContext'
 
 export const Comment = ({ comment }: CommentType) => {
+  const { currentUser } = useCurrentUser()
   const { description, createdAt, user } = comment
   const commentCreatedAt = new Date(createdAt)
+  const isCurrentUser = currentUser?.id == user.id
+
   return (
     <>
-      <Center m="lg">
-        <Link href={`/users/${user.id}`} passHref>
-          <UserIcon userIcon={user.icon} />
-        </Link>
-
-        <Group p="xs">
-          <Paper bg="#FFFFFF" maw="30rem" p="md" radius="lg">
-            <TextWithLinks>{description}</TextWithLinks>
-          </Paper>
-          <Text c="gray">{FormatDate(commentCreatedAt)}</Text>
-        </Group>
-      </Center>
+      <Flex
+        p="xs"
+        direction="column"
+        wrap={isCurrentUser ? 'wrap' : 'wrap-reverse'}
+      >
+        <Flex justify={isCurrentUser ? 'flex-end' : 'flex-start'}>
+          <Flex direction="column">
+            <Group justify={isCurrentUser ? 'flex-end' : 'flex-start'}>
+              {isCurrentUser
+                ? [
+                    <Link key="icon" href={`/users/${user.id}`} passHref>
+                      <UserIcon userIcon={user.icon} />
+                    </Link>,
+                    <Paper
+                      key="name"
+                      style={{
+                        backgroundColor: '#fef6eb',
+                        display: 'inline-block',
+                      }}
+                      mr="0.3rem"
+                    >
+                      {user.name}
+                    </Paper>,
+                  ].reverse()
+                : [
+                    <Link key="icon" href={`/users/${user.id}`} passHref>
+                      <UserIcon userIcon={user.icon} />
+                    </Link>,
+                    <Paper
+                      key="name"
+                      style={{
+                        backgroundColor: '#fef6eb',
+                        display: 'inline-block',
+                      }}
+                      mr="0.3rem"
+                    >
+                      {user.name}
+                    </Paper>,
+                  ]}
+            </Group>
+            <Paper
+              bg="#FFFFFF"
+              maw="30rem"
+              p="md"
+              radius="lg"
+              style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}
+            >
+              <TextWithLinks>{description}</TextWithLinks>
+            </Paper>
+            <Flex justify={isCurrentUser ? 'flex-end' : 'flex-start'}>
+              <Text c="gray">{FormatDate(commentCreatedAt)}</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   )
 }

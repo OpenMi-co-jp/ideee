862372be0fcfcf5ea12f99202d8b1f91803b5794
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-28 04:56:46 +0900

    #1254 fix: URLの表示が正しくされるように修正

--- front/src/components/user/show/LinkComponent.tsx
+++ front/src/components/user/show/LinkComponent.tsx
@@ -18,18 +18,18 @@ const SocialLink = ({ icon, url, isDisabled }: SocialLinkProps) => {
   }
 
   return (
-    <ActionIcon variant="transparent">
-      <Anchor href={url} target="_blank" rel="noopener noreferrer">
-        <div ref={ref}>
+    <Anchor href={url} target="_blank" rel="noopener noreferrer">
+      <div ref={ref}>
+        <ActionIcon variant="transparent">
           {cloneElement(icon, {
             color: 'black',
             style: hovered
               ? { transform: 'scale(1.1)', transition: 'all 0.03s' }
               : {},
           })}
-        </div>
-      </Anchor>
-    </ActionIcon>
+        </ActionIcon>
+      </div>
+    </Anchor>
   )
 }
 
@@ -52,14 +52,17 @@ export const LinkComponent = () => {
 
   return (
     <Group my="sm">
-      {links.map((link) => (
-        <SocialLink
-          key={link.id}
-          icon={link.icon}
-          url={link.url}
-          isDisabled={!link.id}
-        />
-      ))}
+      {links.map(
+        (link) =>
+          link.id && (
+            <SocialLink
+              key={link.id}
+              icon={link.icon}
+              url={link.url}
+              isDisabled={!link.id}
+            />
+          )
+      )}
     </Group>
   )
 }

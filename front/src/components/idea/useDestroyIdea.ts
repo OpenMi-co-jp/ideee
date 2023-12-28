133206a02a0ad60af1f48c945f4b6def83e32948
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2023-12-28 13:28:59 +0900

    #1186 : 一部ロジック内包及びエラーハンドリング追加

--- front/src/components/idea/useDestroyIdea.ts
+++ front/src/components/idea/useDestroyIdea.ts
@@ -1,23 +1,32 @@
 import { useDestroyIdeaMutation } from '@/lib/generated/client'
 import { useRouter } from 'next/router'
+import { showSuccess, showError } from '../notifications'
 
 /**
  * CurrentUserのアイデアを削除するための関数
- * @param {string} id - 削除するアイデアのID。
+ * @param 
  * @returns  アイデアを削除するための関数。
  */
-export const useDestroyIdea = (id: string) => {
+export const useDestroyIdea = () => {
   const router = useRouter()
+  const { id : ideaId } = router.query
   const [destroyIdea] = useDestroyIdeaMutation({
     variables: {
       input: {
-        id: id,
+        id: ideaId as string,
       },
     },
   })
   const handleDestroyIdea = () => {
     destroyIdea().then(() => {
+      showSuccess({ action: 'アイデア削除' })
       router.push('/idea')
+    })
+    .catch((error) => {
+      showError({
+        action: 'アイデア削除',
+        message: error.message as string,
+      })
     })
   }
 

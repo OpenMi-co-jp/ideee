d39d739fb73859dbc9a5d315097c291e7936616b
naru20181117 <t.naruhiro.1026@gmail.com>
2023-12-28 12:32:58 +0900

    #1254 fix: validationとalertを訂正

--- api/app/graphql/mutations/user/update.rb
+++ api/app/graphql/mutations/user/update.rb
@@ -13,6 +13,7 @@ module Mutations
 
     field :user, Types::UserType, null: false, description: 'ユーザーオブジェクト'
     field :success, Boolean, null: false, description: '成功フラグ'
+    field :errors, [String], null: false, description: 'エラーリスト'
 
     def resolve(**args)
       user = ::User.find(args[:id])
@@ -20,6 +21,11 @@ module Mutations
       {
         user:,
         success: true
+      }
+    rescue ActiveRecord::RecordInvalid => e
+      {
+        success: false,
+        errors: e.record.errors.full_messages
       }
     end
   end
--- front/src/lib/generated/client.ts
+++ front/src/lib/generated/client.ts
@@ -802,6 +802,8 @@ export type UpdateUserPayload = {
   __typename?: 'UpdateUserPayload'
   /** A unique identifier for the client performing the mutation. */
   clientMutationId?: Maybe<Scalars['String']>
+  /** エラーリスト */
+  errors: Array<Scalars['String']>
   /** 成功フラグ */
   success: Scalars['Boolean']
   /** ユーザーオブジェクト */
@@ -1342,6 +1344,7 @@ export type UpdateUserMutation = {
   updateUser?: {
     __typename?: 'UpdateUserPayload'
     success: boolean
+    errors: Array<string>
     user: {
       __typename?: 'User'
       id: string
@@ -3043,6 +3046,7 @@ export const UpdateUserDocument = gql`
         definition
       }
       success
+      errors
     }
   }
 `

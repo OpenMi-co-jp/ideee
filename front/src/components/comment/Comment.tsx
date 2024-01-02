8158761a7f37cebd910dc69102ef791bfc842c4d
yusukenakagawa <yusukenakagawa7023@gmail.com>
2024-01-02 09:34:18 +0900

    curentUserをStringで比較するように変更

--- front/src/components/comment/Comment.tsx
+++ front/src/components/comment/Comment.tsx
@@ -10,7 +10,9 @@ export const Comment = ({ comment }: CommentType) => {
   const { currentUser } = useCurrentUser()
   const { description, createdAt, user } = comment
   const commentCreatedAt = new Date(createdAt)
-  const isCurrentUser = currentUser?.id === Number(user.id)
+  const isCurrentUser = currentUser && user && String(currentUser.id) === String(user.id)
+
+
   const userContents = [
     <Link key="icon" href={`/users/${user.id}`} passHref>
       <UserIcon userIcon={user.icon} />

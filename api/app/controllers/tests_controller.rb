bcaaf550857b03d165b4087ef0f0769746326fbe
hamadash <sho.hamada.92@gmail.com>
2024-01-08 14:46:40 +0900

    fix: lint #1239

--- api/app/controllers/tests_controller.rb
+++ api/app/controllers/tests_controller.rb
@@ -1,15 +1,15 @@
 class TestsController < ApplicationController
   protect_from_forgery except: :create
 
-  def create
-    render json: { message: 'createテスト成功です' }, status: :ok
-  rescue StandardError => e
-    render json: { message: "createテスト失敗です#{e.message}" }, status: :internal_server_error
-  end
-
   def show
     render json: { message: 'showテスト成功です' }, status: :ok
   rescue StandardError => e
     render json: { message: "showテスト失敗です#{e.message}" }, status: :internal_server_error
+  end
+
+  def create
+    render json: { message: 'createテスト成功です' }, status: :ok
+  rescue StandardError => e
+    render json: { message: "createテスト失敗です#{e.message}" }, status: :internal_server_error
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
 
   # テストなので後で消す
-  resource :test, only: %i[create show]
+  resource :test, only: %i[show create]
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   mount Sidekiq::Web => '/sidekiq'

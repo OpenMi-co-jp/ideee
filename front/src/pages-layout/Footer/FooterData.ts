a987e31371e91bc61452be8e1f3a8939ecdffce8
yusukenakagawa <yusukenakagawa7023@gmail.com>
2024-01-18 05:29:47 +0900

    update: #1211 アイデアの探し方ページのリンクを変更

--- front/src/pages-layout/Footer/FooterData.ts
+++ front/src/pages-layout/Footer/FooterData.ts
@@ -5,10 +5,8 @@ export const footerData = {
       links: [
         { label: 'ideeeについて', link: '/about' },
         {
-          // TODO:
-          // https://github.com/naru20181117/ideee/pull/1220でページ実装後に、ここのリンクを修正する
           label: 'アイデアの探し方',
-          link: 'https://www.ideee.tech/how_to_find_idea',
+          link: '/how_to_find_idea',
         },
         {
           label: '月間トレンドまとめ',

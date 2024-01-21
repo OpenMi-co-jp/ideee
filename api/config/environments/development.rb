26b0edb3380d76c1cd4033e0ce17f677ca7f2152
hamadash <sho.hamada.92@gmail.com>
2024-01-21 14:35:43 +0900

    change: 本番環境と同様にCSRF保護を有効にする #1239

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -19,9 +19,6 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  # 確認用
-  config.action_controller.allow_forgery_protection = false
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?

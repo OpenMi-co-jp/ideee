896d4a1285d54c5afd50d2596af7c31657e395be
naru20181117 <t.naruhiro.1026@gmail.com>
2024-01-26 12:08:46 +0900

    #1254 fix: gragのエラーに対処

--- front/src/lib/analytics/gtag.ts
+++ front/src/lib/analytics/gtag.ts
@@ -2,7 +2,9 @@ export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || ''
 // TODO: デプロイ時にプロパティを設定
 
 export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
-    page_path: url,
-  })
+  if (typeof window.gtag === 'function') {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    })
+  }
 }

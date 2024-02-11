246343114b4684807cfa6acdb8c28356b9b442e4
hamadash <sho.hamada.92@gmail.com>
2024-02-11 15:13:05 +0900

    fix: CommentAnnotationを修正 #1280

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -78,15 +78,14 @@ class ApplicationController < ActionController::Base
     render json: { message: '操作が許可されていません。' }, status: :forbidden
   end
 
-  # NOTE:
-  # RequestForgeryProtection#valid_request_origin? を拡張したチェックをしている
-  # ※ 許可する origin は cors.rb に揃える
+  # NOTE: RequestForgeryProtection#valid_request_origin? を拡張したチェックをしている
+  #       ※ 許可する origin は cors.rb に揃える
   #
-  # 拡張している理由は以下。
-  # - API モードのため、別 origin だと RequestForgeryProtection#valid_request_origin? が必ず false になる
-  # - このため、 config.action_controller.forgery_protection_origin_check = false としている
-  # - これだと CSRF 検証のなかでは origin チェックが全くされなくなる
-  # - それは避けたいため、別 origin でも cors.rb で許可している origin だけ許容するようにしたい
+  #       拡張している理由は以下。
+  #       - API モードのため、別 origin だと RequestForgeryProtection#valid_request_origin? が必ず false になる
+  #       - このため、 config.action_controller.forgery_protection_origin_check = false としている
+  #       - これだと CSRF 検証のなかでは origin チェックが全くされなくなる
+  #       - それは避けたいため、別 origin でも cors.rb で許可している origin だけ許容するようにしたい
   def valid_allowed_request_origin
     return if request.get? || request.head?
     return if valid_request_origin?

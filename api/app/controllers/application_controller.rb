b40c5fffd826eea647249f4a0637e739b4ccca28
hamadash <sho.hamada.92@gmail.com>
2024-02-12 16:11:32 +0900

    fix: valid_request_origin?ではなく直接originチェックをする #1280

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -89,7 +89,7 @@ class ApplicationController < ActionController::Base
   def valid_allowed_request_origin
     return if request.get? || request.head?
     # CsrfTokenController では、 valid_request_origin? が必ず true になり、別 origin からのリクエストしかないため
-    return if valid_request_origin? && controller_name != 'csrf_token'
+    return if (request.origin.nil? || request.origin == request.base_url) && controller_name != 'csrf_token'
     # TODO: 本番へ移行時にドメイン設定を変更 (cors.rb と同様)
     return if request.origin.in?(%w[http://localhost:3000 https://ideee.vercel.app])
     return if request.origin.match?(/ideee-(.*)-narucel\.vercel\.app/)

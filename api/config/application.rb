5d523c513e001e510539e10e393f205999be5a60
hamadash <sho.hamada.92@gmail.com>
2024-02-18 14:47:48 +0900

    fix: Style/Lambdaの指摘を修正 #1280

--- api/config/application.rb
+++ api/config/application.rb
@@ -42,7 +42,7 @@ module Ideee
     # TODO: materializeなどを削除してFlashの使用がなくなったら削除
     config.middleware.use ActionDispatch::Flash
     config.middleware.use Rack::MethodOverride
-    config.action_dispatch.cookies_same_site_protection = ->(request) do
+    config.action_dispatch.cookies_same_site_protection = lambda do |request|
       return if request.origin.nil?
       # TODO: 本番へ移行時にドメイン設定を変更 (cors.rb と同様)
       return :none if request.origin.in?(%w[http://localhost:3000 https://ideee.vercel.app])

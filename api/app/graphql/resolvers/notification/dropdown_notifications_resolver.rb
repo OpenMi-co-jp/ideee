476d711241c670c27ce8283cbf5b310eee78ff1a
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-02-20 00:05:39 +0900

    add: ドロップダウン用のNotificationsのresolverを作成する #1304

---
+++ api/app/graphql/resolvers/notification/dropdown_notifications_resolver.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class Notification::DropdownNotificationsResolver < BaseResolver
+    graphql_name 'DropdownNotifications'
+
+    type [Types::NotificationType], null: false
+
+    def resolve
+      current_user = context[:current_user]
+      return [] unless current_user
+
+      current_user.passive_notifications.eager_load(%i[visitor idea]).order(created_at: :desc).limit(5)
+    end
+  end
+end

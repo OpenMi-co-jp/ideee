46a04526877f49591b3c4f564f1e59ad2e547cbe
naru20181117 <t.naruhiro.1026@gmail.com>
2024-02-21 21:24:11 +0900

    #1278 fix: urlのバリデーションエラー対応

--- front/src/components/idea/form/create/hooks.ts
+++ front/src/components/idea/form/create/hooks.ts
@@ -52,11 +52,13 @@ const IdeaCreateFormSchema = z.object({
   githubUrl: z
     .string()
     .url({ message: 'URLの形式で入力してください' })
-    .nullish(),
+    .optional()
+    .or(z.literal('')),
   productUrl: z
     .string()
     .url({ message: 'URLの形式で入力してください' })
-    .nullish(),
+    .optional()
+    .or(z.literal('')),
   draft: z.boolean().nullish(),
   icon: z.string().nullish(),
   tagList: z

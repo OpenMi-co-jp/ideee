3259127e1025e1d189b56843c10dae692ee04a04
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-27 15:39:38 +0900

    #1253 add:状態の追加

--- front/src/components/user/setting/MailSettingForm.tsx
+++ front/src/components/user/setting/MailSettingForm.tsx
@@ -2,23 +2,39 @@ import { Button, Flex, Group, Switch } from '@mantine/core'
 import { useState } from 'react'
 import { useCurrentUser } from '@/context/CurrentUserContext'
 
-const initalValues = [
-  { id: 1, label: '項目 1' },
-  { id: 2, label: '項目 2' },
-  { id: 3, label: '項目 3' },
-  { id: 4, label: '項目 4' },
-  { id: 5, label: '項目 5' },
-  { id: 6, label: '項目 6' },
-  { id: 7, label: '項目 7' },
-  { id: 8, label: '項目 8' },
+const contents = [
+  { id: 1, label: '項目 1', checked: false },
+  { id: 2, label: '項目 2', checked: false },
+  { id: 3, label: '項目 3', checked: false },
+  { id: 4, label: '項目 4', checked: false },
+  { id: 5, label: '項目 5', checked: false },
+  { id: 6, label: '項目 6', checked: false },
+  { id: 7, label: '項目 7', checked: false },
+  { id: 8, label: '項目 8', checked: false },
 ]
 
 export const MailSettingForm = () => {
-  const [items, setItems] = useState(initalValues)
+  const [items, setItems] = useState(contents)
   const { currentUser } = useCurrentUser()
 
+  const handleSwitchChange = (id: number) => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.id === id) {
+          return { ...item, checked: !item.checked }
+        }
+        return item
+      })
+    )
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(items)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Group p="md">
         <Flex direction="column" gap="xl">
           {items.map((item) => (
@@ -26,6 +42,8 @@ export const MailSettingForm = () => {
               key={item.id}
               label={item.label}
               name={`switch-${item.id}`}
+              defaultChecked={item.checked}
+              onChange={() => handleSwitchChange(item.id)}
             />
           ))}
           <Button type="submit">送信</Button>

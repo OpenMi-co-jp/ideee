656b6b63687429e539bc8f4e1d7f318b78f796a9
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-27 15:39:12 +0900

    #1253 fix:ユーザーIdで変更できなかったので修正

--- api/app/graphql/resolvers/notification_config/notification_config_resolver.rb
+++ api/app/graphql/resolvers/notification_config/notification_config_resolver.rb
@@ -8,7 +8,7 @@ module Resolvers
       argument :user_id, ID, required: true, description: 'ユーザーID'
 
       def resolve(**args)
-        ::NotificationConfig.find_by(args[:user_id])
+        ::NotificationConfig.find_by(user_id: args[:user_id])
       end
     end
   end

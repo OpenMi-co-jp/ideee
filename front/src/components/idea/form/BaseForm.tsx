2c1b6e338fa694f7d15faf764ba799c69930c9f8
naru20181117 <t.naruhiro.1026@gmail.com>
2024-03-01 11:08:51 +0900

    #1290 set: SwitchFormの設置

--- front/src/components/idea/form/BaseForm.tsx
+++ front/src/components/idea/form/BaseForm.tsx
@@ -17,6 +17,7 @@ import {
   TextAreaForm,
   TagsForm,
   DropzoneForm,
+  SwitchForm,
 } from '@/components/ReactFormSet'
 import {
   UseFormReturn,
@@ -171,6 +172,17 @@ export const IdeaBaseForm = ({ title, form, onSubmit }: IdeaFormProps) => {
                   )
                 }}
               />
+              {(!form.getValues('publish') ||
+                form.getValues('publish') === undefined) && (
+                <SwitchForm
+                  form={form}
+                  name="publish"
+                  label="公開する"
+                  mt="xl"
+                  onLabel="公開"
+                  offLabel="下書き"
+                />
+              )}
             </Paper>
             <Center>
               <Button

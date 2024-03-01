852277234a4bf75667c7d1b83cbbae141b8d6f6c
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-03-01 17:16:58 +0900

    update:ユーザー数の取得のエラーを非表示代わりに-を表示 #1328

--- front/src/components/features/TopVision.tsx
+++ front/src/components/features/TopVision.tsx
@@ -1,14 +1,13 @@
 import { Title, Text, Button, Box, BackgroundImage } from '@mantine/core'
 import Link from 'next/link'
-import { useGetUserNumQuery } from '@/lib/generated/client'
+import { useGetUserCountQuery } from '@/lib/generated/client'
 
 export const TopVision = () => {
-  const { data, loading, error } = useGetUserNumQuery()
+  const { data, loading, error } = useGetUserCountQuery()
 
   if (loading) return <p>取得中...</p>
-  if (error) return <p>エラー: {error.message}</p>
-
-  const userNum = data?.userNum
+  
+  const userCount = data?.userCount
 
   return (
     <div
@@ -74,7 +73,7 @@ export const TopVision = () => {
         </Link>
         <Text c="#818181" pt="30px" fz="20px" fw="bold">
           現在のユーザー数:{' '}
-          <b style={{ color: '#FF862F', fontSize: '2rem' }}>{userNum}</b>人
+          <b style={{ color: '#FF862F', fontSize: '2rem' }}>{userCount !== undefined ? userCount : '-'}</b>人
         </Text>
       </Box>
     </div>

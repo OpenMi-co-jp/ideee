0c0bc1dd3205a926db4edd2beabc11d4c1bb7039
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-03-03 22:34:35 +0900

    #1253 update:resolever引数修正

--- api/app/graphql/resolvers/notification_config/notification_config_resolver.rb
+++ api/app/graphql/resolvers/notification_config/notification_config_resolver.rb
@@ -6,7 +6,7 @@ module Resolvers
       type Types::NotificationConfigType, null: false
 
       def resolve
-        ::NotificationConfig.find_by(user_id: context[:current_user].id)
+        ::NotificationConfig.find_by(user_id: context[:current_user].notification_config)
       end
     end
   end

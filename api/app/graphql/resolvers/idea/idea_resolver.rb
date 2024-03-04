20f9514ddf80f1db055ee912b28ad717ccb51ca1
naru20181117 <t.naruhiro.1026@gmail.com>
2024-03-04 19:31:58 +0900

    merge: conflict

--- api/app/graphql/resolvers/idea/idea_resolver.rb
+++ api/app/graphql/resolvers/idea/idea_resolver.rb
@@ -10,7 +10,7 @@ module Resolvers
 
     def resolve(**args)
       idea = ::Idea.find(args[:id])
-      if idea.draft? && context[:current_user]&.id != idea.user.id)
+      if idea.draft? && (context[:current_user]&.id != idea.user.id)
         raise GraphQL::ExecutionError, '権限がありません'
       end
 

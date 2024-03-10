fce7fa3c4a9aecacd2da6a726fe07d7866193708
naru20181117 <t.naruhiro.1026@gmail.com>
2024-03-10 19:45:45 +0900

    #1343 set: コメントのメール設定

--- api/app/graphql/mutations/comment/create.rb
+++ api/app/graphql/mutations/comment/create.rb
@@ -16,6 +16,9 @@ module Mutations
         idea_id: args[:idea_id]
       )
       comment.save!
+      idea = comment.idea
+      current_user.create_notification_comment(idea, comment)
+      SendCommentEmailJob.perform_later(current_user, idea, args[:description])
       {
         comment:,
         success: true

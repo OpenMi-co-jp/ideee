06cb547de2248140457a18ff03c9785c6b49bb99
naru20181117 <t.naruhiro.1026@gmail.com>
2024-03-18 04:07:19 +0900

    #1294 set: 準備中の表示

--- front/src/components/user/show/Profile.tsx
+++ front/src/components/user/show/Profile.tsx
@@ -1,13 +1,23 @@
 import { LinkComponent, Features } from '@/components/user/show'
 import { useUser } from '@/context/userProfileContext'
-import { Grid, Flex, Paper, Text, Image, Center } from '@mantine/core'
+import { Grid, Flex, Paper, Text, Tabs, Center, rem } from '@mantine/core'
 import { TextWithLinks } from '@/utils/Text'
 import { UserImage } from '@/components/image'
+import {
+  IconBulb,
+  IconHeart,
+  IconMessageCircle,
+  IconUsers,
+  IconPencil,
+} from '@tabler/icons-react'
+import { useCurrentUser } from '@/context/CurrentUserContext'
 
 export const Profile = () => {
   const user = useUser()
+  const { currentUser } = useCurrentUser()
   const defaultIcon = '/img/undefined_user_icon.webp'
   const icon = user?.image || defaultIcon
+  const isMyPage = String(currentUser?.id) === user?.id
 
   return (
     <>
@@ -32,6 +42,49 @@ export const Profile = () => {
           <TextWithLinks>{user?.description}</TextWithLinks>
         </Paper>
       )}
+      <Tabs color="yellow" variant="pills" defaultValue="idea_list" mt={'xl'}>
+        <Tabs.List>
+          <Tabs.Tab value="idea_list" leftSection={<IconBulb />}>
+            アイデア
+          </Tabs.Tab>
+          <Tabs.Tab value="heart_list" leftSection={<IconHeart />}>
+            ハート
+          </Tabs.Tab>
+          <Tabs.Tab value="comment_list" leftSection={<IconMessageCircle />}>
+            コメント
+          </Tabs.Tab>
+          <Tabs.Tab value="team_list" leftSection={<IconUsers />}>
+            チーム開発
+          </Tabs.Tab>
+          {isMyPage && (
+            <Tabs.Tab value="draft" leftSection={<IconPencil />}>
+              下書き
+            </Tabs.Tab>
+          )}
+        </Tabs.List>
+
+        <Tabs.Panel value="idea_list">
+          Coming Soon <IconBulb />
+        </Tabs.Panel>
+
+        <Tabs.Panel value="heart_list">
+          Coming Soon <IconHeart />
+        </Tabs.Panel>
+
+        <Tabs.Panel value="comment_list">
+          Coming Soon <IconMessageCircle />
+        </Tabs.Panel>
+
+        <Tabs.Panel value="team_list">
+          Coming Soon <IconUsers />
+        </Tabs.Panel>
+
+        {isMyPage && (
+          <Tabs.Panel value="draft">
+            Coming Soon <IconPencil />
+          </Tabs.Panel>
+        )}
+      </Tabs>
     </>
   )
 }

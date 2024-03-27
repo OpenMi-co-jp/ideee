4c9e6884caae4e42e84559f23071e599f6928320
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-03-27 23:45:46 +0900

    add: サイドバーとフッターの共通するリンクの変数化#1389

---
+++ front/src/utils/constant.ts
@@ -0,0 +1,36 @@
+// 認証
+export const LOGIN_URL = '/users/sign_in'
+export const LOGIN_TEXT = 'ログイン'
+
+export const SIGNUP_URL = '/users/sign_up'
+export const SIGNUP_TEXT = 'ユーザー登録'
+
+// CRUD
+export const EDIT_TEXT = '編集'
+export const DLT_TEXT = '削除'
+
+// フッター、サイドバー
+export const ABOUT_URL = '/about'
+export const ABOUT_TEXT = 'ideeeについて'
+
+export const FIND_IDEA_URL = '/how_to_find_idea'
+export const FIND_IDEA_TEXT = 'アイデアの探し方'
+
+export const TREND_URL =
+  'https://qiita.com/naruqiita/items/0ef4b963434226eacb6b'
+export const TREND_TEXT = '月間トレンドまとめ'
+
+export const AVOID_PITFALL_URL = '/avoid_pitfall'
+export const AVOID_PITFALL_TEXT = '個人開発の落とし穴'
+
+export const FAQ_URL = '/frequent_questions'
+export const FAQ_TEXT = 'よくある質問'
+
+export const TOS_URL = '/terms_of_service'
+export const TOS_TEXT = '利用規約'
+
+export const PRIVACY_POLICY_URL = '/privacy_policy'
+export const PRIVACY_POLICY_TEXT = 'プライバシーポリシー'
+
+export const NARU_URL = 'https://naruhiro-portfolio.firebaseapp.com'
+export const NARU_TEXT = '運営者'

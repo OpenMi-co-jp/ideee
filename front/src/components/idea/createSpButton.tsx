4fea490247404de6fd3fe170126fe3eed909f9fa
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-03-31 10:53:19 +0900

    update:コンポーネントの名前をファイル名に合わせる #1389

--- front/src/components/idea/createSpButton.tsx
+++ front/src/components/idea/createSpButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mantine/core'
 import { IconPencil } from '@tabler/icons-react'
 import Link from 'next/link'
 
-export const IdeaCreateSpButton = () => {
+export const CreateSpButton = () => {
   return (
     <Link href="/ideas/new">
       <Button variant="gradient" gradient={{ from: 'yellow', to: 'orange' }}>

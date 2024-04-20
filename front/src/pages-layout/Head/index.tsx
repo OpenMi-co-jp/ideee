e0e424661eb523f37322b15bda74eb4678cb0c85
naru20181117 <t.naruhiro.1026@gmail.com>
2024-04-20 23:11:57 +0900

    #1402 set: 動的なOGPを設定

--- front/src/pages-layout/Head/index.tsx
+++ front/src/pages-layout/Head/index.tsx
@@ -1,17 +1,47 @@
+import { FC } from 'react'
 import Head from 'next/head'
 import { ColorSchemeScript } from '@mantine/core'
 
-export const HeadBlock = () => {
-  const pageTitle = 'アイディー'
-  const title = pageTitle ? `${pageTitle} | ` : null
+interface MetaData {
+  pageTitle?: string
+  pageDescription?: string
+  pagePath?: string
+  pageImg?: string
+  pageKeywords?: string
+}
+
+export const HeadBlock: FC<MetaData> = ({
+  pageTitle,
+  pageDescription = 'エンジニアとアイデアのマッチングプラットフォーム',
+  pagePath,
+  pageImg,
+  pageKeywords,
+}) => {
+  const title = pageTitle ? `${pageTitle} | ideee` : 'アイディー | ideee'
+  const description = pageDescription
+  const url = pagePath || 'https://ideee.tech'
+  const imgUrl =
+    pageImg ||
+    'https://res.cloudinary.com/dnltqdyfu/image/authenticated/s--ffmUz0gR--/v1645150327/default_ogp.webp'
+  const keywords = pageKeywords || 'アイデア,エンジニア,マッチング'
+
   return (
     <Head>
-      <title>{title}ideee</title>
-      <meta
-        name="description"
-        content="エンジニアとアイデアのマッチングプラットフォーム"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <link rel="canonical" href={url} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="ja_JP" />
+      <meta property="og:image" content={imgUrl} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ideee_tech" />
       <link rel="icon" href="/favicon.ico" />
       <ColorSchemeScript />
     </Head>

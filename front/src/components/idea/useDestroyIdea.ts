da5f852b8b3940f16b7c13075de40516b5091bd8
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-04-21 23:11:56 +0900

    update:コメントを戻す＆使ってない定義の削除#1275

--- front/src/components/idea/useDestroyIdea.ts
+++ front/src/components/idea/useDestroyIdea.ts
@@ -2,9 +2,13 @@ import { useDestroyIdeaMutation } from '@/lib/generated/client'
 import { useRouter } from 'next/router'
 import { showSuccess, showError } from '../showNotification'
 
+/**
+ * CurrentUserのアイデアを削除するための関数
+ * @param
+ * @returns  アイデアを削除するための関数。
+ */
 export const useDestroyIdea = () => {
   const router = useRouter()
-  const { id: ideaId } = router.query
   const [destroyIdea] = useDestroyIdeaMutation()
 
   const handleDestroyIdea = (ideaId: string) => {

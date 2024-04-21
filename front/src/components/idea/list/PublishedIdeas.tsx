cdb4ecaa7ee99c70d850ad792d5d72abe7602a17
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-04-21 18:52:31 +0900

    add:PublishedIdeaコンポーネントを作成する#1275

---
+++ front/src/components/idea/list/PublishedIdeas.tsx
@@ -0,0 +1,45 @@
+import { Center, Flex, Pagination, Text } from '@mantine/core'
+import { useGetPublishedIdeasQuery } from '@/lib/generated/client'
+import { IdeaCard } from '@/components/idea'
+import { IdeaCreateButton } from '@/components/idea/createButton'
+import { AlertError } from '@/components/alert'
+import { LoaderBox } from '@/components/features'
+import { useState } from 'react'
+
+const PAGE_SIZE = 10
+export const PublishedIdeas = () => {
+  const { loading, data, error, refetch } = useGetPublishedIdeasQuery()
+  const { pageInfo, nodes } = data?.publishedIdeas || {}
+
+  const [page, setPage] = useState(1)
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage)
+    refetch({ page: newPage, per: PAGE_SIZE })
+  }
+
+  if (loading) return <LoaderBox />
+  if (error) return <AlertError />
+
+  return (
+    <>
+      {pageInfo?.totalCount && pageInfo?.totalCount > 0 ? (
+        <>
+          <IdeaCard ideas={nodes} />
+          <Center my="xl">
+            {pageInfo?.totalPages && pageInfo?.totalPages > 1 && (
+              <Pagination
+                total={pageInfo?.totalPages}
+                onChange={handlePageChange}
+              />
+            )}
+          </Center>
+        </>
+      ) : (
+        <Flex my="xl" wrap="wrap" align="center" justify="center" gap="sm">
+          <Text size="lg">公開中のアイディアがありません</Text>
+          <IdeaCreateButton />
+        </Flex>
+      )}
+    </>
+  )
+}

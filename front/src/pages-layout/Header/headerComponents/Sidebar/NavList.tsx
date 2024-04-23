606fb897962ae6d86ac3a624248254d205b64d2b
hamadash <60833258+hamadash@users.noreply.github.com>
2024-04-24 08:44:13 +0900

    add: hrefに関するFIXMEコメントを追加 #1412

--- front/src/pages-layout/Header/headerComponents/Sidebar/NavList.tsx
+++ front/src/pages-layout/Header/headerComponents/Sidebar/NavList.tsx
@@ -96,6 +96,7 @@ export const NavList = ({ close }: NavListProps) => {
           <NavItem
             item={{
               label: 'ログアウト',
+              // FIXME: hrefはrequiredになっているので仕方なく設定していますが、実質は不要です。hrefの指定がなくなれば、onClickのpreventDefaultもいらなくなります。
               href: '/',
               icon: IconLogout2,
               onClick: (e) => {

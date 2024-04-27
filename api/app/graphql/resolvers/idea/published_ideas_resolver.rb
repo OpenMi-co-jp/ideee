fcb0399734ee9933856a4ba29f4626d4c7f03415
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-04-27 14:42:32 +0900

    change:ユーザーIDから公開アイデアを取得するよう修正する#1275

--- api/app/graphql/resolvers/idea/published_ideas_resolver.rb
+++ api/app/graphql/resolvers/idea/published_ideas_resolver.rb
@@ -4,9 +4,13 @@ module Resolvers
     include Resolvers::Concerns::Pagination
     type Types::Idea::IdeasType, null: false
 
+    argument :id, ID, required: true, description: 'ユーザーID'
+
     def resolve(**args)
-      current_user = context[:current_user]
-      published_ideas = current_user.ideas.published.eager_load(:user).preload(:idea_tags).order(created_at: :desc)
+      user = ::User.find(args[:id])
+      raise GraphQL::ExecutionError, 'ユーザーが見つかりません' if user.nil?
+
+      published_ideas = user.ideas.published.eager_load(:user).preload(:idea_tags).order(created_at: :desc)
       to_paged_result(Kaminari.paginate_array(published_ideas).page(args[:page]).per(args[:per]))
     end
   end

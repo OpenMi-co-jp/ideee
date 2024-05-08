4fef1874fc7781d78f15db4bb37a0f5ca5536bce
naru20181117 <t.naruhiro.1026@gmail.com>
2024-05-08 19:20:17 +0900

    #1430 update: コメントボタンの二重クリックを回避

--- front/src/components/comment/create/CommentCreateForm.tsx
+++ front/src/components/comment/create/CommentCreateForm.tsx
@@ -2,13 +2,22 @@ import { Button, Center } from '@mantine/core'
 import { TextAreaForm } from '@/components/ReactFormSet'
 import { useCommentAction } from './create'
 import { IconSend } from '@tabler/icons-react'
+import { useState } from 'react'
 
 export const CommentCreateForm = () => {
   const { form, onSubmit } = useCommentAction()
   const description = form.watch('description')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsSubmitting(true)
+    await form.handleSubmit(onSubmit)(event)
+    setIsSubmitting(false)
+  }
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit}>
       <TextAreaForm form={form} name="description" label="コメント" />
 
       <Center mt="lg">
@@ -17,7 +26,7 @@ export const CommentCreateForm = () => {
           variant="light"
           size="lg"
           color="orange"
-          disabled={!description}
+          disabled={isSubmitting || !description}
           leftSection={<IconSend />}
         >
           保存

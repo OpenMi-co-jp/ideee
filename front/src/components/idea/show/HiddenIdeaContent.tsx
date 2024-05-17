09d7a89e2455474629e7c79f739f669f04aec9ff
naru20181117 <t.naruhiro.1026@gmail.com>
2024-05-17 13:10:42 +0900

    #1448 change: コメントフォームをログインしてなかったら消す設定

--- front/src/components/idea/show/HiddenIdeaContent.tsx
+++ front/src/components/idea/show/HiddenIdeaContent.tsx
@@ -2,15 +2,18 @@ import { Space } from '@mantine/core'
 import { IdeaContents } from '@/components/idea/show'
 import { IdeaOptions } from '@/components/idea/option'
 import { CommentList, CommentCreateForm } from '@/components/comment'
+import { useCurrentUser } from '@/context/CurrentUserContext'
 
 export const HiddenIdeaContent = () => {
+  const { currentUser } = useCurrentUser()
+
   return (
     <>
       <IdeaContents />
       <IdeaOptions />
       <Space h="xl" />
       <CommentList />
-      <CommentCreateForm />
+      {currentUser && <CommentCreateForm />}
     </>
   )
 }

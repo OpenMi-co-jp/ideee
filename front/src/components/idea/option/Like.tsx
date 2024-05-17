8786d3ca0cf0de1d7342e7af6b604d4b8f140347
naru20181117 <t.naruhiro.1026@gmail.com>
2024-05-17 13:30:33 +0900

    #1448 update: ログインしてなかったらハートを押してもログイン画面に飛ばす

--- front/src/components/idea/option/Like.tsx
+++ front/src/components/idea/option/Like.tsx
@@ -2,17 +2,34 @@ import { IconHeartFilled, IconHeart } from '@tabler/icons-react'
 import { Button } from '@mantine/core'
 import { useToggleLike } from '@/components/like/useToggleLike'
 import { useRouter } from 'next/router'
+import { useCurrentUser } from '@/context/CurrentUserContext'
+import { showError } from '@/components/showNotification'
 
 /**
  * Likeコンポーネントは、アイデアに対する「いいね」の切り替え機能を提供します。
  * @returns Button, IconHeartFilled, IconHeart
  */
 export const Like = () => {
-  const { id } = useRouter().query
+  const router = useRouter()
+  const { id } = router.query
   const { isLike, toggleLike } = useToggleLike(Number(id), 'Idea')
+  const { currentUser } = useCurrentUser()
+
+  const handleLikeClick = () => {
+    if (currentUser) {
+      toggleLike()
+    } else {
+      // ログインしていない場合は、ログインページに遷移する
+      router.push('/users/sign_in')
+      showError({
+        action: 'ハート追加',
+        message: 'ログインしてください',
+      })
+    }
+  }
 
   return (
-    <Button onClick={toggleLike} variant="transparent" px="xs">
+    <Button onClick={handleLikeClick} variant="transparent" px="xs">
       {isLike ? (
         <IconHeartFilled style={{ color: 'black' }} />
       ) : (

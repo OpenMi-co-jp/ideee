d015b751face0ee1ab1334a0b0e44421b6aab292
Taisei <apple@tai0000.local>
2024-05-23 22:49:13 +0900

    fix:サインイン画面の画像サイズ修正

--- front/src/pages-layout/authLayout/AuthLayout.tsx
+++ front/src/pages-layout/authLayout/AuthLayout.tsx
@@ -1,24 +1,19 @@
-import { Card, Grid, Image } from '@mantine/core'
+import { Grid, Image } from '@mantine/core'
 import type { CustomLayout } from 'next'
 
 export const AuthLayout: CustomLayout = (page) => {
   return (
-    <Card padding="xl">
-      <Grid grow gutter="xs">
-        <Grid.Col span={{ base: 12, md: 6 }}>
-          <Card>
-            <Card.Section>
-              <Image
-                mx="auto"
-                fit="cover"
-                src="/img/userAuth.webp"
-                alt="user auth image"
-              />
-            </Card.Section>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6 }}>{page}</Grid.Col>
-      </Grid>
-    </Card>
+    <Grid grow gutter="xs">
+      <Grid.Col span={{ base: 12, md: 6 }}>
+        <Image
+          mx="auto"
+          fit="contain"
+          src="/img/userAuth.webp"
+          alt="user auth image"
+          h={{ base: 300, md: 450 }}
+        />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6 }}>{page}</Grid.Col>
+    </Grid>
   )
 }

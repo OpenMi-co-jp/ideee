897a74dffd287c369b513985139d48a40fe992d1
ShotaMatsushima <shota19890523.engineer@gmail.com>
2024-05-26 18:39:09 +0900

    change: Containerの位置を変更

--- front/src/components/idea/form/BaseForm.tsx
+++ front/src/components/idea/form/BaseForm.tsx
@@ -45,11 +45,11 @@ export const IdeaBaseForm = ({ type, form, onSubmit }: IdeaFormProps) => {
   const { errors } = useFormState({ control: form.control })
 
   return (
-    <Paper py={rem(40)}>
-      <Title order={2} mb={30} fw={500} ta="center">
-        アイデア{type === 'create' ? '作成' : '編集'}
-      </Title>
-      <Container size="sm">
+    <Container size="sm">
+      <Paper py={rem(40)}>
+        <Title order={2} mb={30} fw={500} ta="center">
+          アイデア{type === 'create' ? '作成' : '編集'}
+        </Title>
         <form onSubmit={handleSubmit} role="form">
           <DropzoneForm
             form={form}
@@ -199,7 +199,7 @@ export const IdeaBaseForm = ({ type, form, onSubmit }: IdeaFormProps) => {
             </Button>
           </Center>
         </form>
-      </Container>
-    </Paper>
+      </Paper>
+    </Container>
   )
 }

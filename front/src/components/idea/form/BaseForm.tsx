c959a5de16c5a1f104b53dede3c6a01e51c77c74
ShotaMatsushima <shota19890523.engineer@gmail.com>
2024-05-26 18:24:00 +0900

    change: CenterからContainerに変更

--- front/src/components/idea/form/BaseForm.tsx
+++ front/src/components/idea/form/BaseForm.tsx
@@ -8,6 +8,7 @@ import {
   Button,
   Center,
   getGradient,
+  Container,
 } from '@mantine/core'
 import { IconBulb, IconBrandGithub, IconApps } from '@tabler/icons-react'
 import {
@@ -48,7 +49,7 @@ export const IdeaBaseForm = ({ type, form, onSubmit }: IdeaFormProps) => {
       <Title order={2} mb={30} fw={500} ta="center">
         アイデア{type === 'create' ? '作成' : '編集'}
       </Title>
-      <Center>
+      <Container size="sm">
         <form onSubmit={handleSubmit} role="form">
           <DropzoneForm
             form={form}
@@ -198,7 +199,7 @@ export const IdeaBaseForm = ({ type, form, onSubmit }: IdeaFormProps) => {
             </Button>
           </Center>
         </form>
-      </Center>
+      </Container>
     </Paper>
   )
 }

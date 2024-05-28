f0e9cf26350a3e44760e2ea15f06e3143cdf6a16
naru20181117 <t.naruhiro.1026@gmail.com>
2024-05-28 19:35:55 +0900

    #1433 ref: ai_responseをリファクタリング

--- api/config/initializers/openai.rb
+++ api/config/initializers/openai.rb
@@ -1,3 +1,5 @@
+OPENAI_MODEL = ENV.fetch('OPENAI_MODEL', 'gpt-4-turbo')
+OPENAI_TEMPERATURE = ENV.fetch('OPENAI_TEMPERATURE', 0.3).to_f
 OpenAI.configure do |config|
   config.access_token = Rails.application.credentials.dig(:open_ai_api, :access_token)
   # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional

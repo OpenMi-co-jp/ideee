fb4d87d0024281cd344c2dc01fd89c745263aac6
naru20181117 <t.naruhiro.1026@gmail.com>
2024-06-04 19:44:13 +0900

    hot_release: Sentryの設定（感度上昇）

--- front/sentry.client.config.ts
+++ front/sentry.client.config.ts
@@ -8,16 +8,19 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.2,
+  tracesSampleRate: 1.0,
+
+  profilesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: process.env.NODE_ENV === 'development',
+  debug: true,
+  // debug: process.env.NODE_ENV === 'development',
 
-  replaysOnErrorSampleRate: 0.2,
+  replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.2,
+  replaysSessionSampleRate: 1.0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [

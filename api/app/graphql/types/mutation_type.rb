2d064f0ad40b55cbc50bceec5e938842f15609b9
naru20181117 <t.naruhiro.1026@gmail.com>
2024-06-14 16:46:12 +0900

    #1094 set: AIでreview取得用バックエンドを設定

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -7,6 +7,7 @@ module Types
     field :create_idea, mutation: Mutations::Idea::Create, description: 'アイデア作成'
     field :update_idea, mutation: Mutations::Idea::Update, description: 'アイデア更新'
     field :destroy_idea, mutation: Mutations::Idea::Destroy, description: 'アイデア削除'
+    field :create_ai_review, mutation: Mutations::Idea::AiReview, description: 'AIレビュー作成'
 
     field :create_user, mutation: Mutations::User::Create, description: 'ユーザー作成'
     field :update_user, mutation: Mutations::User::Update, description: 'ユーザー更新'

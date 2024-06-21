ecb70e0f0642e902247666914bed7b63bc807279
naru20181117 <t.naruhiro.1026@gmail.com>
2024-06-21 16:52:01 +0900

    #1485 update: ログの回数制限を見える化

--- front/src/components/user/show/Features.tsx
+++ front/src/components/user/show/Features.tsx
@@ -1,40 +1,51 @@
 import { useUser } from '@/context/userProfileContext'
 import { Grid, Flex, Box, Text, Center } from '@mantine/core'
+import { CustomDonutChart } from '@/lib/mantine/CustomDonutChart'
 
 export const Features = () => {
   const user = useUser()
+  const todaysAiLogCount = user?.todaysAiLogCount || 0
+  const remainingAiLogCount = 3 - todaysAiLogCount
 
   return (
-    <>
-      <Grid>
-        <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
-          <Box
-            p="md"
-            style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}
-          >
-            <Center>
-              <Flex direction="column" align="center" gap="md">
-                <Text c="gray">タイプ</Text>
-                <Text fz="1.4rem">{user?.definition}</Text>
-              </Flex>
-            </Center>
-          </Box>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
-          <Box
-            p="md"
-            style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}
-          >
-            <Center>
-              <Flex direction="column" align="center" gap="md">
-                <Text c="gray">Contributions</Text>
-                <Text fz="1.4rem">{user?.point}</Text>
-              </Flex>
-            </Center>
-          </Box>
-        </Grid.Col>
-        {/* TODO: 追加要素を加える */}
-      </Grid>
-    </>
+    <Grid>
+      <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
+        <UserInfoBox label="タイプ" value={user?.definition || ''} />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
+        <UserInfoBox label="Contributions" value={String(user?.point)} />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
+        <Box p="md" style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}>
+          <Center>
+            <Flex direction="column" align="center" gap="md">
+              <Text c="gray">本日のAI利用回数</Text>
+              <CustomDonutChart
+                label={`${todaysAiLogCount} / 3`}
+                data={[
+                  {
+                    name: '残り回数',
+                    value: remainingAiLogCount,
+                    color: 'orange',
+                  },
+                  { name: '利用回数', value: todaysAiLogCount, color: 'gray' },
+                ]}
+              />
+            </Flex>
+          </Center>
+        </Box>
+      </Grid.Col>
+    </Grid>
   )
 }
+
+const UserInfoBox = ({ label, value }: { label: string; value: string }) => (
+  <Box p="md" style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}>
+    <Center>
+      <Flex direction="column" align="center" gap="md">
+        <Text c="gray">{label}</Text>
+        <Text fz="1.4rem">{value}</Text>
+      </Flex>
+    </Center>
+  </Box>
+)

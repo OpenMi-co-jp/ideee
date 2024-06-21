353d8c98f63e49b157ca38e939fbf5186d287648
naru20181117 <t.naruhiro.1026@gmail.com>
2024-06-21 16:50:28 +0900

    #1485 set: ai_logのモデルを設定

--- api/app/graphql/mutations/idea/ai_review.rb
+++ api/app/graphql/mutations/idea/ai_review.rb
@@ -13,16 +13,27 @@ module Mutations
       return { success: false, errors: ['アイデアが見つかりません'] } if idea.nil?
       return { success: false, errors: ['アイデアが公開されていません'] } if idea.draft
 
+      todays_logs_count = idea.user.todays_ai_log_count
+
+      return { success: false, errors: ['本日のAI利用制限を超えています'] } if todays_logs_count >= 3
+
+      todays_logs_count += 1 if create_ai_log(context[:current_user], 'review')
+
       job = AI::ReviewsJob.perform_later(args[:idea_id])
       {
         job_id: job.job_id,
-        success: true
+        success: true,
+        errors: ["本日の残りAI利用回数：#{3 - todays_logs_count} 回"]
       }
     rescue StandardError => e
       {
         success: false,
         errors: [e.message]
       }
+    end
+
+    def create_ai_log(user, action)
+      ::AiLog.create!(user:, action:)
     end
   end
 end
--- front/src/lib/generated/client.ts
+++ front/src/lib/generated/client.ts
@@ -1047,6 +1047,8 @@ export type User = {
   point?: Maybe<Scalars['Int']>
   /** サイトURL */
   siteUrl?: Maybe<Scalars['String']>
+  /** 本日のAI利用回数 */
+  todaysAiLogCount?: Maybe<Scalars['Int']>
   /** TwitterID */
   twitterId?: Maybe<Scalars['String']>
 }
@@ -1845,6 +1847,7 @@ export type GetUserQuery = {
     githubId?: string | null
     ideasNum?: number | null
     image?: string | null
+    todaysAiLogCount?: number | null
   }
 }
 
@@ -4172,6 +4175,7 @@ export const GetUserDocument = gql`
       githubId
       ideasNum
       image
+      todaysAiLogCount
     }
   }
 `

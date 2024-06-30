0ff569adab5ec051a77180d90337eb10c3e1f601
naru20181117 <t.naruhiro.1026@gmail.com>
2024-06-30 14:58:10 +0900

    #1395 fix: AI利用回数表示を自分だけ表示に修正

--- front/src/components/user/show/Features.tsx
+++ front/src/components/user/show/Features.tsx
@@ -1,9 +1,11 @@
 import { useUser } from '@/context/userProfileContext'
 import { Grid, Flex, Box, Text, Center } from '@mantine/core'
 import { CustomDonutChart } from '@/lib/mantine/CustomDonutChart'
+import { useCurrentUser } from '@/context/CurrentUserContext'
 
 export const Features = () => {
   const user = useUser()
+  const { currentUser } = useCurrentUser()
   const todaysAiLogCount = user?.todaysAiLogCount || 0
   const remainingAiLogCount = 3 - todaysAiLogCount
 
@@ -15,26 +17,35 @@ export const Features = () => {
       <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
         <UserInfoBox label="Contributions" value={String(user?.point)} />
       </Grid.Col>
-      <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
-        <Box p="md" style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}>
-          <Center>
-            <Flex direction="column" align="center" gap="md">
-              <Text c="gray">本日のAI利用回数</Text>
-              <CustomDonutChart
-                label={`${todaysAiLogCount} / 3`}
-                data={[
-                  {
-                    name: '残り回数',
-                    value: remainingAiLogCount,
-                    color: 'orange',
-                  },
-                  { name: '利用回数', value: todaysAiLogCount, color: 'gray' },
-                ]}
-              />
-            </Flex>
-          </Center>
-        </Box>
-      </Grid.Col>
+      {currentUser && String(currentUser.id) === user?.id && (
+        <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
+          <Box
+            p="md"
+            style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}
+          >
+            <Center>
+              <Flex direction="column" align="center" gap="md">
+                <Text c="gray">本日のAI利用回数</Text>
+                <CustomDonutChart
+                  label={`${todaysAiLogCount} / 3`}
+                  data={[
+                    {
+                      name: '残り回数',
+                      value: remainingAiLogCount,
+                      color: 'orange',
+                    },
+                    {
+                      name: '利用回数',
+                      value: todaysAiLogCount,
+                      color: 'gray',
+                    },
+                  ]}
+                />
+              </Flex>
+            </Center>
+          </Box>
+        </Grid.Col>
+      )}
     </Grid>
   )
 }

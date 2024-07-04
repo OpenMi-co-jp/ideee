b124f718558321285be0d594adcd556795c0a499
hamadash <60833258+hamadash@users.noreply.github.com>
2024-07-04 20:31:03 +0900

    change: ログイン画面はメールアドレスやパスワードのタイポで頻繁にエラーが出そうなので決め打ちでメッセージを出す

--- front/src/components/Auth/SignIn/hooks.ts
+++ front/src/components/Auth/SignIn/hooks.ts
@@ -10,17 +10,29 @@ export const handleSignIn = async (
   props: SignInFormValues,
   storeCurrentUser: CurrentUserContextType['storeCurrentUser']
 ) => {
-  const response = await signIn(props)
-  const token = response.headers['authorization']
-  // TODO: validateTokenメソッドを設定
-  if (token) {
-    Cookies.set('authToken', String(token), {
-      expires: 7,
-      secure: true,
+  signIn(props)
+    .then((response) => {
+      const token = response.headers['authorization']
+      // TODO: validateTokenメソッドを設定
+      if (token) {
+        Cookies.set('authToken', String(token), {
+          expires: 7,
+          secure: true,
+        })
+        const decodedToken = DecodeJwt(String(token))
+        storeCurrentUser(decodedToken)
+      }
+      showSuccess({ action: 'ログイン' })
     })
-    const decodedToken = DecodeJwt(String(token))
-    storeCurrentUser(decodedToken)
-  }
-  showSuccess({ action: 'ログイン' })
+    .catch(() => {
+      // FIXME: 本当は500エラーの可能性もあるので決め打ちでエラーメッセージを出すのはよくないが、バックエンドでちゃんとハンドリングされていないのでそちらから修正する必要がある
+      // ref. https://github.com/naru20181117/ideee/pull/1501/files#r1660337857
+      showError({
+        action: 'ログイン',
+        message:
+          'メールアドレスかパスワードが間違っています。もう一度やり直してください。',
+      })
+    })
+
   modals.closeAll()
 }

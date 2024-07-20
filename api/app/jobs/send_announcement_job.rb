30d44a57c2320a82889dca3ba268ebd3de99fc0c
naru20181117 <t.naruhiro.1026@gmail.com>
2024-07-20 15:20:40 +0900

    fix: userがなかったときの対応追加

--- api/app/jobs/send_announcement_job.rb
+++ api/app/jobs/send_announcement_job.rb
@@ -3,8 +3,8 @@ class SendAnnouncementJob < ApplicationJob
 
   def perform(user_ids)
     user_ids.each do |user_id|
-      user = User.find(user_id)
-      ::UserMailer.send_ai_feature_announcement(user).deliver_later
+      user = User.find_by(id: user_id)
+      ::UserMailer.send_ai_feature_announcement(user).deliver_later if user
     end
   end
 end

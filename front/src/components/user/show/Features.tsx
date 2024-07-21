202b25fc4646796f33b6fe57088df93b53eec41f
Taisei <apple@tai0000.local>
2024-07-21 17:30:48 +0900

    fix:入力達成率の追加

--- front/src/components/user/show/Features.tsx
+++ front/src/components/user/show/Features.tsx
@@ -1,7 +1,19 @@
 import { useUser } from '@/context/userProfileContext'
-import { Grid, Flex, Box, Text, Center, SimpleGrid } from '@mantine/core'
+import { Grid, Box, Text, Center, Flex, Title, Progress } from '@mantine/core'
 import { CustomDonutChart } from '@/lib/mantine/CustomDonutChart'
 import { useCurrentUser } from '@/context/CurrentUserContext'
+import { GetUserQuery } from '@/lib/generated/client'
+
+const calculateCompletionRate = (
+  user: GetUserQuery['user'] | undefined
+): number => {
+  const ratePerField = 25
+  const fields = ['name', 'description', 'definition', 'image'] as const
+  return fields.reduce(
+    (acc, field) => (user?.[field] ? acc + ratePerField : acc),
+    0
+  )
+}
 
 export const Features = () => {
   const user = useUser()
@@ -16,16 +28,54 @@ export const Features = () => {
         ? 'アイディアマン'
         : ''
 
+  const completionRateForUser = calculateCompletionRate(user)
+
   return (
-    <Grid>
+    <Grid mb={20}>
+      <Grid.Col span={{ base: 12, xs: 12, sm: 12, md: 12 }}>
+        <Flex
+          justify={{ base: 'center', md: 'flex-end' }}
+          align="center"
+          wrap="wrap"
+        >
+          <Title c="gray" fz="1.2rem" mb={15}>
+            ユーザー情報入力完了率
+            <Text span c="orange" fz="2rem" pl={5} inherit>
+              {completionRateForUser}%
+            </Text>
+          </Title>
+        </Flex>
+        <Progress.Root size={20} mb={20}>
+          {user?.name && (
+            <Progress.Section value={25} color="cyan">
+              <Progress.Label>ユーザー名</Progress.Label>
+            </Progress.Section>
+          )}
+          {user?.description && (
+            <Progress.Section value={25} color="pink">
+              <Progress.Label>自己紹介文</Progress.Label>
+            </Progress.Section>
+          )}
+          {user?.image && (
+            <Progress.Section value={25} color="lime">
+              <Progress.Label>プロフィール画像</Progress.Label>
+            </Progress.Section>
+          )}
+          {user?.definition && (
+            <Progress.Section value={25} color="orange">
+              <Progress.Label>ユーザータイプ</Progress.Label>
+            </Progress.Section>
+          )}
+        </Progress.Root>
+      </Grid.Col>
       <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
         <UserInfoBox label="タイプ" value={userType} />
       </Grid.Col>
       <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
         <UserInfoBox label="Contributions" value={String(user?.point)} />
       </Grid.Col>
-      {currentUser && String(currentUser.id) === user?.id && (
-        <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4 }}>
+      {currentUser && String(currentUser?.id) === user?.id && (
+        <Grid.Col span={{ base: 12, xs: 12, sm: 12, md: 4 }}>
           <Box
             p="md"
             style={{ borderRadius: '5%', border: '1px solid #dcdcdc' }}

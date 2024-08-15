6e916cd1b3047d447f2270d6813fe34f897f2c31
naru20181117 <t.naruhiro.1026@gmail.com>
2024-08-15 15:46:44 +0900

    #1533 set: sitemapを生成

---
+++ front/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_FRONT_URL || 'https://ideee.tech',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+}

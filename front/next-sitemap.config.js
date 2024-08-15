a7fe38702620fa1051d3149678c27cdee6d71af7
naru20181117 <t.naruhiro.1026@gmail.com>
2024-08-15 15:49:59 +0900

    #1533 update: excludeの設定

--- front/next-sitemap.config.js
+++ front/next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_FRONT_URL || 'https://ideee.tech',
   generateRobotsTxt: true,
   changefreq: 'weekly',
+  exclude: ['/mypage/*', '/ideas/new', '/user/*']
 }

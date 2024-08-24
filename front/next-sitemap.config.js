780640daeaf302e472aa656688714f9297353bba
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-08-24 17:13:45 +0900

    add:SSRによるidsの取得

--- front/next-sitemap.config.js
+++ front/next-sitemap.config.js
@@ -1,3 +1,5 @@
+import generateIdeasIds from './src/lib/generateIdeasIds'
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_FRONT_URL || 'https://ideee.tech',
@@ -6,20 +8,11 @@ module.exports = {
   exclude: ['/mypage/*', '/ideas/new', '/user/*'],
   // 動的ルーティングの設定
   additionalPaths: async (config) => {
-    const ideaPaths = await fetchIdeaPaths();
+    const ideaPaths = await generateIdeasIds()
     return ideaPaths.map((id) => ({
       loc: `/ideas/${id}`,
       changefreq: 'weekly',
       priority: 0.7,
-      }));
-    },
-}
-
-// 動的ルーティングのパスを取得する関数
-async function fetchIdeaPaths() {
-  // ここでAPIやデータベースから動的パスを取得します
-  // 例: const res = await fetch('https://api.example.com/ideas');
-  // const ideas = await res.json();
-  // return ideas.map((idea) => idea.id);
-  return ['1', '2', '3']; // 仮のデータ
+    }));
+  },
 }

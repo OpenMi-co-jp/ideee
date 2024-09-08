9370e5500f8b571659f02ca1befbd122c9e10144
naru20181117 <t.naruhiro.1026@gmail.com>
2024-09-08 15:21:16 +0900

    merge: conflict

--- api/app/graphql/types/idea/ai_ideas_type.rb
+++ api/app/graphql/types/idea/ai_ideas_type.rb
@@ -5,6 +5,6 @@ module Types
     field :name, String, null: false, description: 'AIアイデア名'
     field :goal, String, description: 'ゴール'
     field :background, String, description: '背景'
-    field :idea_tags, [Types::TagType], null: true, description: 'タグオブジェクト'
+    field :idea_tags, [String], null: true, description: 'タグオブジェクト'
   end
 end

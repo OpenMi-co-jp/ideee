1ed8cffd231e3bddb978424e561d3dded011b87a
naru20181117 <t.naruhiro.1026@gmail.com>
2024-09-16 14:24:52 +0900

    #1526 fix: update_teamのエラー対応

--- api/app/graphql/mutations/team/update.rb
+++ api/app/graphql/mutations/team/update.rb
@@ -3,21 +3,21 @@ module Mutations
     graphql_name 'UpdateTeam'
 
     argument :id, ID, required: true, description: 'チームID'
-    argument :owner_id, ID, required: true, description: '【必須】オーナーID'
     argument :status, Integer, required: false, description: 'チームステータス'
     argument :requirement, String, required: false, description: 'お願いすること'
     argument :offer, String, required: false, description: '(メンバーが)得られるもの'
 
-    field :team, Types::TeamType, null: false, description: 'チームオブジェクト'
+    field :team, Types::TeamType, null: true, description: 'チームオブジェクト'
     field :success, Boolean, null: false, description: '成功フラグ'
     field :errors, [String], null: true, description: 'エラーリスト'
 
     def resolve(**args)
-      if context[:current_user].id != args[:owner_id].to_i
+      team = ::Team.find(args[:id])
+
+      if context[:current_user].id != team.owner_id.to_i
         return { success: false, errors: ['ユーザーの権限がありません'] }
       end
 
-      team = ::Team.find(args[:id])
       team.update!(
         status: args[:status],
         requirement: args[:requirement],

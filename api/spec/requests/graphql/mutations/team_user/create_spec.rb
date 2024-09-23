2929b6f7bd0ee2a4c25d02242b44b099c32c06fc
rdhdchloe <chloe.socrates.7z@gmail.com>
2024-09-23 18:45:24 +0900

    add: RSpec

---
+++ api/spec/requests/graphql/mutations/team_user/create_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::TeamUser::Create do
+  subject(:graphql_post) { post graphql_path, params: { query:, variables: variables.to_json }, headers: tokens }
+
+  let(:user)   { create(:user) }
+  let(:team)   { create(:team) }
+  let(:tokens) { sign_in(user) }
+
+  let(:query) do
+    <<-GQL
+      mutation joinTeam($input: JoinTeamInput!) {
+        joinTeam(input: $input) {
+          teamUser {
+            teamId
+            userId
+          }
+          success
+        }
+      }
+    GQL
+  end
+
+  describe 'チームの参加' do
+    let(:variables) do
+      {
+        input: {
+          teamId: team.id.to_s,
+          userId: user.id.to_s
+        }
+      }
+    end
+
+    context 'チームへ参加する時' do
+      it '作成に成功すること' do
+        graphql_post
+        res = response.parsed_body
+        expect(res['data']['joinTeam']['success']).to be_truthy
+        expect(res['data']['joinTeam']['teamUser']['teamId']).to eq(team.id)
+        expect(res['data']['joinTeam']['teamUser']['userId']).to eq(user.id)
+      end
+    end
+
+    context '作成されていないteam指定されたとき' do
+      before do
+        variables[:input][:teamId] = 0
+      end
+
+      it 'raise errror ActiveRecord::RecordInvalidとなること' do
+        request_result = expect do
+          graphql_post
+        end
+
+        request_result.to raise_error(ActiveRecord::RecordInvalid, 'バリデーションに失敗しました: Teamを入力してください')
+      end
+    end
+
+    context 'すでにチームに参加している場合' do
+      before do
+        create(:team_user, team:, user:)
+      end
+
+      it '作成に失敗すること' do
+        graphql_post
+        res = response.parsed_body
+        expect(res['data']['joinTeam']['success']).to be false
+        expect(res['data']['joinTeam']['teamUser']).to be_nil
+      end
+    end
+
+    context 'teamIdを指定していないとき' do
+      before do
+        variables[:input][:teamId] = nil
+      end
+
+      it '作成に失敗しレスポンスにエラー内容が含まれること' do
+        graphql_post
+        res = response.parsed_body
+        expect(res['errors'][0]['message']).to include('Variable $input of type JoinTeamInput! was provided invalid value for teamId (Expected value to not be null)')
+      end
+    end
+
+    context 'userIdを指定していないとき' do
+      before do
+        variables[:input][:userId] = nil
+      end
+
+      it '作成に失敗しレスポンスにエラー内容が含まれること' do
+        graphql_post
+        res = response.parsed_body
+        expect(res['errors'][0]['message']).to include('Variable $input of type JoinTeamInput! was provided invalid value for userId (Expected value to not be null)')
+      end
+    end
+  end
+end
---
+++ api/spec/requests/graphql/mutations/team_user/destroy_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::TeamUser::Destroy do
+  subject(:graphql_post) { post graphql_path, params: { query:, variables: variables.to_json }, headers: tokens }
+
+  let(:user)   { create(:user) }
+  let(:team)   { create(:team) }
+  let(:tokens) { sign_in(user) }
+
+  let(:query) do
+    <<-GQL
+      mutation leaveTeam($input: LeaveTeamInput!) {
+        leaveTeam(input: $input) {
+          success
+          errors
+        }
+      }
+    GQL
+  end
+
+  describe 'チームの離脱' do
+    let(:variables) do
+      {
+        input: {
+          teamId: team.id.to_s,
+          userId: user.id.to_s
+        }
+      }
+    end
+
+    context '適切なinputで削除するとき' do
+      before do
+        create(:team_user, team:, user:)
+      end
+
+      it '削除に成功すること' do
+        graphql_post
+        res = response.parsed_body
+        reloaded_team_user = TeamUser.find_by(team_id: team.id, user_id: user.id)
+        expect(res['data']['leaveTeam']['success']).to be_truthy
+        expect(reloaded_team_user).to be_nil
+      end
+    end
+
+    context '更新対象のidが存在していないとき' do
+      before do
+        variables[:input][:teamId] = nil
+      end
+
+      it '作成に失敗しレスポンスにエラー内容が含まれること' do
+        graphql_post
+        res = response.parsed_body
+        expect(res['errors'][0]['message']).to include('Variable $input of type LeaveTeamInput! was provided invalid value for teamId (Expected value to not be null)')
+      end
+    end
+  end
+end

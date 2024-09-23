3728bcb86b7c7fb156ba096fb17e130073ce42c8
rdhdchloe <chloe.socrates.7z@gmail.com>
2024-09-23 18:43:50 +0900

    add: team_userが存在してるかどうかチェック

--- api/app/graphql/mutations/team_user/create.rb
+++ api/app/graphql/mutations/team_user/create.rb
@@ -5,11 +5,20 @@ module Mutations
     argument :team_id, ID, required: true, description: '【必須】チームID'
     argument :user_id, ID, required: true, description: '【必須】ユーザーID'
 
-    field :team_user, Types::TeamUserType, null: false, description: 'チームユーザーオブジェクト'
+    field :team_user, Types::TeamUserType, null: true, description: 'チームユーザーオブジェクト'
     field :success, Boolean, null: false, description: '成功フラグ'
     field :errors, [String], null: true, description: 'エラーリスト'
 
     def resolve(**args)
+      team_user = ::TeamUser.find_by(team_id: args[:team_id], user_id: args[:user_id])
+
+      if team_user.present?
+        return {
+          success: false,
+          errors: ['既に参加済み']
+        }
+      end
+
       team_user = ::TeamUser.new(
         team_id: args[:team_id],
         user_id: args[:user_id]
@@ -18,11 +27,6 @@ module Mutations
       {
         team_user:,
         success: true
-      }
-    rescue ActiveRecord::RecordInvalid => e
-      {
-        success: false,
-        errors: e.record.errors.full_messages
       }
     end
   end

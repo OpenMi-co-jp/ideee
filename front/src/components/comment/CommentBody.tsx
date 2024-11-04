2cd309a40be448150c90d47b307a6ba2485fbd03
rdhdchloe <chloe.socrates.7z@gmail.com>
2024-11-04 18:04:52 +0900

    change: フォマート修正

--- front/src/components/comment/CommentBody.tsx
+++ front/src/components/comment/CommentBody.tsx
@@ -40,51 +40,53 @@ export const CommentBody = ({ isCurrentUser }: { isCurrentUser: boolean }) => {
       >
         <TextWithLinks>{description}</TextWithLinks>
       </Paper>
-      {isCurrentUser ?
-        (
-          <Flex align="center" justify="start">
-            <Group
-              p={2}
-              c={hasLikes ? 'red' : 'gray'}
-              style={{
-                position: 'absolute',
-                bottom: '5px',
-                left: '5px',
-                background: 'white',
-                borderRadius: '5px',
-                border: '1px solid #ced4da'
-              }}>
-              <IconHeart
-                fill={hasLikes ? 'red' : 'gray'}
-                className={`${heartStyle.heartIcon} ${hasLikes ? heartStyle.liked : ''
-                  }`}
-              />
-              <Text c="gray">{comment.likesCount}</Text>
-            </Group>
-          </Flex>
-        ) : (
-          <Flex align="center" justify="end">
-            <ActionIcon
-              onClick={handleLike}
-              variant="default"
-              w={'40px'}
-              h={'30px'}
-              p={2}
-              c={isLike ? 'red' : 'gray'}
-              style={{
-                position: 'absolute',
-                bottom: '10px',
-                right: '5px',
-              }}
-            >
-              <IconHeart
-                fill={isLike ? 'red' : 'gray'}
-                className={`${heartStyle.heartIcon} ${isLike ? heartStyle.liked : ''
-                  }`}
-              />
-            </ActionIcon>
-          </Flex>
-        )}
+      {isCurrentUser ? (
+        <Flex align="center" justify="start">
+          <Group
+            p={2}
+            c={hasLikes ? 'red' : 'gray'}
+            style={{
+              position: 'absolute',
+              bottom: '5px',
+              left: '5px',
+              background: 'white',
+              borderRadius: '5px',
+              border: '1px solid #ced4da',
+            }}
+          >
+            <IconHeart
+              fill={hasLikes ? 'red' : 'gray'}
+              className={`${heartStyle.heartIcon} ${
+                hasLikes ? heartStyle.liked : ''
+              }`}
+            />
+            <Text c="gray">{comment.likesCount}</Text>
+          </Group>
+        </Flex>
+      ) : (
+        <Flex align="center" justify="end">
+          <ActionIcon
+            onClick={handleLike}
+            variant="default"
+            w={'40px'}
+            h={'30px'}
+            p={2}
+            c={isLike ? 'red' : 'gray'}
+            style={{
+              position: 'absolute',
+              bottom: '10px',
+              right: '5px',
+            }}
+          >
+            <IconHeart
+              fill={isLike ? 'red' : 'gray'}
+              className={`${heartStyle.heartIcon} ${
+                isLike ? heartStyle.liked : ''
+              }`}
+            />
+          </ActionIcon>
+        </Flex>
+      )}
       <Flex justify={isCurrentUser ? 'flex-end' : 'flex-start'}>
         <Text c="gray" mx="xs">
           {dayjs(commentCreatedAt).format('YYYY-MM-DD HH:mm')}

8e73d8d08fd88b1e3f50269ba4f70ae2f71db54c
rdhdchloe <chloe.socrates.7z@gmail.com>
2024-11-09 19:24:23 +0900

    add: アクションボタンにBaseTooltip使用

--- front/src/components/idea/option/UrlCopy.tsx
+++ front/src/components/idea/option/UrlCopy.tsx
@@ -2,26 +2,31 @@ import { IconCopy, IconSquareRoundedCheckFilled } from '@tabler/icons-react'
 import { CopyButton, Button } from '@mantine/core'
 import { useRouter } from 'next/router'
 import { showSuccess } from '@/components/showNotification'
+import { BaseTooltip } from '@/lib/mantine/BaseTooltip'
 
 export const UrlCopy = () => {
   const router = useRouter()
   const currentUrl = process.env.NEXT_PUBLIC_FRONT_URL + router.asPath
 
   return (
-    <CopyButton value={currentUrl} timeout={2000}>
-      {({ copied, copy }) => (
-        <Button
-          color={copied ? 'teal' : 'gray'}
-          onClick={() => {
-            copy()
-            showSuccess({ action: 'URLコピー' })
-          }}
-          variant="outline"
-          size="compact-sm"
-        >
-          {copied ? <IconSquareRoundedCheckFilled /> : <IconCopy />}
-        </Button>
-      )}
-    </CopyButton>
+    <BaseTooltip label="URLをコピー" position="top">
+      <div>
+        <CopyButton value={currentUrl} timeout={2000}>
+          {({ copied, copy }) => (
+            <Button
+              color={copied ? 'teal' : 'gray'}
+              onClick={() => {
+                copy()
+                showSuccess({ action: 'URLコピー' })
+              }}
+              variant="outline"
+              size="compact-sm"
+            >
+              {copied ? <IconSquareRoundedCheckFilled /> : <IconCopy />}
+            </Button>
+          )}
+        </CopyButton>
+      </div>
+    </BaseTooltip>
   )
 }

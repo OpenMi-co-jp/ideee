359dfe744ff89db683f46164643f6e2dbb519e59
naru20181117 <t.naruhiro.1026@gmail.com>
2026-05-19 12:47:01 +0900

    debug: Add response debug output to like/create_spec

--- api/spec/requests/graphql/mutations/like/create_spec.rb
+++ api/spec/requests/graphql/mutations/like/create_spec.rb
@@ -37,6 +37,9 @@ RSpec.describe Mutations::Like::Create do
 
       it 'リクエストが成功すること' do
         graphql_post
+        puts "DEBUG: Response status: #{response.status}"
+        puts "DEBUG: Response body: #{response.body}"
+        puts "DEBUG: Parsed body: #{response.parsed_body.inspect}"
         res = response.parsed_body
 
         expect(res['data']['createLike']['success']).to be_truthy
